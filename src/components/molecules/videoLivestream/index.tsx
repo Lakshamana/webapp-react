08bff881c0560d3f72dd025341c930d839b9228e
Murillo Vieira <murillov.dev@gmail.com>
2021-09-09 13:40:49 -0300

    Components for videoLivestream and videoOnDemand

---
+++ src/components/molecules/videoLivestream/index.tsx
@@ -0,0 +1,29 @@
+import { useCallback, useEffect, useState } from 'react'
+import 'videojs-youtube'
+const videoJS = require('video.js');
+
+const VideoLivestream = (props: any) => {
+  const [videoEl, setVideoEl] = useState(null)
+  const onVideo = useCallback((el) => {
+    setVideoEl(el)
+  }, [])
+
+  useEffect(() => {
+    if (videoEl == null) return
+    const player = videoJS(videoEl, props)
+    return () => {
+      player.dispose()
+    }
+  }, [props, videoEl])
+
+  return (
+    <>
+      <h1>Implementation for Video JS Livestream</h1>
+      <div data-vjs-player>
+        <video ref={onVideo} className="video-js" playsInline />
+      </div>
+    </>
+  )
+}
+
+export { VideoLivestream }
