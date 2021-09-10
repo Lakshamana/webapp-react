8bbf89c6500da86983e111556a268b81ec29f938
jorgeh71218 <jorge.hidalgo@sevenapps.com.br>
2021-09-11 00:12:03 +0300

    feature(modules): some modules/screens examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { AppRouter } from "router";
 
 import "./styles/index.css";
 import "./App.css";
 
-const App = () => {
-  return (
-    <ChakraProvider>
-      <div className="App" />
-    </ChakraProvider>
-  );
-};
+const App = () => (
+  <ChakraProvider>
+    <AppRouter />
+  </ChakraProvider>
+);
 
 export default App;
