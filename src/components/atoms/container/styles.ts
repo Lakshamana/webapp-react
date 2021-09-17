10fabed65b2344fe5c216b7f553c6c4186c9182e
jorgeh71218 <jorge.hidalgo@sevenapps.com.br>
2021-09-17 16:04:40 +0300

    fix(container): new props to container

--- src/components/atoms/container/styles.ts
+++ src/components/atoms/container/styles.ts
@@ -1,10 +1,11 @@
 import styled from "styled-components";
-import { space, flexbox, layout } from "styled-system";
+import { space, flexbox, layout, color } from "styled-system";
 
 import { Props } from "./types";
 
 export const StyleContainer = styled.div<Props>`
   ${space}
+  ${color}
   ${layout}
   ${flexbox}
 `;
--- src/components/atoms/container/types.ts
+++ src/components/atoms/container/types.ts
@@ -3,6 +3,7 @@ import { LayoutProps, SpaceProps, FlexboxProps } from "styled-system";
 export interface Props extends FlexboxProps, LayoutProps, SpaceProps {
   children: JSX.Element | JSX.Element[];
   onClick?: any;
+  backgroundColor?: string;
 }
 
 export const defaultProps = {
