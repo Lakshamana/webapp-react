3b06b46e6270268e5d29b328c70e6db444b4554d
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-09-20 17:45:21 -0300

    feat(button-types): add new types to button

---
+++ src/components/atoms/button/types.ts
@@ -0,0 +1,13 @@
+import { ColorProps, SpaceProps, LayoutProps, TypographyProps, BorderProps, VariantArgs } from 'styled-system';
+
+export interface ButtonProps extends SpaceProps, LayoutProps, TypographyProps, VariantArgs, BorderProps, ColorProps {
+  label: string;
+  style?: React.CSSProperties;
+  type: ButtonType;
+}
+
+export const defaultProps = {
+  label: ''
+};
+
+type ButtonType = 'submit' | 'reset' | 'billboard';
