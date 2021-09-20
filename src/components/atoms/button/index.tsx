b16df013902f6ba370267b33a7beae21f4fdaa9f
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-09-20 17:43:53 -0300

    feat(button-atom): created new button atom

---
+++ src/components/atoms/button/index.tsx
@@ -0,0 +1,22 @@
+import { ButtonProps, defaultProps } from './types';
+import { BoxButton } from './style';
+
+const Button = ({ label, type, ...props }: ButtonProps) => {
+    const getButtonStyle = () => {
+        switch (type) {
+            case 'submit':
+                return <BoxButton variant="primary" borderRadius={4} {...props}>{label}</BoxButton>
+            case 'reset':
+                return <BoxButton variant="secondary"  borderRadius={4} {...props}>{label}</BoxButton>
+            case 'billboard':
+                return <BoxButton  borderRadius={6} {...props}>{label}</BoxButton>
+            default:
+                return <BoxButton variant="primary" {...props}>{label}</BoxButton>
+        }
+    }
+    return getButtonStyle();
+};
+
+Button.defaultProps = defaultProps
+
+export { Button }
