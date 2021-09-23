a447f06e06f7c46461e3531ec9b9fd42719e5ddf
biancasilva <bianca@fanhero.com>
2021-09-23 12:23:19 -0300

    feat(apollo): Inital config for Apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,23 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import {
+	ApolloClient,
+	ApolloProvider,
+	InMemoryCache
+} from "@apollo/client";
 import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
+
+
+const client = new ApolloClient({
+	uri: `https://${process.env.REACT_APP_API_ENDPOINT}/graphql`,
+	cache: new InMemoryCache(),
+	credentials: "include",
+	resolvers: {}
+});
 
 Sentry.init({
 	dsn: process.env.REACT_APP_SENTRY_DSN,
@@ -16,7 +29,9 @@ Sentry.init({
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
 )
