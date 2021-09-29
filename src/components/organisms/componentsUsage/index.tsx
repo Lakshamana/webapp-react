e87be69d54f69ed0858da3befac418eea1734286
murillo7apps <76689782+murillo7apps@users.noreply.github.com>
2021-09-29 09:00:39 -0300

    Update index.tsx

--- src/components/organisms/componentsUsage/index.tsx
+++ src/components/organisms/componentsUsage/index.tsx
@@ -3,18 +3,16 @@ import { pxToRem } from "styles/metrics";
 
 import { Vote } from "components/atoms";
 
-const ComponentsUsage = () => {
-  return (
-    <Box background="white">
-      <Box w={pxToRem(144)} mb={pxToRem(24)}>PX_TO_REM SAMPLE</Box>
+const ComponentsUsage = () => (
+  <Box background="white">
+    <Box w={pxToRem(144)} mb={pxToRem(24)}>PX_TO_REM SAMPLE</Box>
 
-      <Flex>
-        <Vote type="upvote" votes={"1.3K"} />
-        <Spacer w={3} />
-        <Vote type="downvote" votes={"100"} />
-      </Flex>
-    </Box>
-  )
-}
+    <Flex>
+      <Vote type="upvote" votes={"1.3K"} />
+      <Spacer w={3} />
+      <Vote type="downvote" votes={"100"} />
+    </Flex>
+  </Box>
+)
 
 export { ComponentsUsage }
