4ced0c4caab0b914ca9655f0847da725e14eed5a
jorgeh71218 <jorge.hidalgo@sevenapps.com.br>
2021-10-04 21:26:49 +0300

    feature(side-menu): items sending

--- src/components/organisms/header/components/sideMenu/index.tsx
+++ src/components/organisms/header/components/sideMenu/index.tsx
@@ -1,11 +1,26 @@
-import { Container } from "components";
+import { Link } from "react-router-dom";
+import { Container, Text } from "components";
 import { PropsSideMenu } from "../../types";
-import { SideContainer } from "./styles";
+import { SideContainer, ScrollContainer } from "./styles";
 import { colors } from "styles";
 
-const SideMenu = ({ open, setOpen }: PropsSideMenu) => (
+const SideMenu = ({ open, data }: PropsSideMenu) => (
   <SideContainer backgroundColor={colors.black} {...{ open }}>
-    <Container>{/**/}</Container>
+    <ScrollContainer
+      flexDirection="column"
+      display={open ? "flex" : "none"}
+      my={3}
+    >
+      {data?.map((item: any) => (
+        <Link to={item.url} key={`Path-${item.id}`}>
+          <Container width={1} pl={3} py={3}>
+            <Text style={{ textTransform: "uppercase" }} color={colors.white}>
+              {item.label}
+            </Text>
+          </Container>
+        </Link>
+      ))}
+    </ScrollContainer>
   </SideContainer>
 );
 
--- src/components/organisms/header/components/sideMenu/styles.ts
+++ src/components/organisms/header/components/sideMenu/styles.ts
@@ -6,18 +6,26 @@ interface PropsSideContainer {
 }
 
 export const SideContainer = styled(StyleContainer)<PropsSideContainer>`
-  width: 100%;
   position: absolute;
+  top: 70px;
   z-index: 1000;
-  top: 100px;
-  height: 100%;
+  overflow: auto;
+  height: calc(100% - 70px);
   ${({ open }) =>
     open
       ? css`
-          width: 200px;
+          width: 500px;
           transition: ease-in 0.25s;
         `
       : `
       width: 0px;
       transition: ease-in 0.25s;`}
+  @media (min-width: 64em) {
+    top: 100px;
+    height: calc(100% - 100px);
+  }
+`;
+
+export const ScrollContainer = styled(StyleContainer)`
+  overflow: auto;
 `;
