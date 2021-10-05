f338a0879367fd6a8daabbe14e65aba8524aba04
jorgeh71218 <jorge.hidalgo@sevenapps.com.br>
2021-10-05 18:42:50 +0300

    fix(menuIcon): Fixes on styles

--- src/components/organisms/header/components/menuIcon/index.tsx
+++ src/components/organisms/header/components/menuIcon/index.tsx
@@ -1,14 +1,10 @@
-import { Container } from "components";
-
 import { PropsMenuIcon } from "../../types";
 import { Parent } from "./styles";
 
-const MenuIcon = ({ open, setOpen, ...props }: PropsMenuIcon) => (
-  <Container {...props}>
-    <Parent width="25px" onClick={setOpen} {...{ open }}>
-      <div />
-    </Parent>
-  </Container>
+const MenuIcon = ({ open, setOpen }: PropsMenuIcon) => (
+  <Parent width="25px" onClick={setOpen} {...{ open }}>
+    <div />
+  </Parent>
 );
 
 export { MenuIcon };
