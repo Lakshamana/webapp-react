3b277b9005a3bb8a6652f38829039b46189f7300
jorgeh71218 <jorge.hidalgo@sevenapps.com.br>
2021-10-07 10:23:37 +0300

    fix(footer): adjustments

--- src/components/molecules/footer/index.tsx
+++ src/components/molecules/footer/index.tsx
@@ -1,23 +1,31 @@
-import React, { ReactElement } from 'react';
-import { Text } from 'components/atoms';
-import { Logo } from 'components/atoms'
-import { BoxFooter, FooterItems, TextFooter } from "./style";
+import { Text, Container, Logo } from "components";
+
+import { TextFooter } from "./style";
 import { Props, defaultProps } from "./types";
+import { colors } from "styles";
 
+const Footer = ({ text }: Props) => (
+  <Container
+    display="flex"
+    width={1}
+    px={3}
+    py={[4, 0]}
+    alignItems="center"
+    backgroundColor={colors.black}
+    flexDirection={["column", "row"]}
+    justifyContent={["center", "space-between"]}
+    height={["315px", "100px"]}
+  >
+    <Logo py={[3, 0]} width={161} height={44} />
+    <TextFooter>
+      <Text color={colors.white} py={[3, 0]} fontSize={["16px", "12px"]}>
+        {text}
+      </Text>
+    </TextFooter>
+    <div>{/**/}</div>
+  </Container>
+);
 
-const Footer = ({ text }: Props): ReactElement => {
-    return (
-        <BoxFooter>
-            <FooterItems>
-                <Logo width={161} height={44} />
-                <TextFooter>
-                    <Text>{text}</Text>
-                </TextFooter>
-            </FooterItems>
-        </BoxFooter>
-    )
-}
-
-Footer.defaultProps = defaultProps
+Footer.defaultProps = defaultProps;
 
 export { Footer };
