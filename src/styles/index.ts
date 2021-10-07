92b9a5579f579cfbec64b8f96a81e66ae63413db
jorgeh71218 <jorge.hidalgo@sevenapps.com.br>
2021-10-07 18:08:57 +0300

    fix(files): new values for colors, new styles, removed unnecessary imports

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,6 +1,19 @@
-import { colors } from './colors'
-import * as metrics from './metrics'
-import { globalStyles, fonts, breakpoints, breakpointKeys } from './globalStyles'
+import { colors } from "./colors";
+import * as metrics from "./metrics";
+import {
+  globalStyles,
+  fonts,
+  breakpoints,
+  breakpointKeys,
+} from "./globalStyles";
 
-const theme = { dark: colors, light: colors, ...metrics }
-export { theme, colors, metrics, globalStyles, breakpoints, breakpointKeys, fonts }
+const theme = { dark: colors, light: colors, ...metrics };
+export {
+  theme,
+  colors,
+  metrics,
+  globalStyles,
+  breakpoints,
+  breakpointKeys,
+  fonts,
+};
