b7c23ceeff8365daff581b052d59dd4c7afbe7ce
biancasilva <bianca@fanhero.com>
2021-10-11 10:25:56 -0300

    feat(cardContainer): add gstyle for cardContainer atom

---
+++ src/components/atoms/cardContainer/style.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+	padding: 2rem 3rem;
+	mix-blend-mode: normal;
+	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+	border-radius: 8px;
+	/* TO DO: Add color variable for theme-dark or theme-light */
+	background-color: #222222;
+`
