5326fb7f4e4fd82f271ead78da454e550ac38c44
Bianca <biasilvabsi11@gmail.com>
2021-10-11 16:25:39 -0300

    feature(cardContainer): add styled-system props

--- src/components/atoms/cardContainer/style.tsx
+++ src/components/atoms/cardContainer/style.tsx
@@ -1,10 +1,17 @@
 import styled from "styled-components"
+import { Props } from "./types"
+import { space, layout, flexbox, position } from "styled-system";
 
-export const Container = styled.div`
-	padding: 2rem 3rem;
+export const Container = styled.div<Props>`
+	${space}
+	${layout}
+	${flexbox}
+	${position}
+
 	mix-blend-mode: normal;
 	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 	border-radius: 8px;
+	
 	/* TO DO: Add color variable for theme-dark or theme-light */
 	background-color: #222222;
 `
