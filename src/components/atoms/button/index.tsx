7414d48f420f5d8072f786a5788040bea15ff765
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-10-12 15:18:51 -0300

    fix(button): add icon in type billboard button

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -1,15 +1,19 @@
 import { ButtonProps, defaultProps } from './types';
 import { BoxButton } from './style';
+import { Icon } from '@iconify/react-with-api';
 
-const Button = ({ label, type, ...props }: ButtonProps) => {
+const Button = ({ label, type, iconName, ...props }: ButtonProps) => {
     const getButtonStyle = () => {
         switch (type) {
             case 'submit':
                 return <BoxButton variant="primary" borderRadius={4} {...props}>{label}</BoxButton>
             case 'reset':
-                return <BoxButton variant="secondary"  borderRadius={4} {...props}>{label}</BoxButton>
+                return <BoxButton variant="secondary" borderRadius={4} {...props}>{label}</BoxButton>
             case 'billboard':
-                return <BoxButton  borderRadius={6} {...props}>{label}</BoxButton>
+                return <BoxButton borderRadius={6} {...props}>
+                    <Icon width={20} icon={`mdi:${iconName}`}></Icon>
+                    {label}
+                </BoxButton>
             default:
                 return <BoxButton variant="primary" {...props}>{label}</BoxButton>
         }
