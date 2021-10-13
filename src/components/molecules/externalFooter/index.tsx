927dcee5dc28d6d54ca1213631f8be7c22297a0d
Bianca <biasilvabsi11@gmail.com>
2021-10-13 11:50:29 -0300

    fix(external-footer): return element directly

--- src/components/molecules/externalFooter/index.tsx
+++ src/components/molecules/externalFooter/index.tsx
@@ -3,19 +3,17 @@ import { Logo, Text } from 'components/atoms'
 import { BoxFooter, FooterItems, TextFooter } from "./style";
 import { Link } from '@chakra-ui/react';
 
-const ExternalFooter = (): ReactElement => {
-    return (
-        <BoxFooter>
-            <FooterItems>
-                <TextFooter>
-                    <Link to="">Terms and Conditions</Link>
-                    <Text mx={1}>&</Text>
-                    <Link to="">Privacy and Policy</Link>
-                </TextFooter>
-                <Logo justifyContent={'right'} mb={[4, 0]} width={161} height={44} />
-            </FooterItems>
-        </BoxFooter>
-    )
-}
+const ExternalFooter = (): ReactElement => (
+    <BoxFooter>
+        <FooterItems>
+            <TextFooter>
+                <Link to="">Terms and Conditions</Link>
+                <Text mx={1}>&</Text>
+                <Link to="">Privacy and Policy</Link>
+            </TextFooter>
+            <Logo justifyContent={'right'} mb={[4, 0]} width={161} height={44} />
+        </FooterItems>
+    </BoxFooter>
+)
 
 export { ExternalFooter };
