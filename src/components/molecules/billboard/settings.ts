4353fab0f32361008435e6560cfdf436c6fb7da0
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-10-13 17:30:11 -0300

    fix(billboard): add new settings

---
+++ src/components/molecules/billboard/settings.ts
@@ -0,0 +1,19 @@
+import { SwiperOptions } from 'swiper'
+
+export const Params: SwiperOptions = {
+    speed: 800,
+    lazy: {
+        loadPrevNext: true,
+        loadPrevNextAmount: 1,
+        loadOnTransitionStart: true
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: '.pagination',
+        clickable: true,
+        bulletElement: 'button'
+    },
+}
