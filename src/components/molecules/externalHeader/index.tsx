cbc1f653b99e7d5fe135a1f2d04017d8e12651e5
Bianca <biasilvabsi11@gmail.com>
2021-10-13 11:27:01 -0300

    fix(external-header): return element directly without 'return'

--- src/components/molecules/externalHeader/index.tsx
+++ src/components/molecules/externalHeader/index.tsx
@@ -2,15 +2,13 @@ import React, { ReactElement } from 'react';
 import { Logo, LanguageSelector } from 'components'
 import { BoxHeader, HeaderItems } from "./style";
 
-const ExternalHeader = (): ReactElement => {
-    return (
-        <BoxHeader>
-            <HeaderItems>
-                <Logo marginRight={4}></Logo>
-                <LanguageSelector></LanguageSelector>
-            </HeaderItems>
-        </BoxHeader>
-    )
-}
+const ExternalHeader = (): ReactElement => (
+    <BoxHeader>
+        <HeaderItems>
+            <Logo marginRight={4}></Logo>
+            <LanguageSelector></LanguageSelector>
+        </HeaderItems>
+    </BoxHeader>
+)
 
 export { ExternalHeader };
