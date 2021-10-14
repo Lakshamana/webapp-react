2589cdd5d3427950a4aea04f57a32012693eb161
Luiz Gustavo <64854850+LGustavos@users.noreply.github.com>
2021-10-14 10:26:59 -0300

    fix(button): remove primitive type

--- src/components/atoms/button/types.ts
+++ src/components/atoms/button/types.ts
@@ -1,10 +1,10 @@
 import { ColorProps, SpaceProps, LayoutProps, TypographyProps, BorderProps, VariantArgs } from 'styled-system';
 
 export interface ButtonProps extends SpaceProps, LayoutProps, TypographyProps, VariantArgs, BorderProps, ColorProps {
-  label: String;
+  label: string;
   style?: React.CSSProperties;
   type?: ButtonType;
-  iconName?: String;
+  iconName?: string;
 }
 
 export const defaultProps = {
