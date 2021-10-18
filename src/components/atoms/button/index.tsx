d80ab5ad85edc8c34e6241bb7d1150a25c60341d
jorgeh71218 <jorge.hidalgo@sevenapps.com.br>
2021-10-18 16:00:35 +0300

    feature(button): formats

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -1,29 +1,47 @@
-import { ButtonProps, defaultProps } from './types';
-import { BoxButton } from './style';
-import { Icon } from '@iconify/react-with-api';
+import { ButtonProps, defaultProps } from "./types";
+import { BoxButton } from "./style";
+import { Icon } from "@iconify/react-with-api";
 
 const Button = ({ label, type, iconName, ...props }: ButtonProps) => {
-    const getButtonStyle = () => {
-        switch (type) {
-            case 'submit':
-                return <BoxButton variant="primary" borderRadius={4} {...props}>{label}</BoxButton>
-            case 'reset':
-                return <BoxButton variant="secondary" borderRadius={4} {...props}>{label}</BoxButton>
-            case 'billboard':
-                return <BoxButton borderRadius={6} {...props}>
-                    <Icon width={20} icon={`mdi:${iconName}`}></Icon>
-                    {label}
-                </BoxButton>
-                return <BoxButton borderRadius={6} {...props}>{label}</BoxButton>
-            case 'disabled':
-                return <BoxButton variant="grey" borderRadius={4} {...props}>{label}</BoxButton>
-            default:
-                return <BoxButton variant="primary" {...props}>{label}</BoxButton>
-        }
+  const getButtonStyle = () => {
+    switch (type) {
+      case "submit":
+        return (
+          <BoxButton variant="primary" borderRadius={4} {...props}>
+            {label}
+          </BoxButton>
+        );
+      case "reset":
+        return (
+          <BoxButton variant="secondary" borderRadius={4} {...props}>
+            {label}
+          </BoxButton>
+        );
+      case "billboard":
+        return (
+          <BoxButton borderRadius={6} {...props}>
+            <Icon width={20} icon={`mdi:${iconName}`}></Icon>
+            {label}
+          </BoxButton>
+        );
+
+      case "disabled":
+        return (
+          <BoxButton variant="grey" borderRadius={4} {...props}>
+            {label}
+          </BoxButton>
+        );
+      default:
+        return (
+          <BoxButton variant="primary" {...props}>
+            {label}
+          </BoxButton>
+        );
     }
-    return getButtonStyle();
+  };
+  return getButtonStyle();
 };
 
-Button.defaultProps = defaultProps
+Button.defaultProps = defaultProps;
 
-export { Button }
+export { Button };
