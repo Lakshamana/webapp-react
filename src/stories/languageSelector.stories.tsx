e0f2109f308f648e9e31605a366b2a7dbf2db417
Yuri <yuri.mustifaga@sevenapps.com.br>
2021-10-19 21:12:17 -0300

    feature: implements initial config i18n

--- src/stories/languageSelector.stories.tsx
+++ src/stories/languageSelector.stories.tsx
@@ -1,19 +1,21 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { LanguageSelector } from 'components';
+import { LanguageSelector } from "components";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-    title: 'WEBAPP/LanguageSelector',
-    component: LanguageSelector,
-    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-    //   argTypes: {
-    //     backgroundColor: { control: 'color' },
-    //   },
+  title: "WEBAPP/LanguageSelector",
+  component: LanguageSelector,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  //   argTypes: {
+  //     backgroundColor: { control: 'color' },
+  //   },
 } as ComponentMeta<typeof LanguageSelector>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof LanguageSelector> = (args) => <LanguageSelector {...args} />;
+const Template: ComponentStory<typeof LanguageSelector> = (args: any) => (
+  <LanguageSelector {...args} />
+);
 
 export const Component = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
