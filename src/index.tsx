0b3c492fe5bc6715ebbebc1744ed855060d053f3
biancasilva <bianca@fanhero.com>
2021-10-22 14:08:40 -0300

    fix(index): Change client import name

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,8 @@ import { TemplateProvider } from 'components/templates'
 import reportWebVitals from "./reportWebVitals"
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
-import { client } from "services/api/config"
+import { Client } from "services/api"
 import { ApolloProvider } from "@apollo/client"
-
 
 Sentry.init({
 	dsn: process.env.REACT_APP_SENTRY_DSN,
@@ -20,7 +19,7 @@ Sentry.init({
 ReactDOM.render(
 	<React.StrictMode>
 		<TemplateProvider>
-			<ApolloProvider client={client}>
+			<ApolloProvider client={Client}>
 				<App />
 			</ApolloProvider>
 		</TemplateProvider>
@@ -28,7 +27,4 @@ ReactDOM.render(
 	document.getElementById("root")
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
