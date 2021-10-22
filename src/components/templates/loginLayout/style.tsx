004a9ee70af8af0767198b0bdab86cc9da5f3614
biancasilva <bianca@fanhero.com>
2021-10-22 14:05:17 -0300

    fix(loginLayout): Fix style

--- src/components/templates/loginLayout/style.tsx
+++ src/components/templates/loginLayout/style.tsx
@@ -1,16 +1,19 @@
 import styled from "styled-components";
 import { Props } from "./types"
-import { StyleContainer } from "components";
 import { breakpoints } from 'styles';
+import { StyleContainer } from "components";
 
 export const ChildContainer = styled(StyleContainer) <Props>`
     display: flex;
-    min-height: calc(100vh - 108px - 100px);
+    height: 100%;
+    min-height: 770px;
     background: ${(props: Props) => (props.backgroundImage ? `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.8)), url(${props.backgroundImage})` : '')} center/cover fixed;
+    overflow: auto;
 
     @media screen and (max-width: ${breakpoints.md}) {
-        min-height: auto;
-    }
+      height: auto;
+      min-height: auto;
+  }
 `;
 
 export const LayoutContainer = styled(StyleContainer)`
