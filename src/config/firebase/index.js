714a33cf271846e548ed89d69f6102433a2ee99a
Yuri <yuri.mustifaga@sevenapps.com.br>
2021-10-25 15:51:31 -0300

    fix: change the env name in .env integration

--- src/config/firebase/index.js
+++ src/config/firebase/index.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "@firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.WEBAPP_FIREBASE_KEY,
-  authDomain: process.env.WEBAPP_FIREBASE_DOMAIN,
-  databaseURL: process.env.WEBAPP_FIREBASE_BASE_URL,
-  projectId: process.env.WEBAPP_FIREBASE_PROJECT,
-  storageBucket: process.env.WEBAPP_FIREBASE_BUCKET,
-  messagingSenderId: process.env.WEBAPP_FIREBASE_SENDER,
-  appId: process.env.WEBAPP_FIREBASE_APP,
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_BASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT,
+  storageBucket: process.env.REACT_APP_FIREBASE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER,
+  appId: process.env.REACT_APP_FIREBASE_APP,
 };
 
 const firebase = initializeApp(firebaseConfig);
