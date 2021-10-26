01913a760134cc368b5a52ea74086447d334d83b
Marcus Vinicius Sgorla Franklin <kiinho@gmail.com>
2021-10-26 14:51:41 -0300

    fix(videojs): separete default configs in separate file

--- src/components/molecules/vodWrapper/index.tsx
+++ src/components/molecules/vodWrapper/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useRef } from 'react'
+import { ReactElement, useRef } from 'react'
 
 import VideoJS from "components/molecules/videoJs"
 
@@ -16,6 +16,7 @@ import '@silvermine/videojs-chromecast/dist/silvermine-videojs-chromecast.css'
 import 'videojs-mux'
 
 import { VODWrapperProps } from './types'
+import { getDefaultConfigs } from './settings'
 
 const VODWrapper = ({ 
   src, 
@@ -29,55 +30,7 @@ const VODWrapper = ({
 
   const playerRef = useRef(null);
 
-  const defaultOptions = { // lookup the options in the docs for more options1
-    autoplay: true,
-    controls: true,
-    muted: false,
-    responsive: true,
-    fill: true,
-    fluid: true,
-    preload: 'auto',
-    preloadWebComponents: true,
-    techOrder: ['chromecast', 'html5'],
-    html5: {
-      hls: {
-        enableLowInitialPlaylist: true,
-        smoothQualityChange: true,
-        overrideNative: true,
-      },
-    },
-    playbackRates: [0.5, 1, 1.5, 2],
-    plugins: {
-      hlsQualitySelector: {
-          vjsIconClass: 'vjs-icon-hd',
-          displayCurrentQuality: true,
-      },
-      mux: {
-        data: {
-          env_key:  'f79842543033c226c5d396a7d',
-          viewer_user_id: 'viewer_user_id',
-          video_id: 'video_id',
-          video_title: title || 'title',
-          video_series: 'series',
-          player_name: 'Clappr-ContentVideo',
-          player_init_time: Date.now(),
-          video_stream_type: 'on-demand',
-          ...muxConfig,
-        }
-      },
-      chromecast: {
-        appId: "10AE2759",
-        metadata: {
-          title: title || "Title",
-          subtitle: subtitle || "Subtitle"
-        }
-      },
-    },
-    sources: [{
-      src,
-      type: 'application/x-mpegURL'
-    }]
-  }
+  const defaultOptions = getDefaultConfigs(src, muxConfig, title, subtitle)
 
   const handlePlayerReady = (player: any) => {
     playerRef.current = player;
