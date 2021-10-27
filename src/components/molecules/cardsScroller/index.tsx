eb6aaa0e73c35c01f9a8f3eee93a6df7eb2f538a
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-10-27 16:27:28 -0300

    fix(cards-scroller): change breakpoints to swiper and add new parameter slidesOffsetAfter

--- src/components/molecules/cardsScroller/index.tsx
+++ src/components/molecules/cardsScroller/index.tsx
@@ -1,64 +1,59 @@
-import React from 'react';
-import { Content } from './style';
-import './style.css';
-import { PostsProps } from './types';
-import { Swiper } from 'swiper/react';
-import SwiperCore, { Navigation, SwiperOptions } from 'swiper';
-import 'swiper/swiper-bundle.min.css';
+import React from "react"
+import { Content } from "./style"
+import "./style.css"
+import { PostsProps } from "./types"
+import { Swiper } from "swiper/react"
+import SwiperCore, { Navigation, SwiperOptions } from "swiper"
+import "swiper/swiper-bundle.min.css"
 
 SwiperCore.use([Navigation])
 
 const CardsScroller = ({ children }: PostsProps) => {
-  const Params: SwiperOptions = {
-    spaceBetween: 10,
-    navigation: {
-      prevEl: '.swiper-button-prev',
-      nextEl: '.swiper-button-next'
-    },
-    speed: 800,
-    setWrapperSize: true,
-    roundLengths: false,
-    watchOverflow: false,
-    touchMoveStopPropagation: true,
-    touchStartPreventDefault: true,
-    freeModeMomentum: false,
-    preloadImages: false,
-    preventClicks: false,
-    freeModeMomentumBounce: false,
-    breakpoints: {
-      0: {
-        slidesPerView: 1
-      },
-      600: {
-        slidesPerView: 2
-      },
-      800: {
-        slidesPerView: 3
-      },
-      900: {
-        slidesPerView: 4
-      },
-      1100: {
-        slidesPerView: 5
-      },
-      2000: {
-        slidesPerView: 6
-      }
-    },
-  }
+	const Params: SwiperOptions = {
+		spaceBetween: 10,
+		navigation: {
+			prevEl: ".swiper-button-prev",
+			nextEl: ".swiper-button-next"
+		},
+		speed: 800,
+		setWrapperSize: true,
+		roundLengths: false,
+		watchOverflow: true,
+		touchMoveStopPropagation: true,
+		touchStartPreventDefault: true,
+		freeModeMomentum: false,
+		preloadImages: false,
+		preventClicks: false,
+		freeModeMomentumBounce: false,
+		slidesOffsetAfter: 0,
+		breakpoints: {
+			1440: {
+				slidesPerView: 5
+			},
+			1024: {
+				slidesPerView: 4
+			},
+			768: {
+				slidesPerView: 3
+			},
+			425: {
+				slidesPerView: 2
+			},
+			320: {
+				slidesPerView: 1
+			}
+		}
+	}
 
-  return (
-    <React.Fragment>
-      <Content>
-        <Swiper {...Params}>
-          {children}
-        </Swiper>
-        <div className='swiper-button-prev' />
-        <div className='swiper-button-next' />
-      </Content>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<Content>
+				<Swiper {...Params}>{children}</Swiper>
+				<div className='swiper-button-prev' />
+				<div className='swiper-button-next' />
+			</Content>
+		</React.Fragment>
+	)
 }
-
 
 export { CardsScroller }
