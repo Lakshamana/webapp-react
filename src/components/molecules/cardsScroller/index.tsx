a575d4d08af9c4e5e30793bac6578f7622e2aece
biancasilva <bianca@fanhero.com>
2021-10-27 10:54:50 -0300

    fix(cards-scroller):  fix config and separate to a settings file

--- src/components/molecules/cardsScroller/index.tsx
+++ src/components/molecules/cardsScroller/index.tsx
@@ -3,54 +3,17 @@ import { Content } from './style';
 import './style.css';
 import { PostsProps } from './types';
 import { Swiper } from 'swiper/react';
-import SwiperCore, { Navigation, SwiperOptions } from 'swiper';
+import SwiperCore, { Navigation } from 'swiper';
 import 'swiper/swiper-bundle.min.css';
+import { SWIPPER_PARAMS } from './settings'
 
 SwiperCore.use([Navigation])
 
 const CardsScroller = ({ children }: PostsProps) => {
-  const Params: SwiperOptions = {
-    spaceBetween: 10,
-    navigation: {
-      prevEl: '.swiper-button-prev',
-      nextEl: '.swiper-button-next'
-    },
-    speed: 800,
-    setWrapperSize: true,
-    roundLengths: false,
-    watchOverflow: false,
-    touchMoveStopPropagation: true,
-    touchStartPreventDefault: true,
-    freeModeMomentum: false,
-    preloadImages: false,
-    preventClicks: false,
-    freeModeMomentumBounce: false,
-    breakpoints: {
-      0: {
-        slidesPerView: 1
-      },
-      600: {
-        slidesPerView: 2
-      },
-      800: {
-        slidesPerView: 3
-      },
-      900: {
-        slidesPerView: 4
-      },
-      1100: {
-        slidesPerView: 5
-      },
-      2000: {
-        slidesPerView: 6
-      }
-    },
-  }
-
   return (
     <React.Fragment>
       <Content>
-        <Swiper {...Params}>
+        <Swiper {...SWIPPER_PARAMS}>
           {children}
         </Swiper>
         <div className='swiper-button-prev' />
