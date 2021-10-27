ffbbae1e072673691d05c66f5d52c0b399a99563
biancasilva <bianca@fanhero.com>
2021-10-27 10:59:10 -0300

    fix(home): Fix space between scrollers

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,5 +1,6 @@
 import { MainLayout, Container } from "components";
 import { BillboardScroller, CollectionScroller, LivestreamScroller, OnDemandScroller } from "components/molecules"
+import { Flex } from '@chakra-ui/layout'
 import billboardData from "./billboard.json"
 import liveStreamsData from "./livestreams.json"
 import collectionsData from "./collections.json"
@@ -7,11 +8,13 @@ import onDemandData from "./onDemand.json"
 
 const HomePage = () => (
   <MainLayout>
-    <Container flexDirection="column">
+    <Container flexDirection={"column"} display={"flex"}>
       <BillboardScroller items={billboardData}></BillboardScroller>
-      <LivestreamScroller items={liveStreamsData} sectionTitle={'Ao vivo'}></LivestreamScroller>
-      <OnDemandScroller items={onDemandData} sectionTitle={'Destaques'}></OnDemandScroller>
-      <CollectionScroller items={collectionsData} sectionTitle={'Categorias'}></CollectionScroller>
+      <Flex pb={10} gridGap={10} flexDirection={"column"}>
+        <LivestreamScroller items={liveStreamsData} sectionTitle={'Ao vivo'}></LivestreamScroller>
+        <OnDemandScroller items={onDemandData} sectionTitle={'Destaques'}></OnDemandScroller>
+        <CollectionScroller items={collectionsData} sectionTitle={'Categorias'}></CollectionScroller>
+      </Flex>
     </Container>
   </MainLayout>
 )
