fa39e1122e21113176ad59d493982ef40981466a
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-10-28 14:10:24 -0300

    fix(gridCards): fix style from grid cards

--- src/components/molecules/gridCards/style.tsx
+++ src/components/molecules/gridCards/style.tsx
@@ -1,27 +1,29 @@
-import styled from 'styled-components';
-import { Props } from './types'
-import { breakpoints } from 'styles';
+import styled from "styled-components"
+import { Props } from "./types"
+import { breakpoints } from "styles"
 
 export const GridContent = styled.div<Props>`
-  width: 100%;
-  display: grid;
-  ${(props: Props) => `grid-template-columns: repeat(${props.xl}, 1fr);`}
-  ${(props: Props) => `grid-column-gap: ${props.columnGap}px;`}
-  ${(props: Props) => `grid-row-gap: ${props.rowGap}px;`}
+	${(props: Props) => `
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(${props.xl}, 1fr);
+    grid-column-gap: ${props.columnGap}px;
+    grid-row-gap: ${props.rowGap}px;
 
-  @media screen and (max-width: ${breakpoints.lg}) {
-    ${(props: Props) => `grid-template-columns: repeat(${props.lg}, 1fr);`}
-  }
+    @media screen and (max-width: ${breakpoints.lg}) {
+    grid-template-columns: repeat(${props.lg}, 1fr);
+    }
 
-  @media screen and (max-width: ${breakpoints.md}) {
-    ${(props: Props) => `grid-template-columns: repeat(${props.md}, 1fr);`}
-  }
+    @media screen and (max-width: ${breakpoints.md}) {
+    grid-template-columns: repeat(${props.md}, 1fr);
+    }
 
-  @media screen and (max-width: ${breakpoints.sm}) {
-    ${(props: Props) => `grid-template-columns: repeat(${props.sm}, 1fr);`}
-  }
+    @media screen and (max-width: ${breakpoints.sm}) {
+    grid-template-columns: repeat(${props.sm}, 1fr);
+    }
 
-  @media screen and (max-width: ${breakpoints.ssm}) {
-    ${(props: Props) => `grid-template-columns: repeat(${props.ssm}, 1fr);`}
-  }
+    @media screen and (max-width: ${breakpoints.ssm}) {
+    grid-template-columns: repeat(${props.ssm}, 1fr);
+    }
+  `}
 `
