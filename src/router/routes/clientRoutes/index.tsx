b86a41f40940d71375ece82da00273036de70143
Marcus Vinicius Sgorla Franklin <kiinho@gmail.com>
2021-10-29 19:15:19 -0300

    chore: resolve conflict to update branch

--- src/router/routes/clientRoutes/index.tsx
+++ src/router/routes/clientRoutes/index.tsx
@@ -2,11 +2,7 @@ import { Switch } from "react-router-dom";
 
 import { ClientRoute } from "../../components";
 
-<<<<<<< HEAD
-import { HomePage, LoginPage, LiveChat, User, SignupPage } from "modules";
-=======
-import { DashboardPage, LoginPage, LiveChat, User, SignupPage, PlayerPage } from "modules";
->>>>>>> 8b24c7e... chore: adjust conflict to add page
+import { HomePage, LoginPage, LiveChat, User, SignupPage, PlayerPage } from "modules";
 
 const ClientRoutes = ({ isAccesible }: any) => (
   <Switch>
