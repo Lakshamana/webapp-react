4763002c32f452af321ee0f87a0d191ed4ee7969
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-10-29 13:50:59 -0300

    fix(collectionScroller): moved map function to collectionItems

--- src/components/molecules/collectionScroller/index.tsx
+++ src/components/molecules/collectionScroller/index.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import { CollectionPostCard, CardsScroller } from "components"
-import { SwiperSlide } from "swiper/react"
+import { CardsScroller } from "components"
+import { CollectionList } from "./collectionItems"
 import { CollectionScrollerProps } from "./types"
 import { Header, ContentScroller } from "./style"
 import { Text } from "components"
@@ -12,51 +12,34 @@ const CollectionScroller = ({
 	sectionUrl,
 	hasMoreLink
 }: CollectionScrollerProps) => {
-	let slides = items?.map((post) => {
-		return (
-			<SwiperSlide key={`slide-${post.id}-collection`}>
-				<CollectionPostCard
-					id={post.id}
-					collectionTitle={post.collectionTitle}
-					collectionUrl={post.collectionUrl}
-					coverImage={post.coverImage}
-					isNew={post.isNew}
-					newTag={post.newTag}
-					isExclusive={post.isExclusive}
-					isGeolocked={post.isGeolocked}
-				/>
-			</SwiperSlide>
-		)
-	})
-
 	return (
-		<>
-			<ContentScroller>
-				<Header>
-					<Text
-						color={"#FFFFFF"}
-						fontSize={"28px"}
-						fontWeight={500}
-						marginRight={"18px"}
-						lineHeight={"34px"}>
-						{sectionTitle || ""}
-					</Text>
-					{hasMoreLink ? (
-						<Link
-							color={"#0460FF"}
-							fontSize={"20px"}
-							fontWeight={400}
-							textDecoration={"underline"}
-							to={sectionUrl}>
-							more
-						</Link>
-					) : (
-						""
-					)}
-				</Header>
-				<CardsScroller children={slides} type='collection' />
-			</ContentScroller>
-		</>
+		<ContentScroller>
+			<Header>
+				<Text
+					color={"#FFFFFF"}
+					fontSize={"28px"}
+					fontWeight={500}
+					marginRight={"18px"}
+					lineHeight={"34px"}>
+					{sectionTitle || ""}
+				</Text>
+				{hasMoreLink ? (
+					<Link
+						color={"#0460FF"}
+						fontSize={"20px"}
+						fontWeight={400}
+						textDecoration={"underline"}
+						to={sectionUrl}>
+						more
+					</Link>
+				) : (
+					""
+				)}
+			</Header>
+			<CardsScroller type='collection'>
+				<CollectionList items={items} />
+			</CardsScroller>
+		</ContentScroller>
 	)
 }
 
