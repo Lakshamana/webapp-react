75996cdf53c6864f39d79eb52954230d40d0ce54
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-10-29 11:36:49 -0300

    fix(collection-scroller): change size from text and add new type property in cards scroller

--- src/components/molecules/collectionScroller/index.tsx
+++ src/components/molecules/collectionScroller/index.tsx
@@ -1,37 +1,63 @@
-import React from 'react';
-import { CollectionPostCard, CardsScroller } from 'components';
-import { SwiperSlide } from 'swiper/react';
-import { CollectionScrollerProps } from './types';
-import { Header, ContentScroller } from './style'
-import { Text } from 'components'
-import { Link } from '@chakra-ui/react'
+import React from "react"
+import { CollectionPostCard, CardsScroller } from "components"
+import { SwiperSlide } from "swiper/react"
+import { CollectionScrollerProps } from "./types"
+import { Header, ContentScroller } from "./style"
+import { Text } from "components"
+import { Link } from "@chakra-ui/react"
 
-const CollectionScroller = ({ items, sectionTitle, sectionUrl, hasMoreLink }: CollectionScrollerProps) => {
+const CollectionScroller = ({
+	items,
+	sectionTitle,
+	sectionUrl,
+	hasMoreLink
+}: CollectionScrollerProps) => {
+	let slides = items?.map((post) => {
+		return (
+			<SwiperSlide key={`slide-${post.id}-collection`}>
+				<CollectionPostCard
+					id={post.id}
+					collectionTitle={post.collectionTitle}
+					collectionUrl={post.collectionUrl}
+					coverImage={post.coverImage}
+					isNew={post.isNew}
+					newTag={post.newTag}
+					isExclusive={post.isExclusive}
+					isGeolocked={post.isGeolocked}
+				/>
+			</SwiperSlide>
+		)
+	})
 
-    let slides = items?.map(post=> {
-        return (<SwiperSlide key={`slide-${post.id}-collection`}>
-            <CollectionPostCard
-                id={post.id}
-                collectionTitle={post.collectionTitle}
-                collectionUrl={post.collectionUrl}
-                coverImage={post.coverImage}
-                isNew={post.isNew}
-                newTag={post.newTag}
-                isExclusive={post.isExclusive}
-                isGeolocked={post.isGeolocked}
-            />
-        </SwiperSlide>)
-    })
-
-    return (<>
-        <ContentScroller>
-            <Header>
-                <Text color={"#FFFFFF"} fontSize={"22px"} fontWeight={500} marginRight={"18px"}>{sectionTitle || ''}</Text>
-                {hasMoreLink ? <Link color={"#0460FF"} fontSize={"16px"} fontWeight={400} textDecoration={"none"} to={sectionUrl}>mais</Link> : ''}
-            </Header>
-            <CardsScroller children={slides} />
-        </ContentScroller>
-    </>)
+	return (
+		<>
+			<ContentScroller>
+				<Header>
+					<Text
+						color={"#FFFFFF"}
+						fontSize={"28px"}
+						fontWeight={500}
+						marginRight={"18px"}
+						lineHeight={"34px"}>
+						{sectionTitle || ""}
+					</Text>
+					{hasMoreLink ? (
+						<Link
+							color={"#0460FF"}
+							fontSize={"20px"}
+							fontWeight={400}
+							textDecoration={"underline"}
+							to={sectionUrl}>
+							more
+						</Link>
+					) : (
+						""
+					)}
+				</Header>
+				<CardsScroller children={slides} type='collection' />
+			</ContentScroller>
+		</>
+	)
 }
 
 export { CollectionScroller }
