4849e030418ab265c64223eb190b68d216d245e1
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-10-29 13:48:54 -0300

    fix(ondemandScroller): moved map function to ondemandItems

--- src/components/molecules/onDemandScroller/index.tsx
+++ src/components/molecules/onDemandScroller/index.tsx
@@ -1,37 +1,48 @@
-import React from 'react';
-import { VideoPostCard, CardsScroller } from 'components';
-import { SwiperSlide } from 'swiper/react';
-import { VideoScrollerProps } from './types';
-import { Header, ContentScroller } from './styles'
-import { Text } from 'components'
-import { Link } from '@chakra-ui/react'
+import React from "react"
+import { Link } from "@chakra-ui/react"
+import { CardsScroller } from "components"
+import { OndemandList } from "./ondemandItems"
+import { Text } from "components"
+import { VideoScrollerProps } from "./types"
+import { Header, ContentScroller } from "./styles"
 
-const OnDemandScroller = ({ items, sectionTitle, sectionUrl, hasMoreLink }: VideoScrollerProps) => {
-
-    let slides = items?.map(post => {
-        return (<SwiperSlide key={`slide-${post.id}-ondemand`}>
-            <VideoPostCard
-                id={post.id}
-                postTitle={post.postTitle}
-                postUrl={post.postUrl}
-                coverImage={post.coverImage}
-                mediaLength={post.mediaLength}
-                views={post.views}
-                isExclusive={post.isExclusive}
-                isGeolocked={post.isGeolocked}
-            />
-        </SwiperSlide>)
-    })
-
-    return (<>
-        <ContentScroller>
-            <Header>
-                <Text color={"#FFFFFF"} fontSize={"28px"} fontWeight={500} marginRight={"18px"} lineHeight={'34px'}>{sectionTitle || ''}</Text>
-                {hasMoreLink ? <Link color={"#0460FF"} fontSize={"20px"} fontWeight={400} textDecoration={'underline'} to={sectionUrl}>more</Link> : ''}
-            </Header>
-            <CardsScroller children={slides} type='ondemand'/>
-        </ContentScroller>
-    </>)
+const OnDemandScroller = ({
+	items,
+	sectionTitle,
+	sectionUrl,
+	hasMoreLink
+}: VideoScrollerProps) => {
+	return (
+		<>
+			<ContentScroller>
+				<Header>
+					<Text
+						color={"#FFFFFF"}
+						fontSize={"28px"}
+						fontWeight={500}
+						marginRight={"18px"}
+						lineHeight={"34px"}>
+						{sectionTitle || ""}
+					</Text>
+					{hasMoreLink ? (
+						<Link
+							color={"#0460FF"}
+							fontSize={"20px"}
+							fontWeight={400}
+							textDecoration={"underline"}
+							to={sectionUrl}>
+							more
+						</Link>
+					) : (
+						""
+					)}
+				</Header>
+				<CardsScroller type='ondemand'>
+					<OndemandList items={items} />
+				</CardsScroller>
+			</ContentScroller>
+		</>
+	)
 }
 
 export { OnDemandScroller }
