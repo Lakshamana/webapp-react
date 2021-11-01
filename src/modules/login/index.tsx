66fb7da94905cb930ad6be601816acc2e792c0cb
biancasilva <bianca@fanhero.com>
2021-11-01 19:17:03 -0300

    feat(login): Add translation strings and set card width

--- src/modules/login/index.tsx
+++ src/modules/login/index.tsx
@@ -2,30 +2,35 @@ import { Text, LoginLayout, CardContainer, SocialSigninButton, SigninForm } from
 import { Link } from 'react-router-dom';
 import { Container } from './styles';
 import { Flex } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next'
+import { sizes } from 'styles'
 
-const LoginPage = () => (
-  <LoginLayout>
-    <Container py={[0, 10]}>
-      <CardContainer paddingX={[30, 60]} width={[ 1, 550 ]}>
-        <Text fontSize={24} textAlign={'center'} fontWeight={'bolder'} color={'white'}>Access FanHero now</Text>
-        <Text fontSize={16} paddingTop={10} textAlign={'center'} color={'#A4A4A4'}>Please login into your account now</Text>
-        <Flex gridGap={7} marginY={30} justifyContent={'center'}>
-          <SocialSigninButton type={'facebook'}></SocialSigninButton>
-          <SocialSigninButton type={'google'}></SocialSigninButton>
-          <SocialSigninButton type={'apple'}></SocialSigninButton>
-          <SocialSigninButton type={'linkedin'}></SocialSigninButton>
-        </Flex>
-        <Text fontSize={16} textAlign={'center'} color={'#A4A4A4'}>or</Text>
-        <SigninForm></SigninForm>
-        <Flex marginTop={10} marginBottom={5} justifyContent={'center'}>
-          <Text color={'white'} paddingRight={1}>
-            Doesn't have an account?
-          </Text>
-          <Link to="signup">Sign Up here</Link>
-        </Flex>
-      </CardContainer>
-    </Container>
-  </LoginLayout >
-);
+const LoginPage = () => {
+  const { t } = useTranslation()
+  return (
+    <LoginLayout>
+      <Container py={[0, 10]}>
+        <CardContainer paddingX={[30, 60]} width={[1, sizes.loginCardWidth]}>
+          <Text fontSize={24} textAlign={'center'} fontWeight={'bolder'} color={'white'}>{t('signin.title')}</Text>
+          <Text fontSize={16} paddingTop={10} textAlign={'center'} color={'#A4A4A4'}>{t('signin.subtitle')}</Text>
+          <Flex gridGap={7} marginY={30} justifyContent={'center'}>
+            <SocialSigninButton type={'facebook'}></SocialSigninButton>
+            <SocialSigninButton type={'google'}></SocialSigninButton>
+            <SocialSigninButton type={'apple'}></SocialSigninButton>
+            <SocialSigninButton type={'linkedin'}></SocialSigninButton>
+          </Flex>
+          <Text fontSize={16} textAlign={'center'} color={'#A4A4A4'}>or</Text>
+          <SigninForm></SigninForm>
+          <Flex marginTop={10} marginBottom={5} justifyContent={'center'}>
+            <Text color={'white'} paddingRight={1}>
+              {t('signin.label.dont_have_account')}
+            </Text>
+            <Link to="signup">{t('signin.actions.signup_here')}</Link>
+          </Flex>
+        </CardContainer>
+      </Container>
+    </LoginLayout >
+  )
+};
 
 export { LoginPage }
