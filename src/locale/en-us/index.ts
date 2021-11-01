e386fa0c8dfe5c0bbfb41a26e0c415d0aa8f46da
biancasilva <bianca@fanhero.com>
2021-11-01 19:18:05 -0300

    feat(locale): Add new locale strings

--- src/locale/en-us/index.ts
+++ src/locale/en-us/index.ts
@@ -1,25 +1,29 @@
 const EN_US = {
-  translations: {
-    common: {
-      terms: 'Terms and Conditions',
-      privacy: 'Privacy and Policy'
-    },
-    signin: {
-      title: 'Access Fanhero now',
-      subtitle: 'Please login into your account now',
-      or: 'or',
-      label: {
-        email: 'Email',
-        password: 'Password',
-        dont_have_account: "Doesn't have an account?"
-      },
-      actions: {
-        login: 'Login',
-        signup_here: 'Sign Up here',
-        forgot_password: 'Forgot password?'
-      }
-    }
-  },
-};
+	translations: {
+		common: {
+			terms: "Terms and Conditions",
+			privacy: "Privacy and Policy"
+		},
+		signin: {
+			title: "Access Fanhero now",
+			subtitle: "Please login into your account now",
+			or: "or",
+			label: {
+				email: "Email",
+				password: "Password",
+				dont_have_account: "Doesn't have an account?",
+				save_as_default: "Save as default"
+			},
+			actions: {
+				login: "Login",
+				signup_here: "Sign Up here",
+				forgot_password: "Forgot password?"
+			},
+			error: {
+				field_required: 'Field {{field_name}} is required',
+			}
+		}
+	}
+}
 
-export default EN_US;
+export default EN_US
--- src/locale/pt-br/index.ts
+++ src/locale/pt-br/index.ts
@@ -1,25 +1,29 @@
 const PT_BR = {
-  translations: {
-    common: {
-      terms: 'Termos e Condições',
-      privacy: 'Política de privacidade'
-    },
-    signin: {
-      title: 'Acesse Fanhero agora',
-      subtitle: 'Por favor acesse a sua conta agora',
-      or: 'ou',
-      label: {
-        email: 'Email',
-        password: 'Senha',
-        dont_have_account: "Não tem uma conta?"
-      },
-      actions: {
-        login: 'Entrar',
-        signup_here: 'Registre-se',
-        forgot_password: 'Esqueceu a senha?'
-      }
-    }
-  },
-};
+	translations: {
+		common: {
+			terms: "Termos e Condições",
+			privacy: "Política de privacidade"
+		},
+		signin: {
+			title: "Acesse Fanhero agora",
+			subtitle: "Por favor acesse a sua conta agora",
+			or: "ou",
+			label: {
+				email: "Email",
+				password: "Senha",
+				dont_have_account: "Não tem uma conta?",
+				save_as_default: "Salvar como padrão"
+			},
+			actions: {
+				login: "Entrar",
+				signup_here: "Registre-se",
+				forgot_password: "Esqueceu a senha?"
+			},
+			error: {
+				field_required: 'Campo {{field_name}} é obrigatório',
+			}
+		}
+	}
+}
 
-export default PT_BR;
+export default PT_BR
