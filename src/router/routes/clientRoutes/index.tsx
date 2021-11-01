cf906a2798c00ddc82a93d579dbacfecf6e7b44b
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-11-01 12:33:28 -0300

    feat(client-routes): add new client routes to collections page

--- src/router/routes/clientRoutes/index.tsx
+++ src/router/routes/clientRoutes/index.tsx
@@ -2,7 +2,7 @@ import { Switch } from "react-router-dom";
 
 import { ClientRoute } from "../../components";
 
-import { HomePage, LoginPage, LiveChat, User, SignupPage } from "modules";
+import { HomePage, LoginPage, LiveChat, User, SignupPage, CollectionsPage } from "modules";
 
 const ClientRoutes = ({ isAccesible }: any) => (
   <Switch>
@@ -29,6 +29,12 @@ const ClientRoutes = ({ isAccesible }: any) => (
       path="/signup"
       component={SignupPage}
       fallback={LoginPage}
+    />
+    <ClientRoute
+      isAccesible={isAccesible?.user}
+      path="/collections"
+      component={CollectionsPage}
+      fallback={HomePage}
     />
     <ClientRoute isAccesible={isAccesible?.login} path="/" component={LoginPage} />
   </Switch>
