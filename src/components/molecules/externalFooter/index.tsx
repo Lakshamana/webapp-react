c22448ba3d7e2083e3c46deaa906ad11c41e1c44
biancasilva <bianca@fanhero.com>
2021-11-01 19:22:23 -0300

    feat(footer): Add locale strings

--- src/components/molecules/externalFooter/index.tsx
+++ src/components/molecules/externalFooter/index.tsx
@@ -1,18 +1,22 @@
 import { Link } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next'
 import { Logo, Text } from 'components/atoms'
 import { BoxFooter, FooterItems, TextFooter } from "./style";
 
-const ExternalFooter = () => (
-    <BoxFooter px={4} display={'flex'} alignItems={'center'}>
-        <FooterItems width={1} py={20}>
-            <TextFooter>
-                <Link to="" fontSize={[16]}>Terms and Conditions</Link>
-                <Text mx={1} fontSize={[16]}>&</Text>
-                <Link to="" fontSize={[16]}>Privacy and Policy</Link>
-            </TextFooter>
-            <Logo alignItems={'center'} justifyContent={'right'} mb={[4, 4, 0, 0]} width={161} height={44} />
-        </FooterItems>
-    </BoxFooter>
-)
+const ExternalFooter = () => {
+    const { t } = useTranslation()
+    return (
+        <BoxFooter px={4} display={'flex'} alignItems={'center'}>
+            <FooterItems width={1} py={20}>
+                <TextFooter>
+                    <Link to="" fontSize={[16]}>{t('common.terms')}</Link>
+                    <Text mx={1} fontSize={[16]}>&</Text>
+                    <Link to="" fontSize={[16]}>{t('common.privacy')}</Link>
+                </TextFooter>
+                <Logo alignItems={'center'} justifyContent={'right'} mb={[4, 4, 0, 0]} width={161} height={44} />
+            </FooterItems>
+        </BoxFooter>
+    )
+}
 
 export { ExternalFooter };
