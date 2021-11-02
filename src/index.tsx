eeaa6508609dc114e1712e951b69df87b5d0e301
biancasilva <bianca@fanhero.com>
2021-11-02 15:57:05 -0300

    feat(sentry): Remove Sentry config from index and create a config page

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { TemplateProvider } from "components/templates";
 import reportWebVitals from "./reportWebVitals";
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
+
 import { Client } from "services/api"
 import { ApolloProvider } from "@apollo/client";
 import "./config/firebase";
+import "./config/sentry";
 import "./config/i18n";
 
-Sentry.init({
-  dsn: process.env.REACT_APP_SENTRY_DSN,
-  integrations: [new Integrations.BrowserTracing()],
-  autoSessionTracking: true,
-  debug: true,
-  tracesSampleRate: 1.0,
-});
 
 ReactDOM.render(
 	<React.StrictMode>
