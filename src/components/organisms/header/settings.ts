53240c147be69a2c706a42f38e10d0d679a1c0b5
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-11-03 17:02:56 -0300

    feature(header-settings): replace strings to locale strings

--- src/components/organisms/header/settings.ts
+++ src/components/organisms/header/settings.ts
@@ -1,14 +1,15 @@
 import { Channel } from "./types";
+import i18n from "config/i18n"
 
 export const MENUTABS = [
-  { id: "home", label: "home", url: "/" },
-  { id: "live", label: "live", url: "/" },
-  { id: "feed", label: "feed", url: "/" },
-  { id: "collections", label: "collections", url: "/collections" },
-  { id: "list", label: "my list", url: "/mylist" },
-  { id: "settings", label: "settings", url: "/" },
-  { id: "tools", label: "tools", url: "/" },
-  { id: "example 1", label: "example", url: "/" },
+  { id: "home", label: i18n.t("header.tabs.home"), url: "/" },
+  { id: "live", label: i18n.t("header.tabs.live"), url: "/" },
+  { id: "feed", label: i18n.t("header.tabs.feed"), url: "/" },
+  { id: "collections", label: i18n.t("header.tabs.collections"), url: "/collections" },
+  { id: "list", label: i18n.t("header.tabs.my_list"), url: "/mylist" },
+  { id: "settings", label: i18n.t("header.tabs.settings"), url: "/" },
+  { id: "tools", label: i18n.t("header.tabs.tools"), url: "/" },
+  { id: "example 1", label:  i18n.t("header.tabs.example"), url: "/" },
 ];
 
 export const CHANNELS: Array<Channel> = [
