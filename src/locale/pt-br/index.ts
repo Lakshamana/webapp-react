71430f5f1b84b331f60d709ce396540e0de0ade7
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-11-03 16:56:29 -0300

    feature(locale): add new strings in locale (EN/PT)

--- src/locale/pt-br/index.ts
+++ src/locale/pt-br/index.ts
@@ -2,7 +2,16 @@ const PT_BR = {
   translations: {
     common: {
       terms: 'Termos e Condições',
-      privacy: 'Política de privacidade'
+      privacy: 'Política de privacidade',
+      more: 'mais',
+      edit_profile: 'Editar perfil',
+      settings: 'Ajustes',
+      sign_out: 'Sair',
+      enable: 'Habilitar',
+      disable: 'Desabilitar',
+      dark_mode: 'modo escuro',
+      messages: "menssagens",
+      votes: "votos"
     },
     signin: {
       title: 'Acesse Fanhero agora',
@@ -18,8 +27,43 @@ const PT_BR = {
         signup_here: 'Registre-se',
         forgot_password: 'Esqueceu a senha?'
       }
+    },
+    header: {
+      tabs: {
+        home: 'Home',
+        live: 'Live',
+        feed: 'Feed',
+        collections: 'Collections',
+        my_list: 'My List',
+        settings: 'Settings',
+        tools: 'Tools',
+        example: 'Example'
+      },
+      channel_selected: {
+        select: 'Selecionar',
+        channel: 'canal:'
+      },
+      channel_search: 'Buscar canal...'
+    },
+    page: {
+      home: {
+        live: 'Ao vivo',
+        most_recent: 'Mais Recentes',
+        popular: 'Popular'
+      },
+      collection: {
+        action: 'Ação',
+        drama: 'Drama',
+        family: 'Família',
+        watch_now: 'Assistir agora',
+        my_list: 'Minha Lista'
+      },
+      my_list: {
+        my_list: 'Minha Lista',
+        collection: 'Coleção'
+      }
     }
-  },
+  }
 };
 
 export default PT_BR;
