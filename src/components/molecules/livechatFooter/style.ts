f91d56b42cbbf3bce4832ca904a3c4b072a31e0e
Yuri <yuri.mustifaga@sevenapps.com.br>
2021-11-04 16:22:59 -0300

    feat(livechatFooter): implements style in livechatFooter with Icon reactions and styled-system

---
+++ src/components/molecules/livechatFooter/style.ts
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import { flex, space, layout, flexbox, grid } from "styled-system";
+import { StyledProps } from "./types";
+
+import { Icon } from "@iconify/react";
+
+export const LivechatFooterMain = styled.div<StyledProps>`
+  width: 100%;
+  height: 100px;
+  border-top-width: 1px;
+  border-top-color: #444444;
+  border-top-style: solid;
+  background: #000;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-top: 16px;
+  padding-bottom: 16px;
+  padding-right: 16px;
+`;
+
+export const IconContainer = styled.div<StyledProps>`
+  display: flex;
+  ${flexbox}
+  position: relative;
+`;
+
+export const IconItem = styled.div<StyledProps>``;
+
+export const InputContainer = styled.div<StyledProps>`
+  ${flexbox}
+`;
+
+export const AnimatedIcon = styled(Icon)`
+  //Implements animation emote here
+`;
+
+export const PopoverIcon = styled.div<StyledProps>`
+  display: grid;
+  ${flexbox}
+  ${grid}
+  width: 200px;
+  height: 140px;
+  position: absolute;
+  background: #222;
+  z-index: 99;
+  top: -184px;
+  left: 0px;
+  -webkit-box-shadow: 3px 6px 17px 1px #000000;
+  box-shadow: 3px 6px 17px 1px #000000;
+`;
+
+export const Reaction = styled.div<StyledProps>`
+  ${space}
+  ${layout}
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  color: white;
+  cursor: pointer;
+  align-items: center;
+`;
