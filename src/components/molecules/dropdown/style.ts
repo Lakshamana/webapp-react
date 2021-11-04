1800acbebda7c3c537e4c4482670f7afbe98c29b
Yuri <yuri.mustifaga@sevenapps.com.br>
2021-11-04 15:54:24 -0300

    feat(dropdown): initial implements style dropdown (not finished)

---
+++ src/components/molecules/dropdown/style.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+import { layout } from 'styled-system'
+
+export const Main: any = styled.div`
+  ${layout}
+  border-radius: 200px;
+  background: #e1e1e1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Image: any = styled.img`
+  ${layout}
+  border-radius: 200px;
+`
