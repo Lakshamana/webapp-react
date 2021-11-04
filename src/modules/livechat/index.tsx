4f3e146de9cbde4338ce9ff32db662b1ab25ba67
Yuri <yuri.mustifaga@sevenapps.com.br>
2021-11-04 14:37:29 -0300

    fix: fixes when click in password eye

--- src/modules/livechat/index.tsx
+++ src/modules/livechat/index.tsx
@@ -1,18 +1,25 @@
 import { Container, MainLayout } from "components";
-import { VideoRelated } from "components/molecules";
+import { Input, VideoRelated } from "components/molecules";
+import { useState } from "react";
 
 import { dataVideoRelated } from "./mock";
 
 const LiveChat = () => {
+  const [value, setValue] = useState("");
   return (
     <MainLayout>
       <Container flexDirection="column" width={["100%"]} minHeight={["100vh"]}>
-        <VideoRelated
+        <Input
+          type="password"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        {/* <VideoRelated
           options={dataVideoRelated}
           initialAutoplayValue={true}
           onAutoplay={() => {}}
           onSelect={() => {}}
-        />
+        /> */}
       </Container>
     </MainLayout>
   );
