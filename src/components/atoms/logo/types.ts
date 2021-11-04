95b8b5cb9f91879388f86ba98f7294eede5e6cc5
jorgeh71218 <jorge.hidalgo@sevenapps.com.br>
2021-11-04 22:06:17 +0300

    fix(logo): new props and svg for logo use

--- src/components/atoms/logo/types.ts
+++ src/components/atoms/logo/types.ts
@@ -3,9 +3,11 @@ import { LayoutProps, SpaceProps, FlexboxProps } from "styled-system";
 export interface Props extends FlexboxProps, LayoutProps, SpaceProps {
   height?: number;
   width?: number;
+  mode?: string;
 }
 
 export const defaultProps = {
   height: 38,
   width: 140,
+  mode: "light",
 };
