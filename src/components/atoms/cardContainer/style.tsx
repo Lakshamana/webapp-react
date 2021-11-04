fda4c5ffb320cd72e6cd2566b5cf363bece599d8
jorgeh71218 <jorge.hidalgo@sevenapps.com.br>
2021-11-04 22:03:10 +0300

    fix(cardContainer): new props and types for reutilization

--- src/components/atoms/cardContainer/style.tsx
+++ src/components/atoms/cardContainer/style.tsx
@@ -1,22 +1,20 @@
-import styled from "styled-components"
-import { Props } from "./types"
+import styled from "styled-components";
+import { Props } from "./types";
 import { space, layout, flexbox, position } from "styled-system";
-import { breakpoints } from 'styles';
+import { breakpoints } from "styles";
 
 export const Container = styled.div<Props>`
-	${space}
-	${layout}
+  ${space}
+  ${layout}
 	${flexbox}
 	${position}
-
 	mix-blend-mode: normal;
-	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-	border-radius: 8px;
-	
-	/* TO DO: Add color variable for theme-dark or theme-light */
-	background-color: #222222;
-
-	@media screen and (max-width: ${breakpoints.sm}) { 
-		border-radius: 0;
-	}
-`
+  box-shadow: ${({ removeShadow }) =>
+    !!removeShadow ? "none" : "0px 4px 4px rgba(0, 0, 0, 0.25)"};
+  border-radius: 8px;
+  /* TO DO: Add color variable for theme-dark or theme-light */
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  @media screen and (max-width: ${breakpoints.sm}) {
+    border-radius: 0;
+  }
+`;
