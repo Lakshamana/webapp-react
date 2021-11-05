1d8510d0a1936cfccd369b36cb93af15721e7b51
biancasilva <bianca@fanhero.com>
2021-11-05 14:34:27 -0300

    feat(signup): Add logic to change components and add mutation to create account

--- src/components/organisms/signupForm/index.tsx
+++ src/components/organisms/signupForm/index.tsx
@@ -1,11 +1,55 @@
+import { useState } from 'react';
+import { MUTATION_CREATE_ACCOUNT } from 'services/graphql';
 import { RegistrationForm, ConfirmAgeForm, ConfirmEmailForm, AdditionalInformationForm } from "./components";
+import { useMutation } from '@apollo/client';
+
+export type SignUpSteps = 'Register' | 'ConfirmAge' | 'ConfirmEmail' | 'Custom'
 
 const SignupForm = () => {
-    return (
-        <RegistrationForm></RegistrationForm>
-        // <ConfirmAgeForm></ConfirmAgeForm>
-        // <ConfirmEmailForm></ConfirmEmailForm>
-    );
+
+    const [activeStep, setActiveStep] = useState<SignUpSteps>('Register');
+
+    const [createAccount] = useMutation(MUTATION_CREATE_ACCOUNT, {
+        onCompleted: async (result) => {
+            if (!result?.createAccount) {
+                alert('Failed to create account, check your data!')
+                return
+            }
+            setActiveStep('ConfirmAge')
+        },
+        onError: (error) => {
+            // TO-DO ERROR COMPONENT
+            alert('Failed to register, check your data!')
+        }
+    });
+
+    const handleRegistrationSubmit = (FormData) => {
+        createAccount({
+            variables: { ...FormData }
+        })
+    }
+
+    const handleConfirmAgeSubmit = () => {
+        setActiveStep('Custom')
+    }
+
+    const renderStep = () => {
+        switch (activeStep) {
+            case 'Register':
+                return <RegistrationForm handleFormSubmit={handleRegistrationSubmit}></RegistrationForm>;
+            case 'ConfirmAge':
+                return <ConfirmAgeForm handleFormSubmit={handleConfirmAgeSubmit}></ConfirmAgeForm>;
+            case 'ConfirmEmail':
+                return <ConfirmEmailForm></ConfirmEmailForm>;
+            case 'Custom':
+                return <AdditionalInformationForm name={'Bianca'} email={'teste'} fullname={'Bianca Silva'}></AdditionalInformationForm>;
+            default:
+                return <RegistrationForm handleFormSubmit={handleRegistrationSubmit}></RegistrationForm>;
+        }
+    }
+
+    return renderStep();
+
 }
 
 export { SignupForm }
