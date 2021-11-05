8b4c29010b2e3093cda1fc423b8505e9982b8043
Marcus Vinicius Sgorla Franklin <kiinho@gmail.com>
2021-11-04 23:06:06 -0300

    fix: adjust load theme colors on styled components in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,41 @@
 import React from "react"
-import { ChakraProvider } from '@chakra-ui/react'
-import theme from '../src/styles/theme'
+import { ThemeProvider } from "styled-components";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
+import {
+  colors,
+  metrics,
+  fonts,
+  theme,
+  breakpoints as themeBreakpoints,
+} from "../src/styles";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
 
+const breakpoints = createBreakpoints(themeBreakpoints);
+
+const config = {
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+};
+
+const customTheme = extendTheme({
+  ...theme,
+  ...metrics,
+  fonts,
+  breakpoints,
+  config,
+  colors,
+});
+
 const withChakra = (StoryFn) => (
-  <ChakraProvider theme={theme}>
-    <StoryFn />
-  </ChakraProvider>
+  <ThemeProvider theme={{ ...theme }}>
+    <ChakraProvider theme={customTheme}>
+      <StoryFn />
+    </ChakraProvider>
+  </ThemeProvider>
 )
 
 export const decorators = [withChakra]
