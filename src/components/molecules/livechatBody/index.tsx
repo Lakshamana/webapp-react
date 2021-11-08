6bcfb5808b44113db02722fa365441b11bce9112
Yuri <yuri.mustifaga@sevenapps.com.br>
2021-11-08 16:55:55 -0300

    fix(livechat): fixes implementation chatbody

---
+++ src/components/molecules/livechatBody/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { MainChatBody } from "./style";
+import { Props } from "./types";
+
+import { BoxChat } from "components/atoms";
+
+const LivechatBody = ({ options = [] }: Props) => {
+  return (
+    <MainChatBody maxHeight={["55vh"]}>
+      {options.map((e) => (
+        <BoxChat
+          isOwnUser={e.isOwnUser}
+          username={e.username}
+          date={e.date}
+          message={e.message}
+          avatarUrl={e.iconUrl}
+        />
+      ))}
+    </MainChatBody>
+  );
+};
+
+export { LivechatBody };
