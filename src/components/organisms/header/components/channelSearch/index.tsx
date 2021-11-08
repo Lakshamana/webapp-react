941e048cf470a2b1b7b8f2086542df58ed434c2f
jorgeh71218 <jorge.hidalgo@sevenapps.com.br>
2021-11-08 17:57:15 +0300

    fix(channelSearch): removed feather icons

--- src/components/organisms/header/components/channelSearch/index.tsx
+++ src/components/organisms/header/components/channelSearch/index.tsx
@@ -1,32 +1,33 @@
-import { Search as SearchIcon } from "react-feather"
-import { useTranslation } from "react-i18next"
-import { Container, InputInline } from "components"
-import { PropsChannelSearch } from "../../types"
-import { SearchContainer } from "./styles"
-import { colors } from "styles"
+import { Icon } from "@iconify/react-with-api";
+import { useTranslation } from "react-i18next";
+import { Container, InputInline } from "components";
+import { PropsChannelSearch } from "../../types";
+import { SearchContainer } from "./styles";
+import { colors } from "styles";
 
 const ChannelSearch = ({ search, onChange }: PropsChannelSearch) => {
-	const { t } = useTranslation()
-	return (
-		<SearchContainer
-			display='flex'
-			justifyContent='center'
-			width='95%'
-			mx={2}
-			py={3}>
-			<Container width='85%'>
-				<InputInline
-					placeholder={t("header.channel_search")}
-					background={colors.backgroundLayout}
-					value={search}
-					{...{ onChange }}
-				/>
-			</Container>
-			<Container ml={2}>
-				<SearchIcon color={colors.white} height={18} width={18} />
-			</Container>
-		</SearchContainer>
-	)
-}
+  const { t } = useTranslation();
+  return (
+    <SearchContainer
+      display="flex"
+      justifyContent="center"
+      width="95%"
+      mx={2}
+      py={3}
+    >
+      <Container width="85%">
+        <InputInline
+          placeholder={t("header.channel_search")}
+          background={colors.backgroundLayout}
+          value={search}
+          {...{ onChange }}
+        />
+      </Container>
+      <Container ml={2}>
+        <Icon width={18} height={18} icon="bx:bx-search" color="white" />
+      </Container>
+    </SearchContainer>
+  );
+};
 
-export { ChannelSearch }
+export { ChannelSearch };
