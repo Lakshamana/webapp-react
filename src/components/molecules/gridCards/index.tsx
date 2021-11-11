0b82ed23ecd309ac6ca11b4812a60091b1bfc2c5
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-11-11 17:11:42 -0300

    fix(grid-cards): add new theme colors in title from grid cards

--- src/components/molecules/gridCards/index.tsx
+++ src/components/molecules/gridCards/index.tsx
@@ -1,16 +1,18 @@
 import { Text } from "components"
+import { useThemeStore } from "services/stores/theme"
 import { Props } from "./types"
 import { GridContent } from "./style"
 import { colors } from "styles"
 
 const GridCards = ({ ...props }: Props) => {
+	const { colorMode } = useThemeStore()
 	return (
 		<>
 			<Text
 				kind='title'
 				fontSize={"22px"}
 				fontWeight={500}
-				color={`${colors.white}`}
+				color={colors.generalText[colorMode]}
         mb={2}>
 				{props.headerTitle}
 			</Text>
