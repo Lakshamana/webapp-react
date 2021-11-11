7d5fc56fcdec398ccc1112d8ead51728d10285e7
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-11-11 17:09:09 -0300

    fix(reaction-bar): add new theme colors in reactions count

--- src/components/molecules/reactionBar/index.tsx
+++ src/components/molecules/reactionBar/index.tsx
@@ -1,66 +1,75 @@
-import { Container, Text, Popover } from "components";
-import { Plus, Smile } from "react-feather";
+import { Container, Text, Popover } from 'components'
+import { Plus, Smile } from 'react-feather'
+import { useThemeStore } from 'services/stores/theme'
 
-import { Props } from "./types";
+import { Props } from './types'
 
-import { availableReactions } from "./settings";
-import { Reaction } from "./styles";
-import { colors } from 'styles';
+import { availableReactions } from './settings'
+import { Reaction } from './styles'
+import { colors } from 'styles'
 
 const ReactionBar = ({
   reactions = [],
   totalReactions,
-  reactionsTitle
-}: Props) => (
-  <Container alignItems="center">
-    <Container>
-      <Container>
-        {reactions.map(reaction => (
-          <Reaction
-            key={`${reaction.value}-reaction`}
-            p={1}
-            mr={1}
-            minWidth={50}
-            minHeight={32}
-          >
-            <div>{reaction.value}</div>
-            <Text kind="regular" ml={2}>
-              {reaction.count}
-            </Text>
-          </Reaction>
-        ))}
-      </Container>
-      <Container>
-        <Reaction>
-          <Popover
-            trigger={
-              <button>
-                <Reaction p={1} mr={1} minWidth={50} minHeight={32}>
-                  <Plus size={20} />
-                  <Smile size={20} />
-                </Reaction>
-              </button>
-            }
-          >
-            <Container display={'grid'} gridTemplateColumns={'repeat(6, 1fr)'} justifyItems={'center'}>
-              {availableReactions.map(reaction => (
-                <Reaction key={`${reaction.value}-popover`} p={1} m={1}>
-                  {reaction.value}
-                </Reaction>
-              ))}
-            </Container>
-          </Popover>
-        </Reaction>
-      </Container>
-    </Container>
-    <Container ml={2}>
-      <Text
-        kind="regular"
-        fontWeight={500}
-        color={colors.white}
-      >{`${totalReactions} ${reactionsTitle}`}</Text>
-    </Container>
-  </Container>
-);
+  reactionsTitle,
+}: Props) => {
+	const { colorMode } = useThemeStore()
 
-export { ReactionBar };
+  return (
+    <Container alignItems="center">
+      <Container>
+        <Container>
+          {reactions.map((reaction) => (
+            <Reaction
+              key={`${reaction.value}-reaction`}
+              p={1}
+              mr={1}
+              minWidth={50}
+              minHeight={32}
+            >
+              <div>{reaction.value}</div>
+              <Text kind="regular" ml={2}>
+                {reaction.count}
+              </Text>
+            </Reaction>
+          ))}
+        </Container>
+        <Container>
+          <Reaction>
+            <Popover
+              trigger={
+                <button>
+                  <Reaction p={1} mr={1} minWidth={50} minHeight={32}>
+                    <Plus size={20} />
+                    <Smile size={20} />
+                  </Reaction>
+                </button>
+              }
+            >
+              <Container
+                display={'grid'}
+                gridTemplateColumns={'repeat(6, 1fr)'}
+                justifyItems={'center'}
+              >
+                {availableReactions.map((reaction) => (
+                  <Reaction key={`${reaction.value}-popover`} p={1} m={1}>
+                    {reaction.value}
+                  </Reaction>
+                ))}
+              </Container>
+            </Popover>
+          </Reaction>
+        </Container>
+      </Container>
+      <Container ml={2}>
+        <Text
+          kind="regular"
+          fontWeight={500}
+          color={colors.generalText[colorMode]}
+        >{`${totalReactions} ${reactionsTitle}`}</Text>
+      </Container>
+    </Container>
+  )
+}
+
+export { ReactionBar }
