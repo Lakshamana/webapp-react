31b6a82147b21ccfe6f790a8703123117005d620
jorgeh71218 <jorge.hidalgo@sevenapps.com.br>
2021-11-12 18:15:37 +0300

    feature(sizes): integrated on theme and new padding properties

--- src/styles/sizes.ts
+++ src/styles/sizes.ts
@@ -1,7 +1,10 @@
 export const sizes = {
-    headerDesktopHeight: "100px ",
-    headerMobileHeight: "72px",
-    footerDesktopHeight: "108px",
-    footerMobileHeight: "210px",
-    loginCardWidth: "556px"
-}   
+  headerDesktopHeight: "100px ",
+  headerMobileHeight: "72px",
+  footerDesktopHeight: "108px",
+  footerMobileHeight: "210px",
+  loginCardWidth: "556px",
+  paddingSm: "16px",
+  paddingMd: "32px",
+  paddingLg: "65px",
+};
