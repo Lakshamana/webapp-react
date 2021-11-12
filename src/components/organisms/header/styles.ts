d15553992b6b872fa91202fff559b7c71f0c017d
jorgeh71218 <jorge.hidalgo@sevenapps.com.br>
2021-11-12 16:49:39 +0300

    fix(header): styles and margins for header

--- src/components/organisms/header/styles.ts
+++ src/components/organisms/header/styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
+import { StyleContainer } from "components";
 import { space, layout, SpaceProps, LayoutProps } from "styled-system";
+import { breakpoints } from "styles";
 
 interface CircleLayout extends SpaceProps, LayoutProps {
   selected?: boolean;
@@ -12,4 +14,26 @@ export const Circle = styled.div<CircleLayout>`
   background-color: ${({ selected, theme }) =>
     selected && theme.colors.blue["300"]};
   border-radius: 50%;
+`;
+
+export const HeaderContainer = styled(StyleContainer)`
+  display: flex;
+  padding: 0 16px;
+  background: ${({ theme }) => theme.colors.headerBg[theme.colorMode]};
+
+  @media screen and (min-width: ${breakpoints.md}) {
+    padding: 0 32px;
+  }
+
+  @media screen and (min-width: ${breakpoints.lg}) {
+    padding: 0 65px;
+  }
+`;
+
+export const LogoContainer = styled.div`
+  padding: 0 16px;
+
+  @media screen and (min-width: ${breakpoints.xl}) {
+    padding: 0 32px;
+  }
 `;
