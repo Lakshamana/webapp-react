c63ef4b287a89c6269ee92ac9be8b3357801fcea
jorgeh71218 <jorge.hidalgo@sevenapps.com.br>
2021-11-12 18:15:05 +0300

    fix(footer): padding adjustment

--- src/components/molecules/internalFooter/index.tsx
+++ src/components/molecules/internalFooter/index.tsx
@@ -1,34 +1,34 @@
-import { Link } from "@chakra-ui/react"
-import { Logo, Text } from "components/atoms"
-import { useTranslation } from "react-i18next"
-import { BoxFooter, FooterItems, TextFooter } from "./style"
+import { Link } from "@chakra-ui/react";
+import { Logo, Text } from "components/atoms";
+import { useTranslation } from "react-i18next";
+import { BoxFooter, FooterItems, TextFooter } from "./style";
 
 const InternalFooter = () => {
-	const { t } = useTranslation()
-	return (
-		<BoxFooter px={4} display={"flex"} alignItems={"center"}>
-			<FooterItems width={1} py={20}>
-				<TextFooter>
-					<Link to='' fontSize={[16]}>
-						{t("common.terms")}
-					</Link>
-					<Text mx={1} fontSize={[16]}>
-						&
-					</Text>
-					<Link to='' fontSize={[16]}>
-						{t("common.privacy")}
-					</Link>
-				</TextFooter>
-				<Logo
-					alignItems={"center"}
-					justifyContent={"right"}
-					mb={[4, 4, 0, 0]}
-					width={161}
-					height={44}
-				/>
-			</FooterItems>
-		</BoxFooter>
-	)
-}
+  const { t } = useTranslation();
+  return (
+    <BoxFooter px={4} display={"flex"} alignItems={"center"}>
+      <FooterItems width={1} py={20}>
+        <TextFooter>
+          <Link to="" fontSize={[16]}>
+            {t("common.terms")}
+          </Link>
+          <Text mx={1} fontSize={[16]}>
+            &
+          </Text>
+          <Link to="" fontSize={[16]}>
+            {t("common.privacy")}
+          </Link>
+        </TextFooter>
+        <Logo
+          alignItems={"center"}
+          justifyContent={"flex-end"}
+          mb={[4, 4, 0, 0]}
+          width={161}
+          height={44}
+        />
+      </FooterItems>
+    </BoxFooter>
+  );
+};
 
-export { InternalFooter }
+export { InternalFooter };
