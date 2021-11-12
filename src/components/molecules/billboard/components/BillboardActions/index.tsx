ca321eda876e2939480e971939f3fc98e61b1d38
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-11-12 10:57:19 -0300

    fix(billboard-actions): remove conditional sizes from buttons to styled

--- src/components/molecules/billboard/components/BillboardActions/index.tsx
+++ src/components/molecules/billboard/components/BillboardActions/index.tsx
@@ -1,42 +1,27 @@
-import React, { useEffect, useState } from "react"
-import { Button } from "components"
-import { BillboardItemActions } from "../../types"
-import { Actions } from "./style"
+import React from 'react'
+import { Button } from 'components'
+import { BillboardItemActions } from '../../types'
+import { Actions, ContentButton } from './style'
 
 const ActionsList = ({ actions }: { actions: BillboardItemActions[] }) => {
-	const [size, setSize] = useState({
-		x: window.innerWidth,
-		y: window.innerHeight
-	})
-
-	const getSize = () => {
-		setSize({
-			x: window.innerWidth,
-			y: window.innerHeight
-		})
-	}
-
-	useEffect(() => (window.onresize = getSize), [])
-
-	return (
-		<Actions>
-			{actions?.map((actions: BillboardItemActions, i: number) => (
-				<Button
-					key={`Button-${i}`}
-					backgroundColor={actions.bgColor}
-					borderColor={actions.borderColor}
-					iconName={actions.icon}
-					color={actions.textColor}
-					type='billboard'
-					label={actions.label}
-					width={size.x <= 768 ? 146 : 267}
-					height={size.x <= 768 ? 40 : 54}
-					marginRight={size.x > 320 ? 15 : ''}
-					marginBottom={size.x <= 378 ? 10 : ''}
-				/>
-			))}
-		</Actions>
-	)
+  return (
+    <Actions>
+      {actions?.map((actions: BillboardItemActions, i: number) => (
+        <ContentButton>
+          <Button
+						width={'100%'}
+						height={'100%'}
+            key={`Button-${i}`}
+            backgroundColor={actions.bgColor}
+            borderColor={actions.borderColor}
+            iconName={actions.icon}
+            color={actions.textColor}
+            type="billboard"
+            label={actions.label}
+          />
+        </ContentButton>
+      ))}
+    </Actions>
+  )
 }
-
 export { ActionsList }
