2930f9b113d0987153dea6feb5c9e3ac4d360d84
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-11-12 10:58:08 -0300

    fix(billboard-actions): add new style from buttons

--- src/components/molecules/billboard/components/BillboardActions/style.tsx
+++ src/components/molecules/billboard/components/BillboardActions/style.tsx
@@ -1,20 +1,42 @@
-import styled from 'styled-components';
-import { breakpoints } from 'styles';
+import styled from 'styled-components'
+import { breakpoints } from 'styles'
 
 export const Actions: any = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+
+  @media screen and (max-width: ${breakpoints.md}) {
+    justify-content: center;
+  }
+
+  @media screen and (max-width: ${breakpoints.sm}) {
+    max-width: 360px;
     display: flex;
-    flex-wrap: wrap;
-    justify-content: flex-start;
+    margin-right: 5px;
+    margin-left: 5px;
+    justify-content: center;
+  }
+`
 
-    @media screen and (max-width: ${breakpoints.md}) {
-        justify-content: center;
-    }
+export const ContentButton = styled.div`
+  width: 267px;
+  height: 54px;
 
-    @media screen and (max-width: ${breakpoints.sm}) {
-        max-width: 360px;
-        display: flex;
-        margin-right: 5px;
-        margin-left: 5px;
-        justify-content: center;
+  &:first-child {
+    margin-right: 15px;
+  }
+
+  @media screen and (max-width: ${breakpoints.md}) {
+    width: 146px;
+    height: 40px;
+  }
+
+  @media screen and (max-width: ${breakpoints.sm}) {
+    margin-bottom: 10px;
+
+    &:first-child {
+      margin-right: 0px;
     }
-`;
+  }
+`
