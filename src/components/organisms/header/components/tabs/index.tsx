b3b2136e09002f2926ab4ef532c449cdf2477d60
jorgeh71218 <jorge.hidalgo@sevenapps.com.br>
2021-11-12 16:51:26 +0300

    fix(tabs-header): adjustments in spacings and colors

--- src/components/organisms/header/components/tabs/index.tsx
+++ src/components/organisms/header/components/tabs/index.tsx
@@ -1,17 +1,19 @@
 import { Tab } from "components";
 
+import { colors } from "styles";
 import { TabContainer } from "./styles";
 import { PropsTabs } from "../../types";
 
-const Tabs = ({ selected, setSelected, data }: PropsTabs) => (
+const Tabs = ({ selected, setSelected, data, colorMode }: PropsTabs) => (
   <TabContainer display="flex">
     {data.map((tab: any) => (
       <Tab
-        mx={2}
         key={tab.id}
         link={tab.url}
-        selected={selected === tab.label}
+        selected={selected === tab.id}
         onSelect={() => setSelected(tab.label)}
+        color={colors.headerTabText[colorMode]}
+        {...tab.style}
       >
         {tab.label}
       </Tab>
