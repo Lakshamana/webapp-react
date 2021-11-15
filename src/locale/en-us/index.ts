273645c03ac44e1d4c59becc2cf2af3f40c7b1b5
biancasilva <bianca@fanhero.com>
2021-11-15 20:11:29 -0300

    feat(locale): Add new locale strings

--- src/locale/en-us/index.ts
+++ src/locale/en-us/index.ts
@@ -1,98 +1,124 @@
 const EN_US = {
-	translations: {
-		common: {
-			or: "or",
-			error: {
-				field_required: "Campo {{field_name}} é obrigatório",
-				field_must_match: "{{field_name}} must match"
-			},
-			terms: "Terms and Conditions",
-			privacy: "Privacy and Policy",
-			more: "more",
-			edit_profile: "Edit profile",
-			settings: "Settings",
-			sign_out: "Sign out",
-			enable: "Enable",
-			disable: "Disable",
-			dark_mode: "dark mode",
-			messages: "messages",
-			votes: "votes"
-		},
-		signin: {
-			title: "Access Fanhero now",
-			subtitle: "Please login into your account now",
-			or: "or",
-			label: {
-				email: "Email",
-				password: "Password",
-				dont_have_account: "Doesn't have an account?",
-				save_as_default: "Save as default"
-			},
-			actions: {
-				login: "Login",
-				signup_here: "Sign Up here",
-				forgot_password: "Forgot password?"
-			}
-		},
-		signup: {
-			label: {
-				email: "Email",
-				password: "Password",
-				confirm_email: "Confirm Email"
-			},
-			registration: {
-				title: "Don't have an account yet?",
-				subtitle:
-					"Provide your email address and choose a password to create your account and enjoy access to exclusive content.",
-				already_have_account: "You already have an account?"
-			},
-			confirm_age: {
-				title: "Confirm you are over 13 to continue"
-			},
-			actions: {
-				signup: "Signup",
-				signin_here: "Sign In here",
-				accept_terms: "I accept all Terms and Conditions.",
-				confirm: "Confirm",
-				back: "Back"
-			}
-		},
-		header: {
-			tabs: {
-				home: "Home",
-				live: "Live",
-				feed: "Feed",
-				collections: "Collections",
-				my_list: "My List",
-				settings: "Settings",
-				tools: "Tools",
-				example: "Example"
-			},
-			channel_selected: {
-				select: "Select",
-				channel: "channel:"
-			},
-			channel_search: "Search channel..."
-		},
-		page: {
-			home: {
-				live: "Live",
-				most_recent: "Most Recents",
-				popular: "Popular"
-			},
-			collection: {
-				action: "Action",
-				drama: "Drama",
-				family: "Family",
-				watch_now: "Watch Now",
-				my_list: "My List"
-			},
-			my_list: {
-				my_list: "My List",
-				collection: "Collection"
-			}
-		}
-	}
+  translations: {
+    common: {
+      or: 'or',
+      error: {
+        field_required: 'Field {{field_name}} is required',
+        field_must_match: '{{field_name}} must match',
+        accept_terms_and_conditions:
+          'You must accept the terms and conditions.',
+        password_error:
+          'Password must contain min of 8 characteres, one uppercase, one lowercase, one number and one special character',
+      },
+      what_is_this: 'What is this?',
+      more: 'more',
+      terms: 'Terms and Conditions',
+      privacy: 'Privacy and Policy',
+      edit_profile: 'Edit profile',
+      settings: 'Settings',
+      sign_out: 'Sign out',
+      enable: 'Enable',
+      disable: 'Disable',
+      dark_mode: 'dark mode',
+      messages: 'messages',
+      votes: 'votes',
+      yes: 'Yes',
+      no: 'No',
+      close: 'Close',
+    },
+    signin: {
+      title: 'Access Fanhero now',
+      subtitle: 'Please login into your account now',
+      or: 'or',
+      label: {
+        email: 'Email',
+        password: 'Password',
+        dont_have_account: "Doesn't have an account?",
+        save_as_default: 'Save as default',
+      },
+      actions: {
+        login: 'Login',
+        signup_here: 'Sign Up here',
+        forgot_password: 'Forgot password?',
+      },
+    },
+    signup: {
+      label: {
+        email: 'Email',
+        password: 'Password',
+        confirm_email: 'Confirm Email',
+      },
+      registration: {
+        title: "Don't have an account yet?",
+        subtitle:
+          'Provide your email address and choose a password to create your account and enjoy access to exclusive content.',
+        already_have_account: 'You already have an account?',
+      },
+      GDPR: {
+        citizen: 'Are you a citizen or resident of the European Union?',
+      },
+      confirm_age: {
+        title: 'Confirm you are over {{age}} to continue',
+      },
+      our_politics: {
+        title: 'Our Politics',
+        description:
+          'We want you to know exactly how our app works and how we keep your details recorded. We take the protection of your data very seriously and have updated our Privacy Policy and Terms and Conditions.',
+        agree: 'I agree to the Terms and Conditions and Privacy Policy.',
+        confirm_you_read:
+          'Please confirm that you have read both before proceeding.',
+      },
+      confirm_email: {
+        title: 'Email verification',
+        description: 'We sent you a email to confirm your account.',
+        resend: 'Resend Email',
+      },
+      actions: {
+        signup: 'Signup',
+        signin_here: 'Sign In here',
+        accept_terms: 'I accept all Terms and Conditions.',
+        confirm: 'Confirm',
+        register: 'Register',
+        cancel: 'Cancel',
+        back: 'Back',
+      },
+    },
+    header: {
+      tabs: {
+        home: 'Home',
+        live: 'Live',
+        feed: 'Feed',
+        collections: 'Collections',
+        my_list: 'My List',
+        settings: 'Settings',
+        tools: 'Tools',
+        example: 'Example',
+      },
+      channel_selected: {
+        select: 'Select',
+        channel: 'channel:',
+      },
+      channel_search: 'Search channel...',
+    },
+    page: {
+      home: {
+        live: 'Live',
+        most_recent: 'Most Recents',
+        popular: 'Popular',
+      },
+      collection: {
+        action: 'Action',
+        drama: 'Drama',
+        family: 'Family',
+        watch_now: 'Watch Now',
+        my_list: 'My List',
+      },
+      my_list: {
+        my_list: 'My List',
+        collection: 'Collection',
+      },
+    },
+  },
 }
 
 export default EN_US
--- src/locale/pt-br/index.ts
+++ src/locale/pt-br/index.ts
@@ -1,96 +1,125 @@
 const PT_BR = {
-	translations: {
-		common: {
-			or: "ou",
-			error: {
-				field_required: "Campo {{field_name}} é obrigatório",
-				field_must_match: "{{field_name}} deve ser igual"
-			},
-			more: "mais",
-			edit_profile: "Editar perfil",
-			settings: "Ajustes",
-			sign_out: "Sair",
-			enable: "Habilitar",
-			disable: "Desabilitar",
-			dark_mode: "modo escuro",
-			messages: "menssagens",
-			votes: "votos"
-		},
-		signin: {
-			title: "Acesse Fanhero agora",
-			subtitle: "Por favor acesse a sua conta agora",
-			or: "ou",
-			label: {
-				email: "Email",
-				password: "Senha",
-				dont_have_account: "Não tem uma conta?",
-				save_as_default: "Salvar como padrão"
-			},
-			actions: {
-				login: "Entrar",
-				signup_here: "Registre-se",
-				forgot_password: "Esqueceu a senha?"
-			}
-		},
-		signup: {
-			label: {
-				email: "Email",
-				password: "Senha",
-				confirm_email: "Confirmar Email"
-			},
-			registration: {
-				title: "Não tem uma conta?",
-				subtitle:
-					"Insira seu endereço de e-mail e escolha uma senha para criar sua conta e ter acesso a conteúdos exclusivos.",
-				already_have_account: "Voce já tem uma conta?"
-			},
-			confirm_age: {
-				title: "Confirme que você tem mais de 13 anos para continuar"
-			},
-			actions: {
-				signup: "Registrar",
-				signin_here: "Faça o Login",
-				accept_terms: "Eu aceito todos os Termos e Condições",
-				confirm: "Confirmar",
-				back: "Voltar"
-			}
-		},
-		header: {
-			tabs: {
-				home: "Home",
-				live: "Live",
-				feed: "Feed",
-				collections: "Collections",
-				my_list: "My List",
-				settings: "Settings",
-				tools: "Tools",
-				example: "Example"
-			},
-			channel_selected: {
-				select: "Selecionar",
-				channel: "canal:"
-			},
-			channel_search: "Buscar canal..."
-		},
-		page: {
-			home: {
-				live: "Ao vivo",
-				most_recent: "Mais Recentes",
-				popular: "Popular"
-			},
-			collection: {
-				action: "Ação",
-				drama: "Drama",
-				family: "Família",
-				watch_now: "Assistir agora",
-				my_list: "Minha Lista"
-			},
-			my_list: {
-				my_list: "Minha Lista",
-				collection: "Coleção"
-			}
-		}
-	}
+  translations: {
+    common: {
+      or: 'ou',
+      error: {
+        field_required: 'Campo {{field_name}} é obrigatório',
+        field_must_match: '{{field_name}} deve ser igual',
+        accept_terms_and_conditions:
+          'Você precisa aceitar os Termos e Condições',
+        password_error:
+          'A senha deve conter no mínimo 8 caracteres, uma letra maiúscula, uma letra minúscula, um número e um caractere especial',
+      },
+      what_is_this: 'O que é isso?',
+      more: 'mais',
+      terms: 'Termos e Condições',
+      privacy: 'Política de Privacidade',
+      edit_profile: 'Editar perfil',
+      settings: 'Ajustes',
+      sign_out: 'Sair',
+      enable: 'Habilitar',
+      disable: 'Desabilitar',
+      dark_mode: 'modo escuro',
+      messages: 'menssagens',
+      votes: 'votos',
+      yes: 'Sim',
+      no: 'Não',
+      close: 'Fechar',
+    },
+    signin: {
+      title: 'Acesse Fanhero agora',
+      subtitle: 'Por favor acesse a sua conta agora',
+      or: 'ou',
+      label: {
+        email: 'Email',
+        password: 'Senha',
+        dont_have_account: 'Não tem uma conta?',
+        save_as_default: 'Salvar como padrão',
+      },
+      actions: {
+        login: 'Entrar',
+        signup_here: 'Registre-se',
+        forgot_password: 'Esqueceu a senha?',
+      },
+    },
+    signup: {
+      label: {
+        email: 'Email',
+        password: 'Senha',
+        confirm_email: 'Confirmar Email',
+      },
+      registration: {
+        title: 'Não tem uma conta?',
+        subtitle:
+          'Insira seu endereço de e-mail e escolha uma senha para criar sua conta e ter acesso a conteúdos exclusivos.',
+        already_have_account: 'Voce já tem uma conta?',
+      },
+      GDPR: {
+        citizen: 'Você é cidadão ou residente da União Europeia?',
+      },
+      confirm_age: {
+        title: 'Confirme que você tem mais que {{age}} anos para continuar',
+      },
+      our_politics: {
+        title: 'Nossa Política',
+        description:
+          'Queremos que você saiba exatamente como nosso aplicativo funciona e como mantemos seus dados registrados. Levamos a proteção de seus dados muito a sério e atualizamos nossa Política de Privacidade e Termos e Condições.',
+        agree:
+          'Eu concordo com os Termos e Condições e Política de Privacidade.',
+        confirm_you_read:
+          'Por favor, confirme que você leu ambos antes de continuar.',
+      },
+      confirm_email: {
+        title: 'Verificação de Email',
+        description: 'Nós enviamos um email para verificar sua conta.',
+        resend: 'Reenviar Email',
+      },
+      actions: {
+        signup: 'Registrar',
+        signin_here: 'Faça o Login',
+        accept_terms: 'Eu aceito todos os Termos e Condições',
+        confirm: 'Confirmar',
+        register: 'Registrar',
+        cancel: 'Cancelar',
+        back: 'Voltar',
+      },
+    },
+    header: {
+      tabs: {
+        home: 'Home',
+        live: 'Live',
+        feed: 'Feed',
+        collections: 'Collections',
+        my_list: 'My List',
+        settings: 'Settings',
+        tools: 'Tools',
+        example: 'Example',
+      },
+      channel_selected: {
+        select: 'Selecionar',
+        channel: 'canal:',
+      },
+      channel_search: 'Buscar canal...',
+    },
+    page: {
+      home: {
+        live: 'Ao vivo',
+        most_recent: 'Mais Recentes',
+        popular: 'Popular',
+      },
+      collection: {
+        action: 'Ação',
+        drama: 'Drama',
+        family: 'Família',
+        watch_now: 'Assistir agora',
+        my_list: 'Minha Lista',
+      },
+      my_list: {
+        my_list: 'Minha Lista',
+        collection: 'Coleção',
+      },
+    },
+  },
 }
 
 export default PT_BR
