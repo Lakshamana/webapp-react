8153fbf0d9e22c7b4cf89f1674a78fd29c25a316
biancasilva <bianca@fanhero.com>
2021-11-15 19:50:34 -0300

    feat(sizes): Add new size for login/register buttons based on figma

--- src/styles/sizes.ts
+++ src/styles/sizes.ts
@@ -4,6 +4,7 @@ export const sizes = {
   footerDesktopHeight: "108px",
   footerMobileHeight: "210px",
   loginCardWidth: "556px",
+  loginButtonWidth: "267px",
   paddingSm: "16px",
   paddingMd: "32px",
   paddingLg: "65px",
