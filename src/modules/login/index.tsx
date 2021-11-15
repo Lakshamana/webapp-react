b52b46eab75014103a78d2e127ef7b1ff88078c6
biancasilva <bianca@fanhero.com>
2021-11-15 19:52:34 -0300

    fix(colors): Use colors and sizes defined on styles

--- src/modules/login/index.tsx
+++ src/modules/login/index.tsx
@@ -5,12 +5,14 @@ import { useMutation } from '@apollo/client';
 import { MUTATION_SIGNIN } from "services/graphql"
 import { Text, LoginLayout, CardContainer, SocialSigninButton, SigninForm } from "components";
 import { Container } from './styles';
-import { sizes } from 'styles'
+import { sizes, colors } from 'styles'
 import { AUTH_TOKEN } from 'config/constants';
+import { useThemeStore } from 'services/stores/theme'
 
 const LoginPage = () => {
   const { t } = useTranslation();
   const history = useHistory();
+  const { colorMode } = useThemeStore();
 
   const [signIn] = useMutation(MUTATION_SIGNIN, {
     onCompleted: async (result) => {
@@ -37,23 +39,23 @@ const LoginPage = () => {
     <LoginLayout>
       <Container width={1} paddingY={[0, 40]}>
         <CardContainer paddingX={[30, 60]} paddingY={[40, 40]} width={[1, sizes.loginCardWidth]}>
-          <Text fontSize={24} textAlign={'center'} fontWeight={'bolder'} color={'white'}>{t('signin.title')}</Text>
-          <Text fontSize={16} paddingTop={10} textAlign={'center'} color={'#A4A4A4'}>{t('signin.subtitle')}</Text>
+          <Text fontSize={24} textAlign={'center'} fontWeight={'bolder'} color={colors.generalText[colorMode]}>{t('signin.title')}</Text>
+          <Text fontSize={16} paddingTop={10} textAlign={'center'} color={colors.secondaryText[colorMode]}>{t('signin.subtitle')}</Text>
           <Flex gridGap={7} marginY={30} justifyContent={'center'}>
             <SocialSigninButton type={'facebook'}></SocialSigninButton>
             <SocialSigninButton type={'google'}></SocialSigninButton>
           </Flex>
-          <Text fontSize={16} textAlign={'center'} color={'#A4A4A4'}>{t('common.or')}</Text>
+          <Text fontSize={16} textAlign={'center'} color={colors.secondaryText[colorMode]}>{t('common.or')}</Text>
           <SigninForm handleFormSubmit={handleFormSubmit}></SigninForm>
           {/* TO-DO FORGOT PASSWORD, CREATE ATOM FOR LINKS */}
           <Box textAlign={'center'}>
             <Link style={{ 'textTransform': 'uppercase', 'fontWeight': 'bold' }} to="resetPassword">{t('signin.actions.forgot_password')}</Link>
           </Box>
           <Flex marginTop={10} marginBottom={5} justifyContent={'center'} flexWrap={'wrap'}>
-            <Text color={'white'} paddingRight={1}>
+            <Text color={colors.generalText[colorMode]} paddingRight={1}>
               {t('signin.label.dont_have_account')}
             </Text>
-            <Link to="signup">{t('signin.actions.signup_here')}</Link>
+            <Link style={{ color: colors.brand.accent[colorMode] }} to="signup">{t('signin.actions.signup_here')}</Link>
           </Flex>
         </CardContainer>
       </Container>
