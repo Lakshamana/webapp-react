3d47cbfd55838a0d047e57aa2d0ff709fdd82c4c
biancasilva <bianca@fanhero.com>
2021-11-19 12:47:05 -0300

    fix(mutations): fix mutations fields

--- src/services/graphql/mutation/signin.ts
+++ src/services/graphql/mutation/signin.ts
@@ -3,7 +3,12 @@ import { gql } from "@apollo/client"
 export const MUTATION_SIGNIN = gql`
 	mutation Signin($signIn: SignInInput!) {
 		signIn(signIn: $signIn) {
-			accessToken
+			token {
+				accessToken
+			}
+			account {
+				id
+			}
 		}
 	}
 `
