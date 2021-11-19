cb67c9c711c0539ffa0b0f502b1f38d14cb2af55
biancasilva <bianca@fanhero.com>
2021-11-19 12:51:44 -0300

    fix(externalFooter): fix styles

--- src/components/molecules/externalFooter/index.tsx
+++ src/components/molecules/externalFooter/index.tsx
@@ -1,19 +1,25 @@
 import { Link } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next'
-import { Logo, Text } from 'components/atoms'
+import { Logo, Text, Container } from 'components/atoms'
+import { useThemeStore } from 'services/stores/theme';
 import { BoxFooter, FooterItems, TextFooter } from "./style";
+import { colors } from 'styles';
 
 const ExternalFooter = () => {
-    const { t } = useTranslation()
+    const { t } = useTranslation();
+    const { colorMode } = useThemeStore();
     return (
-        <BoxFooter px={4} display={'flex'} alignItems={'center'}>
+        <BoxFooter display={'flex'} alignItems={'center'}>
             <FooterItems width={1} py={20}>
                 <TextFooter>
                     <Link to="" fontSize={[16]}>{t('common.terms')}</Link>
                     <Text mx={1} fontSize={[16]}>&</Text>
                     <Link to="" fontSize={[16]}>{t('common.privacy')}</Link>
                 </TextFooter>
-                <Logo alignItems={'center'} justifyContent={'right'} mb={[4, 4, 0, 0]} width={161} height={44} />
+                <Container marginLeft={['none', 'none', 'auto']} display={'block'} textAlign={'center'}>
+                    <Text fontSize={14} kind={'subheading'} color={colors.secondaryText[colorMode]}> Powered by </Text>
+                    <Logo alignItems={'center'} justifyContent={'right'} mb={[4, 4, 0, 0]} width={161} height={44} />
+                </Container>
             </FooterItems>
         </BoxFooter>
     )
