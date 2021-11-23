4892a2a4572107bbd740c1e9491042cbd685db9f
biancasilva <bianca@fanhero.com>
2021-11-23 16:43:07 -0300

    feat(requestPasswordResetForm): Add new component for require password change

---
+++ src/modules/recoverPassword/components/requestPasswordResetForm/types.ts
@@ -0,0 +1,7 @@
+import { LayoutProps, SpaceProps, FlexboxProps } from 'styled-system'
+
+export interface Props extends FlexboxProps, LayoutProps, SpaceProps {
+  handleFormSubmit: (formData: any) => void
+  dispatchError: () => void
+  error: string
+}
