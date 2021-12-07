1217f99c70494169b17e9668d8c7cb2ebb607b60
biancasilva <bianca@fanhero.com>
2021-12-07 12:29:41 -0300

    fix(alert): fix text color and space

--- src/components/atoms/alert/style.ts
+++ src/components/atoms/alert/style.ts
@@ -7,6 +7,7 @@ export const Container = styled.div<Props>`
 `
 
 export const AlertStyled: any = styled(Alert)`
+  color: #000000;
   border-radius: 4px;
   border: 1px solid
     ${({ theme, status }) => theme.colors.alerts[status].default};
