761ad017408afeafb21c632c77021613ca264afc
biancasilva <bianca@fanhero.com>
2021-12-07 12:31:24 -0300

    fix(confirmCitizen): use new Link atom

--- src/components/organisms/signupForm/components/GDPR/components/confirmCitizenship/index.tsx
+++ src/components/organisms/signupForm/components/GDPR/components/confirmCitizenship/index.tsx
@@ -1,29 +1,48 @@
-import { Link } from 'react-router-dom';
-import { Flex } from '@chakra-ui/react';
+import { Link } from 'components'
+import { Flex } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
-import { Button, Text } from "components";
-import { ConfirmAgeProps } from './types';
-import { colors, sizes } from 'styles';
-import { useThemeStore } from 'services/stores/theme';
+import { Button, Text } from 'components'
+import { ConfirmAgeProps } from './types'
+import { colors, sizes } from 'styles'
+import { useThemeStore } from 'services/stores/theme'
 
 const ConfirmCitizenshipForm = ({ handleFormSubmit }: ConfirmAgeProps) => {
-    const { t } = useTranslation();
-    const { colorMode } = useThemeStore();
+  const { t } = useTranslation()
+  const { colorMode } = useThemeStore()
 
-    return (
-        <Flex alignItems={'center'} flexDirection={'column'} gridGap={3}>
-            <Text fontSize={24} textAlign={'center'} fontWeight={'bolder'} color={colors.generalText[colorMode]}>{t('signup.GDPR.citizen')}</Text>
-            <Link style={{ color: colors.brand.accent[colorMode], textDecoration: 'underline', fontSize: '16px' }} to="">{t('common.what_is_this')}</Link>
-            <Button width={[1, sizes.loginButtonWidth]} paddingLeft={105} paddingRight={105} marginTop={30} type={'submit'} label={t('common.yes')} onClick={() => handleFormSubmit(true)}></Button>
-            <Button width={[1, sizes.loginButtonWidth]} paddingLeft={105} paddingRight={105} type={'cancel'} label={t('common.no')} onClick={() => handleFormSubmit(false)}></Button>
-            <Flex justifyContent={'center'} mt={5}>
-                <Text color={colors.generalText[colorMode]} paddingRight={1}>
-                    {t('signup.registration.already_have_account')}
-                </Text>
-                <Link style={{ color: colors.brand.accent[colorMode] }} to="login">{t('signup.actions.signin_here')}</Link>
-            </Flex>
-        </Flex>
-    );
+  return (
+    <Flex alignItems={'center'} flexDirection={'column'} gridGap={3}>
+      <Text
+        fontSize={24}
+        textAlign={'center'}
+        fontWeight={'bolder'}
+        color={colors.generalText[colorMode]}
+      >
+        {t('signup.GDPR.citizen')}
+      </Text>
+      <Link
+        label={t('common.what_is_this')}
+        externalLink="https://fanhero.com/gdpr/en/"
+      ></Link>
+      <Button
+        width={[1, sizes.loginButtonWidth]}
+        paddingLeft={105}
+        paddingRight={105}
+        marginTop={30}
+        type={'submit'}
+        label={t('common.yes')}
+        onClick={() => handleFormSubmit(true)}
+      ></Button>
+      <Button
+        width={[1, sizes.loginButtonWidth]}
+        paddingLeft={105}
+        paddingRight={105}
+        type={'cancel'}
+        label={t('common.no')}
+        onClick={() => handleFormSubmit(false)}
+      ></Button>
+    </Flex>
+  )
 }
 
 export { ConfirmCitizenshipForm }
