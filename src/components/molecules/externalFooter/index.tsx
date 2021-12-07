f3dca0123f34f060511019bdb0bbcc9f9ff4d56d
biancasilva <bianca@fanhero.com>
2021-12-07 12:30:34 -0300

    fix(footer): fix privacy and terms link using new Link component

--- src/components/molecules/externalFooter/index.tsx
+++ src/components/molecules/externalFooter/index.tsx
@@ -1,28 +1,56 @@
-import { Link } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next'
-import { Logo, Text, Container } from 'components/atoms'
-import { useThemeStore } from 'services/stores/theme';
-import { BoxFooter, FooterItems, TextFooter } from "./style";
-import { colors } from 'styles';
+import { Logo, Text, Container, Link } from 'components/atoms'
+import { useThemeStore } from 'services/stores/theme'
+import { BoxFooter, FooterItems, TextFooter } from './style'
+import { colors } from 'styles'
 
 const ExternalFooter = () => {
-    const { t } = useTranslation();
-    const { colorMode } = useThemeStore();
-    return (
-        <BoxFooter display={'flex'} alignItems={'center'}>
-            <FooterItems width={1} py={20}>
-                <TextFooter>
-                    <Link to="" fontSize={[16]}>{t('common.terms')}</Link>
-                    <Text mx={1} fontSize={[16]}>&</Text>
-                    <Link to="" fontSize={[16]}>{t('common.privacy')}</Link>
-                </TextFooter>
-                <Container marginLeft={['none', 'none', 'auto']} display={'block'} textAlign={'center'}>
-                    <Text fontSize={14} kind={'subheading'} color={colors.secondaryText[colorMode]}> Powered by </Text>
-                    <Logo alignItems={'center'} justifyContent={'right'} mb={[4, 4, 0, 0]} width={161} height={44} />
-                </Container>
-            </FooterItems>
-        </BoxFooter>
-    )
+  const { t } = useTranslation()
+  const { colorMode } = useThemeStore()
+  return (
+    <BoxFooter display={'flex'} alignItems={'center'}>
+      <FooterItems width={1} py={20}>
+        <TextFooter>
+          <Link
+            fontSize={[16]}
+            externalLink={'https://fanhero.com/terms/en/'}
+            label={t('common.terms')}
+            defaultColor
+          />
+          <Text mx={1} fontSize={[16]}>
+            &
+          </Text>
+          <Link
+            externalLink={'https://fanhero.com/privacy/en/'}
+            label={t('common.privacy')}
+            fontSize={[16]}
+            defaultColor
+          />
+        </TextFooter>
+        <Container
+          marginLeft={['none', 'none', 'auto']}
+          display={'block'}
+          textAlign={'center'}
+        >
+          <Text
+            fontSize={14}
+            kind={'subheading'}
+            color={colors.secondaryText[colorMode]}
+          >
+            {' '}
+            Powered by{' '}
+          </Text>
+          <Logo
+            alignItems={'center'}
+            justifyContent={'right'}
+            mb={[4, 4, 0, 0]}
+            width={161}
+            height={44}
+          />
+        </Container>
+      </FooterItems>
+    </BoxFooter>
+  )
 }
 
-export { ExternalFooter };
+export { ExternalFooter }
