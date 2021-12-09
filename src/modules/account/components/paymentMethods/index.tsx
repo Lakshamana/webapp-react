7699db17c3692df794b93f1bb48dc8c11c389b0c
jorgeh71218 <jorge.hidalgo@sevenapps.com.br>
2021-12-09 15:10:27 -0300

    fix(paymentMethods): rendering on other resolutions

--- src/modules/account/components/paymentMethods/index.tsx
+++ src/modules/account/components/paymentMethods/index.tsx
@@ -22,7 +22,7 @@ const PaymentMethods = ({
           width={1}
           flexWrap="wrap"
         >
-          <Container width={2 / 3}>
+          <Container width="60%">
             <Radio
               spacing={{ py: 2 }}
               color={colors.black}
@@ -34,27 +34,29 @@ const PaymentMethods = ({
               {method.type} {method.number}
             </Radio>
           </Container>
-          <Container width={1 / 3} justifyContent="flex-end">
-            {!!index ? (
+          <div>
+            <Container ml={1} justifyContent="flex-end">
+              {!!index ? (
+                <ActionLink
+                  fontSize={12}
+                  fontWeight="bold"
+                  onClick={() => onDelete(method)}
+                >
+                  {strings.delete}
+                </ActionLink>
+              ) : (
+                <></>
+              )}
               <ActionLink
                 fontSize={12}
                 fontWeight="bold"
-                onClick={() => onDelete(method)}
+                onClick={() => onUpdate(method)}
+                ml={2}
               >
-                {strings.delete}
+                {strings.update}
               </ActionLink>
-            ) : (
-              <></>
-            )}
-            <ActionLink
-              fontSize={12}
-              fontWeight="bold"
-              onClick={() => onUpdate(method)}
-              ml={2}
-            >
-              {strings.update}
-            </ActionLink>
-          </Container>
+            </Container>
+          </div>
           {!!index ? <></> : <Separator my={3} />}
         </Container>
       </>
