aed5722eb068b52c467259391a26b98ad30fa68a
biancasilva <bianca@fanhero.com>
2021-12-09 16:06:54 -0300

    feat(template): add flagsprovider to templates

--- src/components/templates/templateProvider/index.tsx
+++ src/components/templates/templateProvider/index.tsx
@@ -1,7 +1,7 @@
-import PropTypes from "prop-types";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { createBreakpoints } from "@chakra-ui/theme-tools";
-import { Global, css } from "@emotion/react";
+import PropTypes from 'prop-types'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { createBreakpoints } from '@chakra-ui/theme-tools'
+import { Global, css } from '@emotion/react'
 import {
   colors,
   metrics,
@@ -9,13 +9,14 @@ import {
   globalStyles,
   theme,
   breakpoints as themeBreakpoints,
-} from "styles";
-import { BreakpointProvider } from "services/hooks";
-import { AuthProvider } from "../authProvider";
-import { ThemeProvider } from "styled-components";
-import { useThemeStore } from "services/stores/theme";
+} from 'styles'
+import { BreakpointProvider } from 'services/hooks'
+import { AuthProvider } from '../authProvider'
+import { ThemeProvider } from 'styled-components'
+import { useThemeStore } from 'services/stores/theme'
+import FlagsProvider from 'config/firebase/FlagsProvider'
 
-const breakpoints = createBreakpoints(themeBreakpoints);
+const breakpoints = createBreakpoints(themeBreakpoints)
 
 const TemplateProvider = ({ children }: any) => {
   const { colorMode } = useThemeStore()
@@ -23,7 +24,7 @@ const TemplateProvider = ({ children }: any) => {
   const config = {
     initialColorMode: 'light',
     useSystemColorMode: false,
-  };
+  }
 
   const customTheme = extendTheme({
     ...theme,
@@ -32,26 +33,30 @@ const TemplateProvider = ({ children }: any) => {
     breakpoints,
     config,
     colors,
-  });
+  })
 
   return (
-    <ThemeProvider theme={{ ...theme, colorMode, darkMode: colorMode === 'dark' }}>
+    <ThemeProvider
+      theme={{ ...theme, colorMode, darkMode: colorMode === 'dark' }}
+    >
       <ChakraProvider theme={customTheme}>
-        <BreakpointProvider>
-          <Global
-            styles={css`
-              ${globalStyles}
-            `}
-          />
-          <AuthProvider>{children}</AuthProvider>
-        </BreakpointProvider>
+        <FlagsProvider>
+          <BreakpointProvider>
+            <Global
+              styles={css`
+                ${globalStyles}
+              `}
+            />
+            <AuthProvider>{children}</AuthProvider>
+          </BreakpointProvider>
+        </FlagsProvider>
       </ChakraProvider>
     </ThemeProvider>
-  );
-};
+  )
+}
 
 TemplateProvider.propTypes = {
   children: PropTypes.node,
-};
+}
 
-export { TemplateProvider };
+export { TemplateProvider }
