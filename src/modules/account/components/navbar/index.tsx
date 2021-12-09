2179fc07e560dd69439de998275768e5bc42d292
jorgeh71218 <jorge.hidalgo@sevenapps.com.br>
2021-12-09 14:29:59 -0300

    fix(navBar - Account): Back to screen

--- src/modules/account/components/navbar/index.tsx
+++ src/modules/account/components/navbar/index.tsx
@@ -1,19 +1,21 @@
 import { Icon } from '@iconify/react'
 import { Container, Text } from 'components'
+import { Link } from 'react-router-dom'
 
 import { colors } from 'styles'
 import { NavbarProps } from './types'
 import { BackTextBox, WrapperIcon } from '../../styles'
 
-const Navbar = ({ onClick, colorMode, text }: NavbarProps) => (
-  <Container width={1} alignItems="center" mt={2} mb={4}>
+const Navbar = ({ colorMode, text }: NavbarProps) => (
+  <Container width={1} mt={2} alignItems="center" mb={4}>
     <WrapperIcon>
-      <Icon
-        icon="mdi:arrow-left"
-        width={20}
-        color={colors.generalText[colorMode]}
-        {...{ onClick }}
-      />
+      <Link to="/home">
+        <Icon
+          icon="mdi:arrow-left"
+          width={20}
+          color={colors.generalText[colorMode]}
+        />
+      </Link>
     </WrapperIcon>
     <BackTextBox ml={2} px={2}>
       <Text fontWeight="bold" color={colors.generalText[colorMode]}>
