fb69127d1ad9699cbb1659257461fc0055b8aee6
jorgeh71218 <jorge.hidalgo@sevenapps.com.br>
2021-12-10 13:53:12 -0300

    fix(navbar): using link component

--- src/modules/account/components/navbar/index.tsx
+++ src/modules/account/components/navbar/index.tsx
@@ -1,7 +1,5 @@
 import { Icon } from '@iconify/react'
-import { Container, Text } from 'components'
-import { Link } from 'react-router-dom'
-
+import { Container, Text, Link } from 'components'
 import { colors } from 'styles'
 import { NavbarProps } from './types'
 import { BackTextBox, WrapperIcon } from '../../styles'
@@ -9,7 +7,7 @@ import { BackTextBox, WrapperIcon } from '../../styles'
 const Navbar = ({ colorMode, text }: NavbarProps) => (
   <Container width={1} mt={2} alignItems="center" mb={4}>
     <WrapperIcon>
-      <Link to="/home">
+      <Link toRoute="/home">
         <Icon
           icon="mdi:arrow-left"
           width={20}
