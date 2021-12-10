a27e7644ebd22cead9c345c8d45734a7079e274e
jorgeh71218 <jorge.hidalgo@sevenapps.com.br>
2021-12-10 15:16:08 -0300

    fix(profile): query with new fields

--- src/services/graphql/query/profile.ts
+++ src/services/graphql/query/profile.ts
@@ -3,14 +3,15 @@ import { gql } from '@apollo/client'
 export const QUERY_PROFILE = gql`
   query Profile($account: ID!) {
     profile(account: $account) {
-      address
       avatar
-      birthday
-      gender
       id
       phone
-      account
       locale
+    }
+    account(FindOneParamsDto: { id: $account }) {
+      username
+      display_name
+      email
     }
   }
 `
