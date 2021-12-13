7e21a280c7d6f53f849a928088c599ff8a5463af
biancasilva <bianca@fanhero.com>
2021-12-13 09:48:54 -0300

    feat(stories): add modal

--- src/stories/modal.stories.tsx
+++ src/stories/modal.stories.tsx
@@ -1,28 +1,39 @@
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { useState } from 'react'
 
-import { Modal } from "components";
+import { Modal, Button } from 'components'
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "WEBAPP/Modal",
+  title: 'WEBAPP/Modal',
   component: Modal,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   //   argTypes: {
   //     backgroundColor: { control: 'color' },
   //   },
-} as ComponentMeta<typeof Modal>;
+} as ComponentMeta<typeof Modal>
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Modal> = (args) => (
-  <Modal {...args}>{args.children}</Modal>
-);
+const Template: ComponentStory<typeof Modal> = (args) => {
+  const [openModal, setOpenModal] = useState(false)
+  return (
+    <>
+      <Button onClick={() => setOpenModal(true)} label={'Open Modal'}></Button>
+      <Modal {...args} isOpen={openModal} onClose={() => setOpenModal(false)}>
+        {args.children}
+      </Modal>
+    </>
+  )
+}
 
-export const Component = Template.bind({});
+export const Default = Template.bind({})
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Component.args = {
-  children: <div>Exemplo</div>,
-  show: true,
-  title: "Title Modal",
-  onClose: () => {},
-  width: "100%",
-};
+Default.args = {
+  isCentered: true,
+  title: 'Testing my modal',
+  subtitle: 'This is a test',
+  actionButton: true,
+  actionLabel: 'Confirm',
+  cancelButton: true,
+  cancelLabel: 'Back',
+}
