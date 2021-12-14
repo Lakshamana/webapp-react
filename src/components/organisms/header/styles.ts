ddd9f6545a998eb3b0b7bf4d503786e8bcf2620c
biancasilva <bianca@fanhero.com>
2021-12-13 21:42:03 -0300

    fix(header): fix padding

--- src/components/organisms/header/styles.ts
+++ src/components/organisms/header/styles.ts
@@ -5,10 +5,19 @@ import { breakpoints } from 'styles'
 export const HeaderContainer = styled(StyleContainer)`
   display: flex;
   background: ${({ theme }) => theme.colors.headerBg[theme.colorMode]};
+  padding: 0 ${({ theme }) => theme.sizes.paddingSm};
 
   ${({ theme }) =>
     theme.colorMode === 'light' &&
     `border-bottom: 1px solid ${theme.colors.grey[200]}`}
+
+    @media screen and (min-width: ${breakpoints.md}) {
+      padding: 0 ${({ theme }) => theme.sizes.paddingMd};
+    }
+  
+    @media screen and (min-width: ${breakpoints.lg}) {
+      padding: 0 ${({ theme }) => theme.sizes.paddingLg};
+    }
 `
 
 export const LogoContainer = styled.div`
