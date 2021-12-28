c9c4e15ae7a698c58233e14b284333fd61788a5c
biancasilva <bianca@fanhero.com>
2021-12-28 15:46:43 -0300

    feat(registration): add new handleSocialSignUp function

--- src/components/organisms/signupForm/components/registration/types.ts
+++ src/components/organisms/signupForm/components/registration/types.ts
@@ -1,5 +1,8 @@
+import { Social } from 'services/firebase'
+
 export interface RegistrationProps {
   handleFormSubmit: (formData: any) => void
+  handleSocialSignUp: (kind: Social) => void
   dispatchError: () => void
   error: string
   isLoading: boolean
