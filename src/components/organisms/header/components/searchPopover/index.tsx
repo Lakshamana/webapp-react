3b01fd64e4e0a15a2cb0220dcde2c1f49e6d9a00
João Pedro Moraes <joaopedromoraes@outlook.com>
2021-12-29 19:39:41 -0300

    feat(SearchPopover): add size in text section

--- src/components/organisms/header/components/searchPopover/index.tsx
+++ src/components/organisms/header/components/searchPopover/index.tsx
@@ -11,12 +11,12 @@ const SearchPopover = ({
     <ScrollContainer maxHeight={250} flexDirection="column" width={1}>
       {data.map((item) => (
         <>
-          <Container key={item.id} backgroundColor={section} width={1} p={3}>
-            <Text color={textColor}>{item.label?.toUpperCase()}</Text>
+          <Container key={item.id} backgroundColor={section} width={1} py={2} px={3}>
+            <Text fontWeight="bold" fontSize="14px" color={textColor} >{item.label?.toUpperCase()}</Text>
           </Container>
           {item.children.map((content) => (
             <Container key={content.id} p={3}>
-              <Text color={textColor}>{content.text}</Text>
+              <Text fontSize="18px"  color={textColor}>{content.text}</Text>
             </Container>
           ))}
         </>
