9203fe1310887ee8d34903e95c829347ddf8de73
biancasilva <bianca@fanhero.com>
2021-12-30 18:54:56 -0300

    fix(sidebar):remove signout function

--- src/components/organisms/header/components/sideMenu/index.tsx
+++ src/components/organisms/header/components/sideMenu/index.tsx
@@ -1,45 +1,33 @@
 import { Link } from 'react-router-dom'
-import { Container, Text, Button } from 'components'
+import { Container, Text } from 'components'
 import { SideUserInfo } from '..'
 
 import { PropsSideMenu } from '../../types'
 import { SideContainer, ScrollContainer } from './styles'
 import { colors } from 'styles'
-import { useAuth } from 'contexts/auth'
 
-const SideMenu = ({ open, data, user, colorMode }: PropsSideMenu) => {
-  const { signOut } = useAuth()
-  return (
-    <SideContainer display="block" {...{ open }}>
-      <ScrollContainer
-        flexDirection="column"
-        display={open ? 'flex' : 'none'}
-        my={3}
-      >
-        <SideUserInfo {...{ user }} />
-        {data?.map((item: any) => (
-          <Link to={item.url} key={`Path-${item.id}`}>
-            <Container width={1} pl={3} py={3}>
-              <Text
-                style={{ textTransform: 'uppercase' }}
-                color={colors.secondaryText[colorMode]}
-              >
-                {item.label}
-              </Text>
-            </Container>
-          </Link>
-        ))}
-        <Container display="flex" align-items="center" width={1}>
-          <Button
-            label={'Sair'}
-            variant="ghost"
-            iconName="power"
-            onClick={signOut}
-          ></Button>
-        </Container>
-      </ScrollContainer>
-    </SideContainer>
-  )
-}
+const SideMenu = ({ open, data, user, colorMode }: PropsSideMenu) => (
+  <SideContainer display="block" {...{ open }}>
+    <ScrollContainer
+      flexDirection="column"
+      display={open ? 'flex' : 'none'}
+      my={3}
+    >
+      <SideUserInfo {...{ user }} />
+      {data?.map((item: any) => (
+        <Link to={item.url} key={`Path-${item.id}`}>
+          <Container width={1} pl={3} py={3}>
+            <Text
+              style={{ textTransform: 'uppercase' }}
+              color={colors.secondaryText[colorMode]}
+            >
+              {item.label}
+            </Text>
+          </Container>
+        </Link>
+      ))}
+    </ScrollContainer>
+  </SideContainer>
+)
 
 export { SideMenu }
