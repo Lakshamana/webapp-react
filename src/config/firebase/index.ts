3f525e920630955086dd30d7de74fde81e9502d3
biancasilva <bianca@fanhero.com>
2022-01-04 12:04:17 -0300

    fix(firebase-remote): change firebase remote settings for dev and prod

--- src/config/firebase/index.ts
+++ src/config/firebase/index.ts
@@ -7,8 +7,11 @@ import 'firebase/auth'
 import 'firebase/database'
 import 'firebase/remote-config'
 
+const { NODE_ENV } = process.env
+
 export const firebaseApp = initializeApp(FIREBASE_CONFIG)
 
 export const firebaseRemoteConfig = getRemoteConfig()
-firebaseRemoteConfig.settings.fetchTimeoutMillis = 60000
-firebaseRemoteConfig.settings.minimumFetchIntervalMillis = 1
+firebaseRemoteConfig.settings.fetchTimeoutMillis = 15000
+firebaseRemoteConfig.settings.minimumFetchIntervalMillis =
+  NODE_ENV === 'development' ? 1000 : 10800000
