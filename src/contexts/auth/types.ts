5e3c80904ce8906a33b82442c6c58c0d836935a9
biancasilva <bianca@fanhero.com>
2022-01-04 12:03:08 -0300

    feature(auth-context): add updateAccount method to auth context types

--- src/contexts/auth/types.ts
+++ src/contexts/auth/types.ts
@@ -1,6 +1,9 @@
+import { Profile } from 'generated/graphql';
+
 export interface AuthTypes {
   signed: boolean
   user: object | null
   signOut: () => Promise<void>
   updateUser: () => Promise<void>
+  updateAccount: (account: Profile) => Promise<void>
 }
