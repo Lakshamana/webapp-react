db4141de6ca098cea25aec52b0c438dd597fb900
biancasilva <bianca@fanhero.com>
2022-01-07 18:56:51 -0300

    feat(confirmAge): add new step for confirm_age form

---
+++ src/components/organisms/signupForm/components/GDPR/components/confirmAge/confirm_email/types.ts
@@ -0,0 +1,8 @@
+export interface ConfirmAgeProps {
+  handleFormSubmit: () => void
+  onCancel: () => void
+  userEmail?: string
+  gdprAge: string
+}
+
+export type ConfirmAgeSteps = 'Confirm' | 'Reconfirm'
