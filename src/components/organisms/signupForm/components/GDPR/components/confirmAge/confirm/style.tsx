cd163c4e0d01212544ac5fffb87d1618766fc368
biancasilva <bianca@fanhero.com>
2022-01-07 19:03:40 -0300

    fix: remve unused files

--- src/components/organisms/signupForm/components/GDPR/components/confirmAge/confirm/style.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { space, layout } from "styled-system";
-
-export const Circle = styled.div`
-  ${space}
-  ${layout}
-  display: flex;
-  border-radius: 50%;
-`;
