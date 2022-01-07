735d20e15e05b7b7057f4c20a888eae4d136dee0
biancasilva <bianca@fanhero.com>
2022-01-07 19:05:36 -0300

    feat(signup): use prop userEmail to send email entered on register form to confirm_email(conirmAge) form

--- src/components/organisms/signupForm/components/GDPR/components/confirmAge/types.ts
+++ src/components/organisms/signupForm/components/GDPR/components/confirmAge/types.ts
@@ -1,7 +1,8 @@
 export interface ConfirmAgeProps {
 	handleFormSubmit: () => void
 	onCancel: () => void
+	userEmail?: string
 	gdprAge: string
 }
 
-export type ConfirmAgeSteps = 'Confirm' | 'Reconfirm'
+export type ConfirmAgeSteps = 'Confirm' | 'Reconfirm' | 'ConfirmEmail'
--- src/components/organisms/signupForm/components/GDPR/types.ts
+++ src/components/organisms/signupForm/components/GDPR/types.ts
@@ -1,5 +1,6 @@
 export interface GDPRProps {
     handleFormSubmit: () => void
     onCancel: () => void
+    userEmail?: string
     isLoading: boolean
 }
