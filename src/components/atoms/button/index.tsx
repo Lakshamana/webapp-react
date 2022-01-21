560ae1fe903fa13c49050bf502d3150d8120a30f
biancasilva <bianca@fanhero.com>
2022-01-21 12:09:21 -0300

    fix(button): fix icon size

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -6,7 +6,7 @@ const Button = ({ label, children, iconName, size, ...props }: Props) => {
   return (
     <ButtonStyled
       {...props}
-      {...(iconName ? { leftIcon: <Icon width={20} icon={`mdi:${iconName}`} /> } : {})}
+      {...(iconName ? { leftIcon: <Icon width={18} icon={`mdi:${iconName}`} /> } : {})}
       {...(size ? { size } : { height: '56px' })}
     >
       {label}
