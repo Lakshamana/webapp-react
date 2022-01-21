9d689dc6c6ed6dc9f248f884507db09192a7fc0a
biancasilva <bianca@fanhero.com>
2022-01-21 11:01:31 -0300

    fix(tabs): fix active state

--- src/components/organisms/header/settings.ts
+++ src/components/organisms/header/settings.ts
@@ -1,82 +1,88 @@
-import { Channel } from "./types";
-import i18n from "config/i18n";
+import { Channel } from './types'
+import i18n from 'config/i18n'
 
 export const MENUTABS = [
   {
-    id: "home",
-    label: i18n.t("header.tabs.home"),
-    url: "/home",
-    style: { mx: "25px" },
+    id: 'home',
+    label: i18n.t('header.tabs.home'),
+    url: '/home',
+    style: { mx: '25px' },
   },
   {
-    id: "live",
-    label: i18n.t("header.tabs.live"),
-    url: "/",
-    style: { mx: "25px" },
+    id: 'live',
+    label: i18n.t('header.tabs.live'),
+    url: '/livechat',
+    style: { mx: '25px' },
   },
   {
-    id: "feed",
-    label: i18n.t("header.tabs.feed"),
-    url: "/feed",
-    style: { mx: "25px" },
+    id: 'feed',
+    label: i18n.t('header.tabs.feed'),
+    url: '/feed',
+    style: { mx: '25px' },
   },
   {
-    id: "collections",
-    label: i18n.t("header.tabs.collections"),
-    url: "/collections",
-    style: { mx: "25px" },
+    id: 'collections',
+    label: i18n.t('header.tabs.collections'),
+    url: '/collections',
+    style: { mx: '25px' },
   },
   {
-    id: "mylist",
-    label: i18n.t("header.tabs.my_list"),
-    url: "/mylist",
-    style: { ml: "25px" },
+    id: 'mylist',
+    label: i18n.t('header.tabs.my_list'),
+    url: '/mylist',
+    style: { ml: '25px' },
   },
-];
+  {
+    id: 'tags',
+    label: i18n.t('header.tabs.tags'),
+    url: '/tags',
+    style: { ml: '25px' },
+  }
+]
 
 export const CHANNELS: Array<Channel> = [
   {
-    id: "channel1",
-    name: "Channel name 1",
-    url: "https://www.logodesign.net/logo/abstract-cuboid-building-4519ld.png",
+    id: 'channel1',
+    name: 'Channel name 1',
+    url: 'https://www.logodesign.net/logo/abstract-cuboid-building-4519ld.png',
   },
   {
-    id: "channel2",
-    name: "Channel name 2",
-    url: "https://www.logodesign.net/logo/abstract-cuboid-building-4519ld.png",
+    id: 'channel2',
+    name: 'Channel name 2',
+    url: 'https://www.logodesign.net/logo/abstract-cuboid-building-4519ld.png',
   },
-];
+]
 
 export const DEFAULT_USER = {
-  name: "Bia Silva",
-  id: "11-user",
+  name: 'Bia Silva',
+  id: '11-user',
   avatar:
-    "https://pixinvent.com/materialize-material-design-admin-template/app-assets/images/user/12.jpg",
-};
+    'https://pixinvent.com/materialize-material-design-admin-template/app-assets/images/user/12.jpg',
+}
 
 export const SEARCH_VALUES = [
   {
-    label: "Channels",
-    id: "0-data",
+    label: 'Channels',
+    id: '0-data',
     children: [
-      { text: "Channel name 1", id: "1" },
-      { text: "Channel name 2", id: "2" },
+      { text: 'Channel name 1', id: '1' },
+      { text: 'Channel name 2', id: '2' },
     ],
   },
   {
-    label: "Content",
-    id: "1-data",
+    label: 'Content',
+    id: '1-data',
     children: [
-      { text: "Content title 1 (Channel name)", id: "11" },
-      { text: "Content title 2 (Channel name)", id: "2" },
+      { text: 'Content title 1 (Channel name)', id: '11' },
+      { text: 'Content title 2 (Channel name)', id: '2' },
     ],
   },
-];
+]
 
 export const initialState: any = {
-  search: "",
-  selected: "",
+  search: '',
+  selected: '',
   openMenu: false,
   openSearch: false,
   channel: CHANNELS[0],
-};
+}
