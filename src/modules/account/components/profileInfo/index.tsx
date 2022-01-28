ec8c7fb85614e7e21cf40d08566b65665f9e6042
biancasilva <bianca@fanhero.com>
2022-01-28 19:34:13 -0300

    feat(profileInfo): update card with DateInput component and modify some code to avoid unnecessry re-renders

--- src/modules/account/components/profileInfo/index.tsx
+++ src/modules/account/components/profileInfo/index.tsx
@@ -1,52 +1,56 @@
 import { useState, useEffect } from 'react'
-import { useFormik } from 'formik'
 import { useTranslation } from 'react-i18next'
-import { Flex } from '@chakra-ui/layout'
+import { useFormik } from 'formik'
+import { format, parseISO } from 'date-fns'
+import { ptBR, enUS } from 'date-fns/locale'
+
+import { Flex, Box } from '@chakra-ui/layout'
 import { Input } from '@chakra-ui/input'
-import { useAuthStore } from 'services/stores'
+import { Avatar, DateInput, Text } from 'components'
 
 import { pxToRem } from 'styles/metrics'
 
 import { useThemeStore } from 'services/stores/theme'
-import { Text, Button, Avatar } from 'components'
+
 import { colors } from 'styles'
 import { Label } from '../../styles'
 
-import { initialValues } from './settings'
+import { UpdateButtons } from '../updateButtons'
 import { ProfileData } from './types'
 
-const ProfileInfo = ({ updateProfile, isLoading }: ProfileData) => {
+const ProfileInfo = ({
+  updateProfile,
+  isLoading,
+  user,
+  locale,
+}: ProfileData) => {
   const [isEditing, setIsEditing] = useState(false)
   const { t } = useTranslation()
   const { colorMode } = useThemeStore()
-  const { user } = useAuthStore()
 
-  const { values, handleSubmit, handleChange, setValues } = useFormik({
-    initialValues: {
-      ...initialValues,
-    },
-    onSubmit: async () => {
-      updateProfile({ ...values })
-    },
-  })
-
-  const setInitialValues = () => {
-    setValues({
-      address: user?.address || '',
-      birthday: user?.birthday || null,
-      phone: user?.phone || '',
+  const { values, handleSubmit, handleChange, setFieldValue, resetForm } =
+    useFormik({
+      initialValues: {
+        address: user.address || '',
+        birthday: user.birthday || '',
+        phone: user.phone || '',
+      },
+      onSubmit: async () => {
+        updateProfile({ ...values, locale: locale })
+      },
     })
-  }
 
   const renderInputByType = (key: string, value: any) => {
     switch (key) {
-      // case 'birthday':
-      //   return (
-      //     <DateInput
-      //       startValue={new Date(value)}
-      //       onChange={(value) => setFieldValue(key, value)}
-      //     ></DateInput>
-      //   )
+      case 'birthday':
+        return (
+          <DateInput
+            startValue={new Date(value)}
+            onChange={(date) => {
+              setFieldValue(key, date)
+            }}
+          ></DateInput>
+        )
       default:
         return (
           <Input
@@ -64,62 +68,38 @@ const ProfileInfo = ({ updateProfile, isLoading }: ProfileData) => {
     setIsEditing(false)
   }, [user])
 
-  useEffect(() => {
-    setInitialValues()
-    // eslint-disable-next-line
-  }, [])
-
   return (
     <>
       <Flex width={'100%'} alignItems="left" direction="column">
-        <Flex justifyContent="center" pb={5}>
+        <Flex justifyContent="center" py={5}>
           <Avatar size="xl" src={user?.avatar_url || ''}></Avatar>
         </Flex>
         {Object.keys(values).map((key) => {
           return (
-            <Text key={key} color={colors.secondaryText[colorMode]} py={'10px'}>
-              <Label fontSize={pxToRem(16)} fontWeight="500">
-                {t(`page.account.${key}`)}:
-              </Label>
-              {!isEditing ? values[key] : renderInputByType(key, values[key])}
-            </Text>
+            <Box color={colors.secondaryText[colorMode]} key={key}>
+              <Text color={colors.secondaryText[colorMode]} py={'10px'}>
+                <Label fontSize={pxToRem(16)} fontWeight="500">
+                  {t(`page.account.${key}`)}:
+                </Label>
+                {!isEditing &&
+                  (key === 'birthday' && values[key]
+                    ? format(parseISO(values[key]), 'P', {
+                        locale: locale === 'pt-BR' ? ptBR : enUS,
+                      })
+                    : values[key])}
+              </Text>
+              {isEditing && renderInputByType(key, values[key])}
+            </Box>
           )
         })}
       </Flex>
-      <Flex alignItems="center" justifyContent="end" mt={10}>
-        {!isEditing ? (
-          <Button
-            size="sm"
-            width={'auto'}
-            variant="link"
-            label="Update"
-            onClick={() => setIsEditing(true)}
-          ></Button>
-        ) : (
-          <>
-            <Button
-              size="sm"
-              mr={5}
-              iconName={'content-save'}
-              width={'auto'}
-              variant="link"
-              isLoading={isLoading}
-              label={t('common.save')}
-              onClick={() => handleSubmit()}
-            ></Button>
-            <Button
-              size="sm"
-              width={'auto'}
-              variant="link"
-              label={t('common.cancel')}
-              onClick={() => {
-                setIsEditing(false)
-                setInitialValues()
-              }}
-            ></Button>
-          </>
-        )}
-      </Flex>
+      <UpdateButtons
+        editFormActive={isEditing}
+        isLoadingAction={isLoading}
+        handleIsEditing={(value) => setIsEditing(value)}
+        handleSubmit={handleSubmit}
+        resetValues={resetForm}
+      ></UpdateButtons>
     </>
   )
 }
