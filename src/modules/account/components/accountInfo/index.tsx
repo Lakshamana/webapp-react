207183106866a9b168b401804d5e7b219689d88d
biancasilva <bianca@fanhero.com>
2022-01-28 19:34:29 -0300

    feat(profileInfo): modify some code to avoid unnecessry re-renders

--- src/modules/account/components/accountInfo/index.tsx
+++ src/modules/account/components/accountInfo/index.tsx
@@ -1,77 +1,57 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useFormik } from 'formik'
 import { useThemeStore } from 'services/stores/theme'
-import { Flex, Box } from '@chakra-ui/layout'
+
+import { Flex } from '@chakra-ui/layout'
 import { Input } from '@chakra-ui/input'
-import { Text, Button } from 'components'
-import { useAuthStore } from 'services/stores'
+import { Text } from 'components'
+
 import { colors } from 'styles'
 import { Label } from '../../styles'
-import { AccountData } from './types'
 import { pxToRem } from 'styles/metrics'
-import { useEffect } from 'react'
-import { initialValues } from './settings'
 
-const AccountInfo = ({ updateAccount, isLoading }: AccountData) => {
+import { UpdateButtons } from '../updateButtons'
+import { AccountData } from './types'
+
+const AccountInfo = ({ updateAccount, isLoading, account }: AccountData) => {
   const [isEditing, setIsEditing] = useState(false)
   const { t } = useTranslation()
   const { colorMode } = useThemeStore()
-  const { account } = useAuthStore()
 
-  const { values, handleSubmit, handleChange, setValues } = useFormik({
+  const { values, handleSubmit, handleChange, resetForm } = useFormik({
     initialValues: {
-      ...initialValues,
+      username: account.username || '',
+      display_name: account.display_name || '',
+      first_name: account.first_name || '',
+      last_name: account.last_name || '',
     },
     onSubmit: async () => {
       updateAccount({ ...values })
     },
   })
 
-  const setInitialValues = () => {
-    setValues({
-      username: account?.username || '',
-      first_name: account?.first_name || '',
-      last_name: account?.last_name || '',
-      display_name: account?.display_name || '',
-    })
-  }
-
   useEffect(() => {
     setIsEditing(false)
   }, [account])
 
-  useEffect(() => {
-    setInitialValues()
-    // eslint-disable-next-line
-  }, [])
-
   return (
     <>
       <Flex width={'100%'} alignItems="left" direction="column">
-        <Text
-          color={colors.secondaryText[colorMode]}
-          py={'10px'}
-        >
+        <Text color={colors.secondaryText[colorMode]} py={'10px'}>
           <Label fontSize={pxToRem(16)} fontWeight="500">
             {t(`page.account.email`)}:
           </Label>
-          {account?.email || ''}
+          {account.email}
         </Text>
 
         {Object.keys(values).map((key) => {
           return (
-            <Text
-              key={key}
-              color={colors.secondaryText[colorMode]}
-              py={'10px'}
-            >
+            <Text key={key} color={colors.secondaryText[colorMode]} py={'10px'}>
               <Label fontSize={pxToRem(16)} fontWeight="500">
                 {t(`page.account.${key}`)}:
               </Label>
-              {!isEditing ? (
-                values[key]
-              ) : (
+              {isEditing ? (
                 <Input
                   onChange={handleChange}
                   width={'100%'}
@@ -79,45 +59,20 @@ const AccountInfo = ({ updateAccount, isLoading }: AccountData) => {
                   name={`${key}`}
                   value={values[key]}
                 />
+              ) : (
+                values[key]
               )}
             </Text>
           )
         })}
       </Flex>
-      <Flex alignItems="center" justifyContent="end" mt={10}>
-        {!isEditing ? (
-          <Button
-            size="sm"
-            width={'auto'}
-            variant="link"
-            label="Update"
-            onClick={() => setIsEditing(true)}
-          ></Button>
-        ) : (
-          <>
-            <Button
-              size="sm"
-              mr={5}
-              iconName={'content-save'}
-              width={'auto'}
-              variant="link"
-              isLoading={isLoading}
-              label={t('common.save')}
-              onClick={() => handleSubmit()}
-            ></Button>
-            <Button
-              size="sm"
-              width={'auto'}
-              variant="link"
-              label={t('common.cancel')}
-              onClick={() => {
-                setIsEditing(false)
-                setInitialValues()
-              }}
-            ></Button>
-          </>
-        )}
-      </Flex>
+      <UpdateButtons
+        editFormActive={isEditing}
+        isLoadingAction={isLoading}
+        handleIsEditing={(value) => setIsEditing(value)}
+        handleSubmit={handleSubmit}
+        resetValues={resetForm}
+      ></UpdateButtons>
     </>
   )
 }
--- src/modules/account/components/accountInfo/types.ts
+++ src/modules/account/components/accountInfo/types.ts
@@ -1,4 +1,7 @@
+import { Account } from 'generated/graphql';
+
 export interface AccountData {
   updateAccount: (account: Object) => void
   isLoading: boolean
+  account: Account
 }
