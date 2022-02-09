b019194e08317d3c94822063e0cd9ed2c21cbfdb
biancasilva <bianca@fanhero.com>
2022-02-09 18:08:55 -0300

    fix(template): fix loginLayout and mainLayout flex props

--- src/components/templates/loginLayout/style.tsx
+++ src/components/templates/loginLayout/style.tsx
@@ -1,26 +1,19 @@
-import styled from "styled-components";
-import { Props } from "./types"
-import { breakpoints, sizes } from 'styles';
-import { StyleContainer } from "components";
+import styled from 'styled-components'
+import { Props } from './types'
+import { StyleContainer } from 'components'
 
-export const ChildContainer = styled(StyleContainer) <Props>`
-    display: flex;
-    align-items: center;
-    height: auto;
-    min-height: calc(100vh - ${sizes.footerDesktopHeight} - ${sizes.headerDesktopHeight});
-    background: ${(props: Props) => (`url('${props.backgroundImage}')`)};
-    background-size: cover;
-    background-position: center top;
-    background-attachment: fixed;
-    @media screen and (max-width: ${breakpoints.md}) {
-      min-height: calc(100vh - ${sizes.footerMobileHeight} - ${sizes.headerMobileHeight});
-  }
-`;
+export const ChildContainer = styled(StyleContainer)<Props>`
+  flex: 1;
+  align-items: center;
+  background: ${(props: Props) => `url('${props.backgroundImage}')`};
+  background-size: cover;
+  background-position: center top;
+  background-attachment: fixed;
+`
 
 export const LayoutContainer = styled(StyleContainer)`
   display: flex;
-  width: 100%;
-  min-height: 100vh;  
-  overflow-x: hidden;
-  position: relative;
-`;
+  flex-direction: column;
+  height: 100%;
+  min-height: 100vh;
+`
--- src/components/templates/mainLayout/style.ts
+++ src/components/templates/mainLayout/style.ts
@@ -1,26 +1,15 @@
-import styled from "styled-components"
-import { StyleContainer } from "components"
-import { breakpoints, sizes } from "styles"
+import styled from 'styled-components'
+import { StyleContainer } from 'components'
 
 export const ChildContainer = styled(StyleContainer)`
-	display: flex;
-	height: max-content;
-	min-height: calc(
-		100vh - ${sizes.footerMobileHeight} - ${sizes.headerMobileHeight}
-	);
-
-	@media screen and (min-width: ${breakpoints.md}) {
-		min-height: calc(
-			100vh - ${sizes.footerDesktopHeight} - ${sizes.headerDesktopHeight}
-		);
-	  }
+  flex: 1;
 `
 
 export const LayoutContainer = styled(StyleContainer)`
-	display: flex;
-	width: 100%;
-	min-height: 100vh;
-	overflow-x: hidden;
-	position: relative;
-	background-color: ${({ theme }) => theme.colors.bodyBg[theme.colorMode]};
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.bodyBg[theme.colorMode]};
+  overflow-x: hidden;
+  min-height: 100vh;
 `
