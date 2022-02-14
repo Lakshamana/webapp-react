ad97d489a60f25bf5a14351d226900083f0767a0
biancasilva <bianca@fanhero.com>
2022-02-14 17:26:57 -0300

    fix(cardsScroller): hide left arrow when scroll is on initial state

--- src/components/molecules/cardsScroller/index.tsx
+++ src/components/molecules/cardsScroller/index.tsx
@@ -1,35 +1,33 @@
-import React from "react"
-import SwiperCore, { Navigation, Pagination } from "swiper"
-import { PostsProps } from "./types"
-import { SWIPPER_PARAMS } from "./settings"
-import { SwiperStyled } from "./style"
-import { Content } from "./style"
-import "swiper/swiper-bundle.min.css"
-import { RANDOM_ID } from "utils"
+import SwiperCore, { Navigation, Pagination } from 'swiper'
+import { PostsProps } from './types'
+import { SWIPPER_PARAMS } from './settings'
+import { SwiperStyled } from './style'
+import { Content } from './style'
+import 'swiper/swiper-bundle.min.css'
+import { RANDOM_ID } from 'utils'
 
 SwiperCore.use([Navigation, Pagination])
 
 const CardsScroller = ({ children, type }: PostsProps) => {
-	let randomId = `${type}-${RANDOM_ID()}`
+  let randomId = `${type}-${RANDOM_ID()}`
 
-	SWIPPER_PARAMS["navigation"] = {
-		prevEl: `.swiper-button-prev.${randomId}`,
-		nextEl: `.swiper-button-next.${randomId}`
-	}
+  SWIPPER_PARAMS['navigation'] = {
+    prevEl: `.swiper-button-prev.${randomId}`,
+    nextEl: `.swiper-button-next.${randomId}`,
+    disabledClass: 'disabled_swiper_button',
+  }
 
-	return (
-		<React.Fragment>
-			<Content>
-				<SwiperStyled {...SWIPPER_PARAMS}>
-					<div className='swiper-wrapper'>
-						{children}
-					</div>
-					<div className="swiper-pagination-cards cards-scroller-pagination" />
-					<div className={`swiper-button-prev ${randomId}`} />
-					<div className={`swiper-button-next ${randomId}`} />
-				</SwiperStyled>
-			</Content>
-		</React.Fragment>
-	)
+  return (
+    <Content>
+      <SwiperStyled {...SWIPPER_PARAMS}>
+        <div className="swiper-wrapper">{children}</div>
+        <div className="swiper-pagination-cards cards-scroller-pagination" />
+        <div
+          className={`swiper-button-prev ${randomId} disabled_swiper_button`}
+        />
+        <div className={`swiper-button-next ${randomId}`} />
+      </SwiperStyled>
+    </Content>
+  )
 }
 export { CardsScroller }
