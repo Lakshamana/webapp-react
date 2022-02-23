c5ec6095e13077f130c14bcce7540a6da62acea5
biancasilva <bianca@fanhero.com>
2022-02-23 19:45:02 -0300

    fix(mainLayout): fix text indentation

--- src/components/templates/mainLayout/index.tsx
+++ src/components/templates/mainLayout/index.tsx
@@ -1,17 +1,19 @@
-import { Header, InternalFooter } from "components";
-import { ChildContainer, LayoutContainer } from "./style";
-import { Props, defaultProps } from "./types";
+import { Header, InternalFooter } from 'components'
+import { ChildContainer, LayoutContainer } from './style'
+import { Props, defaultProps } from './types'
 
-const MainLayout = ({ children, ...props }: Props) => (
-  <LayoutContainer flexDirection="column">
-    <Header />
-    <ChildContainer pb={30} justifyContent={'center'} {...props} >
-      {children}
-    </ChildContainer>
-    <InternalFooter />
-  </LayoutContainer>
-);
+const MainLayout = ({ children, ...props }: Props) => {
+  return (
+    <LayoutContainer flexDirection="column">
+      <Header />
+      <ChildContainer pb={30} justifyContent={'center'} {...props}>
+        {children}
+      </ChildContainer>
+      <InternalFooter />
+    </LayoutContainer>
+  )
+}
 
-MainLayout.defaultProps = defaultProps;
+MainLayout.defaultProps = defaultProps
 
-export { MainLayout };
+export { MainLayout }
