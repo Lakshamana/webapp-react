5edd17c5f732c56498260ffc3b4a7201b8b20eec
biancasilva <bianca@fanhero.com>
2022-02-23 19:24:05 -0300

    feat(types): add new global types

---
+++ src/types/channel.ts
@@ -0,0 +1,11 @@
+export interface ChannelProps {
+    id?: string;
+    name?: string;
+    image?: string;
+    url?: string;
+    description?: string;
+    isGeolocked?: boolean;
+    isExclusive?: boolean;
+    onClick?: any;
+    thumbnail?: string;
+  }
