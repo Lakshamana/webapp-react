2564e708271fbbc8c256be39cde89cbf6b8ca68c
biancasilva <bianca@fanhero.com>
2022-02-23 18:48:29 -0300

    fix(post-cards): fix color of blocked icon

--- src/components/atoms/livestreamPostCard/index.tsx
+++ src/components/atoms/livestreamPostCard/index.tsx
@@ -1,40 +1,38 @@
-import { Icon } from "@iconify/react"
-import { useThemeStore } from 'services/stores/theme';
-import { Text } from "components"
-import { LivePostProps, defaultProps } from "./types"
+import { Icon } from '@iconify/react'
+import { Text } from 'components'
+import { LivePostProps, defaultProps } from './types'
 import { colors } from 'styles'
-import { PostContent, ExclusiveBlocked, GeolockedBlocked, Live } from "./style"
+import { PostContent, ExclusiveBlocked, GeolockedBlocked, Live } from './style'
 
 const LivestreamPostCard = ({ ...props }: LivePostProps) => {
-  const { colorMode } = useThemeStore();
-
-	return (
-		<PostContent {...props}>
-			{props.isExclusive ? (
-				<ExclusiveBlocked>
-					<Icon width={20} icon={`mdi:lock`} color={colors.generalText[colorMode]}></Icon>
-				</ExclusiveBlocked>
-			) : "" || props.isGeolocked ? (
-				<GeolockedBlocked>
-					<Icon width={24} icon={`mdi:earth`} color={colors.generalText[colorMode]}></Icon>
-				</GeolockedBlocked>
-			) : (
-				""
-			)}
-			{props.isLive ? (
-				<Live>
-					<Text
-						kind='headline'
-						children={"Live"}
-						textAlign={"center"}
-						fontSize={12}
-						color={`${colors.white}`}></Text>
-				</Live>
-			) : (
-				""
-			)}
-		</PostContent>
-	)
+  return (
+    <PostContent {...props}>
+      {props.isExclusive ? (
+        <ExclusiveBlocked>
+          <Icon width={20} icon={`mdi:lock`} color={'white'}></Icon>
+        </ExclusiveBlocked>
+      ) : '' || props.isGeolocked ? (
+        <GeolockedBlocked>
+          <Icon width={24} icon={`mdi:earth`} color={'white'}></Icon>
+        </GeolockedBlocked>
+      ) : (
+        ''
+      )}
+      {props.isLive ? (
+        <Live>
+          <Text
+            kind="headline"
+            children={'Live'}
+            textAlign={'center'}
+            fontSize={12}
+            color={`${colors.white}`}
+          ></Text>
+        </Live>
+      ) : (
+        ''
+      )}
+    </PostContent>
+  )
 }
 
 LivestreamPostCard.defaultProps = defaultProps
--- src/components/atoms/videoPostCards/index.tsx
+++ src/components/atoms/videoPostCards/index.tsx
@@ -1,38 +1,34 @@
-import { useThemeStore } from 'services/stores/theme';
-import { VideoPostProps, defaultProps } from "./types"
-import { 
-	CardWrapper, 
-	PostContent, 
-	ExclusiveBlocked, 
-	GeolockedBlocked 
-} from "./style"
-import { colors } from 'styles'
+import { VideoPostProps, defaultProps } from './types'
+import {
+  CardWrapper,
+  PostContent,
+  ExclusiveBlocked,
+  GeolockedBlocked,
+} from './style'
 
-import { Icon } from "@iconify/react";
-import { HoverInfoCard } from '../hoverInfoCard';
+import { Icon } from '@iconify/react'
+import { HoverInfoCard } from '../hoverInfoCard'
 
 const VideoPostCard = ({ ...props }: VideoPostProps) => {
-  const { colorMode } = useThemeStore();
+  return (
+    <CardWrapper>
+      <PostContent className="postContent" {...props}>
+        {props.isExclusive ? (
+          <ExclusiveBlocked>
+            <Icon width={20} icon={`mdi:lock`} color={'white'}></Icon>
+          </ExclusiveBlocked>
+        ) : '' || props.isGeolocked ? (
+          <GeolockedBlocked>
+            <Icon width={24} icon={`mdi:earth`} color={'white'}></Icon>
+          </GeolockedBlocked>
+        ) : (
+          ''
+        )}
+      </PostContent>
 
-	return (
-		<CardWrapper>
-			<PostContent className="postContent" {...props}>
-				{props.isExclusive ? (
-					<ExclusiveBlocked>
-						<Icon width={20} icon={`mdi:lock`} color={colors.generalText[colorMode]}></Icon>
-					</ExclusiveBlocked>
-				) : "" || props.isGeolocked ? (
-					<GeolockedBlocked>
-						<Icon width={24} icon={`mdi:earth`} color={colors.generalText[colorMode]}></Icon>
-					</GeolockedBlocked>
-				) : (
-					""
-				)}
-			</PostContent>
-
-			<HoverInfoCard {...props} />
-		</CardWrapper>
-	)
+      <HoverInfoCard {...props} />
+    </CardWrapper>
+  )
 }
 
 VideoPostCard.defaultProps = defaultProps
