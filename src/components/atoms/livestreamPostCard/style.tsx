f8492153975dd40166ec67a8f0ab2b6ea3c6be4f
biancasilva <bianca@fanhero.com>
2022-02-28 18:41:07 -0300

    fix(livestream-post-card): fix styles and render data from API

--- src/components/atoms/livestreamPostCard/style.tsx
+++ src/components/atoms/livestreamPostCard/style.tsx
@@ -1,70 +1,53 @@
-import styled from "styled-components"
-import { LivePostProps } from "./types"
-import { breakpoints, colors } from "styles"
+import styled from 'styled-components'
+import { LivestreamPostCardProps } from 'types/livestreams'
+import { breakpoints, colors } from 'styles'
 
-export const PostContent = styled.div<LivePostProps>`
-	display: flex;
-	width: auto;
-	padding-top: 56.25%;
-	height: auto;
-	position: relative;
-	border-radius: 4px;
-	cursor: pointer;
-	${(props: LivePostProps) => `background: url('${props.coverImage}');`}
-	background-position: center;
-	background-repeat: no-repeat;
-	background-size: cover;
+export const PostContent = styled.div<LivestreamPostCardProps>`
+  display: flex;
+  width: auto;
+  padding-top: 56.25%;
+  height: auto;
+  position: relative;
+  border-radius: 4px;
+  cursor: pointer;
+  ${(props: LivestreamPostCardProps) =>
+    `background: url('${props.thumbnail}');`}
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 `
 
-export const ExclusiveBlocked: any = styled.div`
-	display: flex;
-	position: absolute;
-	align-items: center;
-	justify-content: center;
-	width: 51px;
-	height: 50px;
-	border-radius: 50%;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-	background-color: ${colors.blue["300"]};
+export const BlockedContent: any = styled.div`
+  display: flex;
+  position: absolute;
+  align-items: center;
+  justify-content: center;
+  width: 51px;
+  height: 50px;
+  border-radius: 50%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: ${({ theme }) =>
+    theme.colors.brand.indicator[theme.colorMode]};
 
-	@media screen and (max-width: ${breakpoints.sm}) {
-		width: 41px;
-		height: 40px;
-	}
-`
-
-export const GeolockedBlocked: any = styled.div`
-	display: flex;
-	position: absolute;
-	align-items: center;
-	justify-content: center;
-	width: 51px;
-	height: 50px;
-	border-radius: 50%;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-	background-color: ${colors.blue["300"]};
-	pointer-events: none;
-
-	@media screen and (max-width: ${breakpoints.sm}) {
-		width: 41px;
-		height: 40px;
-	}
+  @media screen and (max-width: ${breakpoints.sm}) {
+    width: 41px;
+    height: 40px;
+  }
 `
 
 export const Live: any = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: 44px;
-	height: 20px;
-	background-color: ${colors.red["700"]};
-	border-radius: 2px;
-	position: absolute;
-	top: 8px;
-	left: 10px;
-	text-transform: uppercase;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 44px;
+  height: 20px;
+  background-color: ${colors.red['700']};
+  border-radius: 2px;
+  position: absolute;
+  top: 8px;
+  right: 10px;
+  text-transform: uppercase;
+  font-weight: bolder;
 `
