320b856bd780d5eb8e490eef9876bc94cd0559a0
Marcus Vinicius Sgorla Franklin <kiinho@gmail.com>
2022-02-28 18:53:05 -0300

    feature: finish feed posts with infinite scroll

--- src/modules/feed/index.tsx
+++ src/modules/feed/index.tsx
@@ -4,30 +4,66 @@ import { formatDistance, intervalToDuration } from 'date-fns'
 import { QUERY_POSTS } from "services/graphql/query/posts"
 import { useLazyQuery } from "@apollo/client"
 import { ThumborInstanceTypes, useThumbor } from "services/hooks/useThumbor"
-import { useChannelStore } from "services/stores"
+import { useChannelsStore } from 'services/stores'
+import { colors } from 'styles'
+import { Center, Spinner } from "@chakra-ui/react"
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 const FeedPage = () => {
+  const limit = 10
+
   const { generateImage } = useThumbor()
-	const filterList = [
+  const { activeChannel } = useChannelsStore()
+
+  const filterList = [
 		{ value: 'recent', label: 'Most Recent' },
 		{ value: 'old', label: 'Most Old' },
 		{ value: 'popular', label: 'Popular' },
 	]
 
-  const { channel } = useChannelStore()
-
-	const [ filterBy, SetFilterBy ] = useState();
+  const [filterBy, SetFilterBy] = useState();
   const [posts, setPosts] = useState([]);
+  const [hasMore, setHasMore] = useState(true)
+
+  const getOrderBy = () => {
+    console.log('filter', filterBy)
+    if (filterBy === 'popular')
+      return [{ name: "popular", direction: "DESC" }]
+
+    if (filterBy === 'old')
+      return [{ name: "publishedAt", direction: "ASC" }]
+
+    return [{ name: "publishedAt", direction: "DESC" }]
+  }
+
+  useEffect(() => {
+    setPosts([])
+    loadPosts({
+      variables: {
+        limit,
+        skip: 0,
+        orderBy: getOrderBy(),
+      }
+    })
+  }, [filterBy])
 
 	const handleFilterChange = (evt: any) => {
     const { value } = evt?.target;
     SetFilterBy(value)
   }
 
-  const [loadPosts, { data: dataPosts }] = useLazyQuery(
-    QUERY_POSTS, 
-    { variables: {} }
-  )
+  const [loadPosts, { data: dataPosts }] = useLazyQuery(QUERY_POSTS)
+
+  const loadMorePosts = () => {
+    loadPosts({
+      context: { headers: { channel: activeChannel?.id || '5c9277169a57aca84e2cdced' } },
+      variables: {
+        limit,
+        skip: posts.length,
+        orderBy: getOrderBy(),
+      }
+    })
+  }
 
   const getUrl = (obj) =>
     generateImage(
@@ -42,7 +78,12 @@ const FeedPage = () => {
     )
 
   useEffect(() => {
-    if (!dataPosts || !dataPosts?.posts?.length) return
+    if (!dataPosts) return
+
+    if (dataPosts?.posts?.length === 0) {
+      setHasMore(false)
+      return
+    }
 
     const reduced = dataPosts.posts.map(post => {
       const duration = post?.duration 
@@ -95,25 +136,23 @@ const FeedPage = () => {
         isGeolocked: false,
       }
     })
-    console.log('reduced', reduced)
-    setPosts(reduced)
+
+    setPosts(posts.concat(reduced))
   }, [dataPosts])
 
-  useEffect(() => {
-    console.log('channel?.id', channel?.id)
+   useEffect(() => {
+    // if (!activeChannel?.id) return
+    if (!activeChannel?.id) console.log('--- not channel', 'activeChannel?.id', activeChannel?.id)
+
+    setPosts([])
     loadPosts({
-      context: { headers: { channel: channel?.id || '5c9277169a57aca84e2cdced' } },
-      variables: {
-        limit: 20,
-        skip: 0,
-        orderBy: [{ name: "publishedAt", direction: "DESC" }],
-      }
+      context: { headers: { channel: activeChannel?.id || '5c9277169a57aca84e2cdced' } },
+      variables: { limit, skip: 0, orderBy: getOrderBy() }
     })
-  }, [channel])
+  }, [activeChannel])
 
 	return (
     <Container flexDirection={"column"} width={"100%"}>
-
       <Container 
         flexDirection={"column"}
         width={"100%"}
@@ -128,122 +167,21 @@ const FeedPage = () => {
         />
       </Container>
 
-      {!!posts?.length && posts.map((post) => (<FeedPostCard {...post} />))}
-
-      {/* <FeedPostCard
-        postTitle='Tile of the post'
-        postDescription='This is the description of the post...'
-        date='3 days ago'
-        hasActivity={true}
-        coverImage='https://cdn.dooca.store/996/files/breaking-bad.jpg'
-        type='Image'
-        mediaLength=''
-        views={20}
-        countMessages={1500}
-        audioTitle='Audio test'
-        audioArtist='Joãozinho'
-        voteCount={20}
-        timeRemaining='2'
-        itemQuestion='Question test'
-        percentage='50%'
-        voted={true}
-      />
-
-      <FeedPostCard
-        postTitle='Tile of the post'
-        postDescription='This is the description of the post...'
-        date='3 days ago'
-        hasActivity={true}
-        coverImage='https://img.ibxk.com.br/2019/09/05/05150900742176.jpg'
-        type='Video'
-        mediaLength='01:27:00'
-        views={20}
-        countMessages={3200}
-        audioTitle='Audio test'
-        audioArtist='Joãozinho'
-        voteCount={20}
-        timeRemaining='2'
-        itemQuestion='Question test'
-        percentage='50%'
-        voted={true}
-      />
-
-      <FeedPostCard
-        postTitle='Tile of the post'
-        postDescription='This is the description of the post...'
-        date='3 days ago'
-        hasActivity={true}
-        coverImage='https://img.ibxk.com.br/2019/09/05/05150900742176.jpg'
-        type='Blog'
-        mediaLength=''
-        views={20}
-        countMessages={30}
-        audioTitle='Audio test'
-        audioArtist='Joãozinho'
-        voteCount={20}
-        timeRemaining='2'
-        itemQuestion='Question test'
-        percentage='50%'
-        voted={true}
-      />
-
-      <FeedPostCard
-        postTitle='Tile of the post'
-        postDescription='This is the description of the post...'
-        date='3 days ago'
-        hasActivity={true}
-        coverImage='https://img.ibxk.com.br/2019/09/05/05150900742176.jpg'
-        type='Poll'
-        mediaLength=''
-        views={20}
-        countMessages={30}
-        audioTitle='Audio test'
-        audioArtist='Joãozinho'
-        voteCount={20}
-        timeRemaining='2 days'
-        itemQuestion='Question test'
-        itensQuestions={[
-          {
-            id: 1,
-            item: 'This might be a good option',
-            percentage: '30%',
-            winner: false
-          },
-          {
-            id: 2,
-            item: 'This is the one that will win!',
-            percentage: '50%',
-            winner: true
-          },
-          {
-            id: 3,
-            item: 'This is not going anywhere...!',
-            percentage: '20%',
-            winner: false
-          },
-        ]}
-        percentage='60%'
-        voted={true}
-      />
-
-      <FeedPostCard
-        postTitle='Tile of the post'
-        postDescription='This is the description of the post...'
-        date='3 days ago'
-        hasActivity={true}
-        coverImage='https://br.web.img2.acsta.net/newsv7/20/05/21/22/45/4919243.jpg'
-        type='Audio'
-        mediaLength='04:52'
-        views={20}
-        countMessages={2000}
-        audioTitle='Audio test'
-        audioArtist='Joãozinho'
-        voteCount={20}
-        timeRemaining='2 days'
-        itemQuestion='Question test'
-        percentage='50%'
-        voted={true}
-      /> */}
+      <InfiniteScroll
+        dataLength={posts.length}
+        next={loadMorePosts}
+        hasMore={hasMore}
+        loader={(
+          <Center width="100%">
+            <Spinner size={'xl'} color={colors.brand.primary['dark']} />
+          </Center>
+        )}
+        endMessage={<></>}
+      >
+        {!!posts?.length && posts.map((post) => (
+          <FeedPostCard {...post} />
+        ))}
+      </InfiniteScroll>
     </Container>
 	)
 }
