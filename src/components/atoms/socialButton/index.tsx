704c0e86ededa9b656bc1c77da3a231230f65a5e
João Pedro Moraes <joaopedromoraes@outlook.com>
2022-03-09 12:32:45 -0300

    refact(socialButton): refact social icons button

--- src/components/atoms/socialButton/index.tsx
+++ src/components/atoms/socialButton/index.tsx
@@ -1,14 +1,19 @@
 import { SocialSigninButtonProps } from './types';
-import { Icon } from "@iconify/react";
-import { useThemeStore } from 'services/stores/theme';
 import { BoxButton } from './styles';
-import { colors } from 'styles';
+import { ReactComponent as AppleIcon } from "assets/companies/apple.svg";
+import { ReactComponent as FacebookIcon } from "assets/companies/facebook.svg";
+import { ReactComponent as GoogleIcon } from "assets/companies/google.svg";
 
 const SocialSigninButton = ({ kind, ...props }: SocialSigninButtonProps) => {
-    const { colorMode } = useThemeStore();
-    const icons = { facebook: 'fa-brands:facebook-square', apple: 'fa-brands:apple', google: 'fa-brands:google' }
+    const icons: any = {
+        facebook: <FacebookIcon width={24} />,
+        apple: <AppleIcon width={24} />,
+        google: <GoogleIcon width={24} />
+    };
     return (
-        <BoxButton {...props}><Icon width={28} color={colors.generalText[colorMode]} icon={icons[kind]} /></BoxButton>
+        <BoxButton {...props}>
+            { icons[kind] }
+        </BoxButton>
     )
 };
 
