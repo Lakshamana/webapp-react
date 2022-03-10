10e988cb5fc6b3e0970bd2a71822ddcf7b597dc8
biancasilva <bianca@fanhero.com>
2022-03-10 13:59:26 -0300

    feat(route): verify if has active channel searching param in url before enter on route

--- src/router/components/clientRoute/index.tsx
+++ src/router/components/clientRoute/index.tsx
@@ -1,6 +1,9 @@
 import { useEffect } from 'react'
-import { Route, Redirect } from 'react-router-dom'
+import { Route, Redirect, useLocation } from 'react-router-dom'
 import { Props } from './types'
+import { convertDashToCamel } from 'utils'
+import { useChannelsStore } from 'services/stores'
+import { useAuth } from 'contexts/auth'
 
 const ClientRoute = ({
   component: Component,
@@ -10,6 +13,29 @@ const ClientRoute = ({
   template: Template,
   ...rest
 }: Props) => {
+  const { activeChannel } = useChannelsStore()
+  const { updateActiveChannel } = useAuth()
+  const location = useLocation()
+
+  useEffect(() => {
+    if (!activeChannel) {
+      const channelUrl = location.pathname
+      const hasChannelInRoute = channelUrl.indexOf('/c/')
+      if (hasChannelInRoute !== -1) {
+        const channelInRoute = channelUrl?.substring(
+          channelUrl.indexOf('c/') + 2
+        )
+        const hasSlash = channelInRoute.indexOf('/')
+        const channelName =
+          hasSlash > -1
+            ? channelInRoute.substring(0, channelInRoute.indexOf('/'))
+            : channelInRoute
+
+        updateActiveChannel(convertDashToCamel(channelName))
+      }
+    }
+  }, [activeChannel])
+
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [path])
