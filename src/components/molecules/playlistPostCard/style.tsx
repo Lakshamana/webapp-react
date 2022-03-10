7b02601b6950dfd01452bf07c849cb9a0df9dec3
biancasilva <bianca@fanhero.com>
2022-03-10 13:44:03 -0300

    fix(playlistPostCard): fix playlist post cards style

--- src/components/molecules/playlistPostCard/style.tsx
+++ src/components/molecules/playlistPostCard/style.tsx
@@ -1,24 +1,15 @@
-import styled from "styled-components"
-import { Flex, Center } from "@chakra-ui/react"
-import { pxToRem } from "styles/metrics"
-
-export const List: any = styled(Flex)`
-  width: ${pxToRem(500)};
-  flex-direction: column;
-`
+import styled from 'styled-components'
+import { Flex } from '@chakra-ui/react'
+import { pxToRem } from 'styles/metrics'
+import { breakpoints } from 'styles'
 
 export const Wrapper: any = styled(Flex)`
-  flex: 1;
-  padding: ${pxToRem(12)};
   border-radius: 3px;
   cursor: pointer;
 
-  svg {
-    display: none;
-  }
-
   &:hover {
-    background-color: ${({theme}) => theme.colors?.grey[theme.colorMode !== 'dark' ? 650 : 700]};
+    background-color: ${({ theme }) =>
+      theme.colors?.grey[theme.colorMode !== 'dark' ? 650 : 850]};
 
     svg {
       display: block;
@@ -26,36 +17,40 @@ export const Wrapper: any = styled(Flex)`
   }
 `
 
-export const VideoThumb: any = styled(Center)`
-  flex: 1;
-  border-radius: 3px;
-  background-image: url('${({image}) => image}');
-  background-position: center center;
-`
-
 export const InfoBox: any = styled(Flex)`
   flex: 1;
   flex-direction: column;
-  padding: 0 ${pxToRem(12)};
+  padding-left: ${pxToRem(12)};
 `
 
 export const Title: any = styled(Flex)`
-  color: ${({ theme }) => theme.colorMode !== 'dark' ? 'black' : 'white'};
-  font-size: ${pxToRem(20)};
-  font-weight: 600;
+  color: ${({ theme }) => theme.colors.generalText[theme.colorMode]};
+  font-size: 1.2rem;
+  font-weight: bold;
   text-overflow: ellipsis;
   display: -webkit-box;
-  -webkit-line-clamp: 3;
+  -webkit-line-clamp: 1;
   -webkit-box-orient: vertical;
   overflow: hidden;
+  padding-bottom: 2px;
+
+  @media screen and (max-width: ${breakpoints.sm}) {
+    font-size: 1.1rem;
+    line-height: 1.5rem;
+    padding-bottom: 5px;
+  }
 `
 
 export const Subtitle: any = styled(Flex)`
-  color: ${({ theme }) => theme.colorMode !== 'dark' ? 'black' : 'white'};
-  font-size: ${pxToRem(14)};
-  font-weight: 300;
+  color: ${({ theme }) => theme.colors.secondaryText[theme.colorMode]};
+  font-size: 0.9rem;
+  line-height: 1.2rem;
   display: -webkit-box;
   -webkit-line-clamp: 3;
   -webkit-box-orient: vertical;
   overflow: hidden;
+
+  @media screen and (max-width: ${breakpoints.sm}) {
+    font-size: 0.9rem;
+  }
 `
