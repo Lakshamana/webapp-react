c88c9cd9a1ff52e64346555ffb8d9c803577bf6b
biancasilva <bianca@fanhero.com>
2022-03-10 13:43:24 -0300

    fix(videoPlaylist): fix style and render data from API

--- src/components/molecules/videoPlaylist/index.tsx
+++ src/components/molecules/videoPlaylist/index.tsx
@@ -1,38 +1,119 @@
 import { useState } from 'react'
-import { Flex, Text } from '@chakra-ui/react';
-import { List } from './style'
-import { VideoPlaylistProps } from "./types";
-import { ToggleButton } from '../../atoms';
-import { PlaylistPostCard } from "../playlistPostCard";
-import { theme } from "styles/theme"
-import { useThemeStore } from 'services/stores/theme';
+import { Flex } from '@chakra-ui/react'
+import { useThemeStore, useChannelsStore } from 'services/stores'
+import { Text, ToggleButton, PlaylistPostCard } from 'components'
+import { RedactReason } from 'generated/graphql'
+import { VideoPlaylistProps } from './types'
+import { theme } from 'styles/theme'
+import { colors } from 'styles'
+import { useEffect } from 'react'
+import { VideoPostCardProps, VideoPostProps, RedactedVideo } from 'types/posts'
+import { ThumborInstanceTypes, useThumbor, ThumborParams } from 'services/hooks'
+import { convertCamelCaseToDash } from 'utils'
 
 const VideoPlaylist = ({ title, videos, autoplay }: VideoPlaylistProps) => {
-  const [checked, setChecked] = useState(!!autoplay)
+  const { colorMode } = useThemeStore()
+  const { generateImage } = useThumbor()
+  const { activeChannel } = useChannelsStore()
 
-  const { colorMode } = useThemeStore();
+  const [checked, setChecked] = useState(true)
+  const [playlist, setPlaylist] = useState<VideoPostCardProps[]>()
+
+  const getImageUrl = (post: VideoPostProps) => {
+    const imageOptions: ThumborParams = {
+      size: {
+        height: 400,
+      },
+    }
+
+    if (isExclusive(post) || isGeolocked(post)) {
+      imageOptions.blur = 20
+    }
+
+    const image = generateImage(
+      ThumborInstanceTypes.IMAGE,
+      post.thumbnail?.imgPath || '',
+      imageOptions
+    )
+
+    return image
+  }
+
+  const getPostUrl = (id: string) => {
+    return `/c/${convertCamelCaseToDash(activeChannel?.name)}/post/${id}`
+  }
+
+  const isRedacted = (post: VideoPostProps) => {
+    return (
+      post.__typename === 'RedactedOnDemandPost' ||
+      post.__typename === 'RedactedVideoPost'
+    )
+  }
+
+  const isExclusive = (post: VideoPostProps) => {
+    return (
+      (isRedacted(post) &&
+        (post as RedactedVideo).reason === RedactReason.Exclusive) ||
+      false
+    )
+  }
+
+  const isGeolocked = (post: VideoPostProps) => {
+    return (
+      (isRedacted(post) &&
+        (post as RedactedVideo).reason === RedactReason.Geofence) ||
+      false
+    )
+  }
+
+  useEffect(() => {
+    const mapped = videos?.map((item) => {
+      return {
+        id: `${item.id}`,
+        title: `${item.title}`,
+        url: getPostUrl(item.id || ''),
+        description: `${item.description}`,
+        thumbnail: getImageUrl(item),
+        mediaLength: item.media?.duration || 0,
+        countviews: item.counts?.countViewsTotal || 0,
+        isExclusive: isExclusive(item),
+        isGeolocked: isGeolocked(item),
+      }
+    })
+    setPlaylist(mapped)
+  }, [videos])
+
+  const renderPlaylist = () => {
+    return playlist?.map((item) => <PlaylistPostCard {...item} />)
+  }
 
   return (
-    <List>
-      <Flex mb={theme.pxToRem(12)} paddingX={theme.pxToRem(12)}>
-        <Text 
-          fontSize={theme.pxToRem(22)} 
-          isTruncated 
-          fontWeight={500} 
-          color={colorMode !== 'dark' ? 'black' : 'white'}
+    <Flex flexDirection="column" mb={5}>
+      {title && (
+        <Text
+          fontWeight="bolder"
+          fontSize="1.4rem"
+          mb={2}
+          color={colors.generalText[colorMode]}
         >
           {title}
         </Text>
-      </Flex>
-      <Flex alignItems="center" paddingX={theme.pxToRem(12)}>
-        <ToggleButton checked={!!checked} onChange={() => setChecked(!checked)} />
-        <Text ml={theme.pxToRem(12)} color={colorMode !== 'dark' ? 'black' : 'white'}>
+      )}
+      <Flex alignItems="center" mb={2}>
+        <ToggleButton
+          checked={!!checked}
+          onChange={() => setChecked(!checked)}
+        />
+        <Text
+          ml={theme.pxToRem(12)}
+          color={theme.colors.generalText[colorMode]}
+        >
           Autoplay next video
         </Text>
       </Flex>
-      <PlaylistPostCard videos={videos} />
-    </List>
+      {playlist?.length && renderPlaylist()}
+    </Flex>
   )
 }
 
-export { VideoPlaylist };
+export { VideoPlaylist }
