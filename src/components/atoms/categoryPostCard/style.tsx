e4ba33589e44fd0886a432f32b3c639c8d8ff344
biancasilva <bianca@fanhero.com>
2022-03-12 11:37:03 -0300

    feat(categoryPostCard): fix hover, add info on hover and add PIN nd UNPIN functions

--- src/components/atoms/categoryPostCard/style.tsx
+++ src/components/atoms/categoryPostCard/style.tsx
@@ -1,40 +1,12 @@
 import styled from 'styled-components'
 import { CategoryPostCardProps } from 'types/categories'
-import { breakpoints } from 'styles'
 
 export const CardWrapper = styled.div`
   cursor: pointer;
   position: relative;
-  transition: all 0.2s ease-in-out;
-
-  &:hover {
-    z-index: 1;
-    transform: scale(1.3);
-    margin-top: -30%;
-
-    .addMyListIconBg,
-    .addMyListIcon {
-      display: block !important;
-    }
-
-    @media screen and (max-width: ${breakpoints.sm}) {
-      .closeCardMobileIconBg,
-      .closeCardMobileIcon {
-        display: block !important;
-      }
-    }
-
-    .postContent {
-      border-radius: 4px 4px 0 0 !important;
-    }
-
-    .infoCard {
-      display: flex;
-      border-radius: 0 0 4px 4px;
-      height: 80px;
-      transition: all 0.2s ease-in-out;
-    }
-  }
+  transition: transform 1s;
+  transition-delay: 2s;
+  background-color: black;
 `
 
 export const PostContent = styled.div<CategoryPostCardProps>`
@@ -49,4 +21,9 @@ export const PostContent = styled.div<CategoryPostCardProps>`
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
+
+  &:hover {
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+  }
 `
