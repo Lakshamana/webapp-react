24256916bc53f0cebdacedcd2c5662958605a484
biancasilva <bianca@fanhero.com>
2022-03-17 20:37:54 -0300

    feat(typescript): add Maybe type

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="react-scripts" />
+
+declare type Maybe<T> = T | null
+declare type MaybeUndef<T> = T | null | undefined
+
+declare interface Dictionary<T = any> {
+  [key: string]: T
+}
