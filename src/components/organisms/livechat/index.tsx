adde0ad169de5547663487bfecc608fb1bc0aade
biancasilva <bianca@fanhero.com>
2022-03-18 14:51:40 -0300

    fix(livechat): Remove unused code

--- src/components/organisms/livechat/index.tsx
+++ src/components/organisms/livechat/index.tsx
@@ -1,56 +1,10 @@
-import { useState, useRef } from 'react'
-import { Props } from './types'
-import {
-  collection,
-  query,
-  orderBy,
-  limitToLast,
-  addDoc,
-} from '@firebase/firestore'
-
-import { useCollectionData } from 'react-firebase-hooks/firestore'
-import { timestampNow } from 'utils/firebase'
-
+import { useState } from 'react'
 import { Flex } from '@chakra-ui/react'
+import { Props } from './types'
 import { LivechatFooter, LivechatHeader, LivechatBody } from './components'
-import { firebaseDB } from 'config/firebase'
-import { useEffect } from 'react'
-import { useAuthStore } from 'services/stores'
-import { MessageDocumentData } from 'types/firebase'
 
 const Livechat = ({ entityId, onPressEnter }: Props) => {
   const [value, setValue] = useState('')
-  const [startFrom, setStartFrom] = useState(2)
-  const { account, user } = useAuthStore()
-
-  const dummy = useRef()
-  const messagesRef = collection(firebaseDB, `livestream/${entityId}/comments`)
-
-  const queryMessage = query(
-    messagesRef,
-    orderBy('dateAdded'),
-    limitToLast(100)
-  )
-  const [messages] = useCollectionData(queryMessage, {})
-
-  const message: MessageDocumentData = {
-    userId: user?.id || '',
-    username: account?.username || '',
-    avatarPath: user?.avatar_path || '',
-    updatedAt: timestampNow(),
-    text: 'add new message',
-    dateAdded: timestampNow(),
-  }
-
-  const sendMessage = async () => {
-    addDoc(messagesRef, message)
-  }
-
-  useEffect(() => {
-    sendMessage()
-  }, [])
-
-  useEffect(() => {}, [messages])
 
   return (
     <Flex
