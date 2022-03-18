8b895ebe2772d346dc0559fe566f5eab79222203
biancasilva <bianca@fanhero.com>
2022-03-17 22:29:26 -0300

    feat(firebase): add firebase utils and types

---
+++ src/utils/firebase.ts
@@ -0,0 +1,21 @@
+import { Timestamp } from '@firebase/firestore'
+import { FirebaseQuerySnapshot } from 'types/firebase'
+
+export const timestampFromDate = (date: Date): Timestamp => {
+  return Timestamp.fromDate(date)
+}
+
+export const timestampfromMillis = (milliseconds: number): Timestamp => {
+  return Timestamp.fromMillis(milliseconds)
+}
+
+export const timestampNow = (): Timestamp => {
+  return Timestamp.now()
+}
+
+export const parseResultSnapshot = (snapshot: FirebaseQuerySnapshot) => {
+  return snapshot
+    .docChanges()
+    .filter((row) => row.type === 'added')
+    .map((row) => row.doc)
+}
