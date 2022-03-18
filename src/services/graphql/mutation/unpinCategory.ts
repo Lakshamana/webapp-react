126f7b16e465480448836b0dcb355d3992c42587
biancasilva <bianca@fanhero.com>
2022-03-18 16:21:17 -0300

    fix(query): add name to UnpinCategory query

--- src/services/graphql/mutation/unpinCategory.ts
+++ src/services/graphql/mutation/unpinCategory.ts
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client'
 
 export const MUTATION_UNPIN_CATEGORY = gql`
-  mutation ($categoryId: String!) {
+  mutation UnpinCategory($categoryId: String!) {
     unpinCategory(categoryId: $categoryId) {
       id
       pinnedAt
