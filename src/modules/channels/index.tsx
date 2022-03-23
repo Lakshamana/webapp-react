60c4bab753e2722f0813dc692dd8040cfdf256e9
biancasilva <bianca@fanhero.com>
2022-03-23 19:08:15 -0300

    feat(channels): add setPageTitle, reorder imports and fix some code bugs

--- src/modules/channels/index.tsx
+++ src/modules/channels/index.tsx
@@ -1,20 +1,22 @@
+import { useEffect } from 'react'
 import { useQuery } from '@apollo/client'
 import { useTranslation } from 'react-i18next'
 import { useHistory } from 'react-router'
-import { convertCamelCaseToDash } from 'utils'
-import { Channel } from 'generated/graphql'
 import { QUERY_CHANNELS } from 'services/graphql'
 import { useThemeStore } from 'services/stores/theme'
-import { useChannelsStore } from 'services/stores'
+import { useChannelsStore, useCommonStore } from 'services/stores'
+import { Channel } from 'generated/graphql'
 import { Container, Text, Skeleton } from 'components'
 import { ChannelsGrid } from './components'
 import { colors } from 'styles'
+import { convertCamelCaseToDash } from 'utils'
 
 const ChannelsPage = () => {
   const { t } = useTranslation()
   const history = useHistory()
   const { colorMode } = useThemeStore()
   const { setChannelsList, channelsList, setActiveChannel } = useChannelsStore()
+  const { setPageTitle } = useCommonStore()
 
   const { data, loading } = useQuery(QUERY_CHANNELS, {
     variables: {
@@ -22,15 +24,24 @@ const ChannelsPage = () => {
     },
     onCompleted: (result) => {
       setChannelsList(result.channels)
-    }
+    },
   })
 
   const selectChannel = async (channelId: string | null) => {
-    let selected = channelsList.filter((channel: Channel) => channel.id === channelId)
-    let channelRoute = convertCamelCaseToDash(selected[0].name)
-    await setActiveChannel(selected[0])
-    history.push(`/c/${channelRoute}`)
+    let selected = channelsList?.filter(
+      (channel: Channel) => channel.id === channelId
+    )
+    if (selected?.length) {
+      let channelRoute = convertCamelCaseToDash(selected[0].name)
+      await setActiveChannel(selected[0])
+      history.push(`/c/${channelRoute}`)
+    }
   }
+
+  useEffect(() => {
+    setPageTitle(t('page.channels.title'))
+    //eslint-disable-next-line
+  }, [])
 
   return (
     <Container defaultPadding flexDirection="column" width={'100%'}>
@@ -43,12 +54,12 @@ const ChannelsPage = () => {
       >
         {t('page.channels.title')}
       </Text>
-      {loading && <Skeleton kind='cards' numberOfCards={5}></Skeleton>}
+      {loading && <Skeleton kind="cards" numberOfCards={5}></Skeleton>}
       {!!data?.channels?.length && !loading && (
         <ChannelsGrid
           channelSelected={selectChannel}
           channelsList={data.channels}
-        ></ChannelsGrid>
+        />
       )}
     </Container>
   )
