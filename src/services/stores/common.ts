ef5b3261a0a3b6df944ef84cd9607948e2e0cbd2
biancasilva <bianca@fanhero.com>
2022-03-23 18:32:43 -0300

    feat(store): add common store

---
+++ src/services/stores/common.ts
@@ -0,0 +1,13 @@
+import create from 'zustand'
+
+type CommonState = {
+  pageTitle: string | null
+  setPageTitle: (title: string) => void
+}
+
+export const useCommonStore = create<CommonState>((set) => ({
+  pageTitle: '',
+  setPageTitle: (pageTitle: string) => {
+    return set({ pageTitle })
+  },
+}))
