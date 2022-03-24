cb6fbd0e76075a4ec1a9bd360e82e3d62e639338
biancasilva <bianca@fanhero.com>
2022-03-24 11:23:18 -0300

    feat(sideMenu): show menu API data on sideMenu

--- src/components/organisms/header/components/sideMenu/index.tsx
+++ src/components/organisms/header/components/sideMenu/index.tsx
@@ -1,53 +1,73 @@
 import { Link } from 'react-router-dom'
 import { useTabsStore } from 'services/stores'
+import { Box } from '@chakra-ui/layout'
 import { Container, Text } from 'components'
 import { PropsSideMenu } from '../../types'
 import { SideContainer, ScrollContainer, Circle } from './styles'
 import { colors } from 'styles'
 
-const SideMenu = ({ open, colorMode, children }: PropsSideMenu) => {
+const SideMenu = ({ open, colorMode, children, data }: PropsSideMenu) => {
   const { activeTab, setActiveTab, tabsList } = useTabsStore()
+
   return (
     <SideContainer display="block" {...{ open }}>
       {children}
-      <ScrollContainer
-        flexDirection="column"
-        display={open ? 'flex' : 'none'}
-        my={3}
-        px={'20px'}
-      >
-        {tabsList?.map((item: any) => {
-          const isSelected = activeTab?.id === item.id
-          return (
-            <Link
-              to={item.url}
-              key={`Path-${item.id}`}
-              onClick={() => setActiveTab(item)}
-            >
+      <ScrollContainer flexDirection="column" my={3} px={'20px'}>
+        <Box>
+          {tabsList?.map((item: any) => {
+            const isSelected = activeTab?.id === item.id
+            return (
+              <Link
+                to={item.url}
+                key={`Path-${item.id}`}
+                onClick={() => setActiveTab(item)}
+              >
+                <Container
+                  width={1}
+                  py={3}
+                  alignItems={'center'}
+                  justifyContent={'left'}
+                >
+                  {isSelected && <Circle />}
+                  <Text
+                    style={{
+                      textTransform: 'uppercase',
+                      fontWeight: isSelected ? 'bold' : 'normal',
+                    }}
+                    color={
+                      isSelected
+                        ? colors.generalText[colorMode]
+                        : colors.secondaryText[colorMode]
+                    }
+                  >
+                    {item.label}
+                  </Text>
+                </Container>
+              </Link>
+            )
+          })}
+        </Box>
+        <Box>
+          {data?.map((menu: any) => (
+            <Link to={menu.url} key={`menu-${menu.id}`} onClick={() => {}}>
               <Container
                 width={1}
                 py={3}
                 alignItems={'center'}
                 justifyContent={'left'}
               >
-                {isSelected && <Circle />}
                 <Text
                   style={{
                     textTransform: 'uppercase',
-                    fontWeight: isSelected ? 'bold' : 'normal',
                   }}
-                  color={
-                    isSelected
-                      ? colors.generalText[colorMode]
-                      : colors.secondaryText[colorMode]
-                  }
+                  color={colors.generalText[colorMode]}
                 >
-                  {item.label}
+                  {menu.name}
                 </Text>
               </Container>
             </Link>
-          )
-        })}
+          ))}
+        </Box>
       </ScrollContainer>
     </SideContainer>
   )
