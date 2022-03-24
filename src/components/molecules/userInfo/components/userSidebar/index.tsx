748acb013b0ed6dd83c9d13713ffd0cc99ed61d1
biancasilva <bianca@fanhero.com>
2022-03-24 11:25:52 -0300

    fix(userSidebar): fix styles and add buttons to trigger same actions on user popover

--- src/components/molecules/userInfo/components/userSidebar/index.tsx
+++ src/components/molecules/userInfo/components/userSidebar/index.tsx
@@ -1,24 +1,53 @@
-import { Container, Text, Avatar } from 'components'
+import { Container, Text, Avatar, Button } from 'components'
+import { useThemeStore } from 'services/stores'
 
 import { PropsUserSidebar } from './types'
 import { UserContainer } from './styles'
+import { Flex } from '@chakra-ui/react'
 
-const UserSidebar = ({ account }: PropsUserSidebar) => (
-  <UserContainer
-    py={'15px'}
-    m={2}
-    display="flex"
-    flexDirection="column"
-    justifyContent="center"
-    alignItems="center"
-  >
-    <Container ml={2}>
-      <Avatar width={'45px'} height={'45px'} src={account?.avatar} />
-    </Container>
-    <Container px={2} mt={2} fontSize={'1.1rem'} fontWeight={'bolder'}>
-      <Text ellipsis>{account?.username || account?.display_name}</Text>
-    </Container>
-  </UserContainer>
-)
+const UserSidebar = ({ account }: PropsUserSidebar) => {
+  const { colorMode } = useThemeStore()
+  return (
+    <UserContainer
+      py={'25px'}
+      m={2}
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Container ml={2}>
+        <Avatar width={'55px'} height={'55px'} src={account?.avatar} />
+      </Container>
+      <Container px={2} mt={2} fontSize={'1.1rem'} fontWeight={'bolder'}>
+        <Text ellipsis>{account?.username || account?.display_name}</Text>
+      </Container>
+      <Flex gridGap={3} pt={5}>
+        <Button
+          iconName="cog-outline"
+          variant="outline"
+          label="Edit"
+          size="sm"
+        ></Button>
+        <Button
+          iconName={
+            colorMode === 'dark'
+              ? 'white-balance-sunny'
+              : 'moon-waning-crescent'
+          }
+          variant="outline"
+          label={colorMode === 'dark' ? 'light' : 'dark'}
+          size="sm"
+        ></Button>
+        <Button
+          iconName="power"
+          variant="outline"
+          label="Sair"
+          size="sm"
+        ></Button>
+      </Flex>
+    </UserContainer>
+  )
+}
 
 export { UserSidebar }
