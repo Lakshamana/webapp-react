c7e82e43f79b6b54d536b1a94ac273d51e2b1ab7
biancasilva <bianca@fanhero.com>
2022-03-25 11:17:15 -0300

    feat(query): add Category query

---
+++ src/services/graphql/queries/categories/category.ts
@@ -0,0 +1,36 @@
+import { gql } from '@apollo/client'
+
+export const QUERY_CATEGORY = gql`
+  query GetCategory($id: String!) {
+    category(id: $id) {
+      access
+      createdAt
+      customization {
+        desktop {
+          imgPath
+        }
+        mobile {
+          imgPath
+        }
+        thumbnail {
+          imgPath
+        }
+      }
+      children(pagination: { limit: 10 }) {
+        description
+        featuredAt
+        geoFence
+        id
+        name
+        description
+        tag
+      }
+      description
+      featuredAt
+      geoFence
+      id
+      name
+      tag
+    }
+  }
+`
