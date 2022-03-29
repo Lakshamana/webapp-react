4018eedacb58151092770c26f5683734efe9feb4
biancasilva <bianca@fanhero.com>
2022-03-29 13:07:36 -0300

    fix(channelsGrid): use media query ID to render thumbs

--- src/modules/channels/components/channelsGrid/index.tsx
+++ src/modules/channels/components/channelsGrid/index.tsx
@@ -1,31 +1,29 @@
 import { SimpleGrid } from '@chakra-ui/react'
 import { ChannelCard } from 'components'
-import { ThumborInstanceTypes, useThumbor, ThumborParams } from 'services/hooks'
 import { Channel } from 'generated/graphql'
 import { Props } from './types'
 
 const ChannelsGrid = ({ channelsList, channelSelected }: Props) => {
-  const { generateImage } = useThumbor()
+  //TODO: This piece of code should be reused when API works correctly
+  // const getImageUrl = (channel: Channel) => {
+  //   const imageOptions: ThumborParams = {
+  //     size: {
+  //       height: 400,
+  //     },
+  //   }
 
-  const getImageUrl = (channel: Channel) => {
-    const imageOptions: ThumborParams = {
-      size: {
-        height: 400,
-      },
-    }
+  //   if (isGeolocked(channel)) {
+  //     imageOptions.blur = 20
+  //   }
 
-    if (isGeolocked(channel)) {
-      imageOptions.blur = 20
-    }
+  //   const image = generateImage(
+  //     ThumborInstanceTypes.IMAGE,
+  //     channel.customization?.thumbnail?.imgPath as string,
+  //     imageOptions
+  //   )
 
-    const image = generateImage(
-      ThumborInstanceTypes.IMAGE,
-      channel.customization?.thumbnail?.imgPath as string,
-      imageOptions
-    )
-
-    return image
-  }
+  //   return image
+  // }
 
   const isGeolocked = (channel: Channel) =>
     channel.__typename === 'GeolockedChannel'
@@ -40,7 +38,10 @@ const ChannelsGrid = ({ channelsList, channelSelected }: Props) => {
           key={item.id}
           onClick={(value) => channelSelected(value)}
           isGeolocked={isGeolocked(item)}
-          image={getImageUrl(item)}
+          //TODO: This piece of code should be reused when API works correctly
+          // image={getImageUrl(item)}
+          //TODO: This piece of code should be removed when API works correctly
+          mediaId={item.customization?.thumbnail as string}
         ></ChannelCard>
       ))}
     </SimpleGrid>
