af65b66d216ee570f8689d8279557d2f54e58f6c
biancasilva <bianca@fanhero.com>
2022-03-31 15:53:29 -0300

    style(videoPostCard): add style to play icon

--- src/components/atoms/videoPostCards/style.tsx
+++ src/components/atoms/videoPostCards/style.tsx
@@ -47,3 +47,21 @@ export const BlockedContent: any = styled.div`
     height: 40px;
   }
 `
+
+export const PlayContent: any = styled.div`
+  display: flex;
+  position: absolute;
+  align-items: center;
+  justify-content: center;
+  width: 51px;
+  height: 50px;
+  border-radius: 50%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  @media screen and (max-width: ${breakpoints.sm}) {
+    width: 41px;
+    height: 40px;
+  }
+`
