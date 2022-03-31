032b64cd9d7c3baf134ed06faf48f3bf383e9053
Renato Siqueira <renatoelysiqueira@gmail.com>
2022-03-31 20:21:53 -0300

    fix: replaced to updated query

--- src/services/graphql/queries/posts/posts.ts
+++ src/services/graphql/queries/posts/posts.ts
@@ -3,20 +3,30 @@ import { gql } from '@apollo/client'
 export const QUERY_POSTS = gql`
   query GetPosts($filter: PostFilter) {
     posts(filters: $filter) {
-      access
-      description
-      geofence
-      kind
-      id
-      slug
-      status
-      tags
-      thumbnail {
-        imgPath
+      hasNextPage
+      hasPreviousPage
+      isFirstPage
+      isLastPage
+      page
+      pageCount
+      total
+      rows {
+        access
+        description
+        geofence
+        kind
+        id
+        slug
+        status
+        tags
+        thumbnail {
+          imgPath
+        }
+        title
+        type
+        publishedAt
+        countComments
       }
-      title
-      type
-      publishedAt
     }
   }
 `
