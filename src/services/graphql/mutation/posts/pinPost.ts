32c14779100a614de327e9e130dd701d7cdf75d0
biancasilva <bianca@fanhero.com>
2022-04-06 17:24:01 -0300

    feat(mutation): add pinPost mutation

---
+++ src/services/graphql/mutation/posts/pinPost.ts
@@ -0,0 +1,11 @@
+import { gql } from '@apollo/client'
+
+export const MUTATION_PIN_POST = gql`
+  mutation PinPost($payload: CreateAccountPinnedPost!) {
+    pinPost(payload: $payload) {
+      id
+      pinnedAt
+      __typename
+    }
+  }
+`
