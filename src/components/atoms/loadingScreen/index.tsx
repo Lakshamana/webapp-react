94471cb65dd6fc99cdc41ffe88d8867f979d714c
biancasilva <bianca@fanhero.com>
2022-04-06 10:59:27 -0300

    feat(loadingScreen): remove color from spinner

--- src/components/atoms/loadingScreen/index.tsx
+++ src/components/atoms/loadingScreen/index.tsx
@@ -1,16 +1,25 @@
 import { Spinner, Flex } from '@chakra-ui/react'
+import { useThemeStore } from 'services/stores'
 import { colors } from 'styles'
 
-const LoadingScreen = () => (
-  <Flex
-    width="100vw"
-    height={'100vh'}
-    alignItems={'center'}
-    justifyContent={'center'}
-    backgroundColor={colors.bodyBg['dark']}
-  >
-    <Spinner size={'xl'} color={colors.brand.primary['dark']}></Spinner>
-  </Flex>
-)
+const LoadingScreen = () => {
+  const { colorMode } = useThemeStore()
+  return (
+    <Flex
+      width="100vw"
+      height={'100vh'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      backgroundColor={colors.bodyBg['dark']}
+    >
+      <Spinner
+        speed="0.65s"
+        thickness={'3px'}
+        size={'xl'}
+        color={colors.secondaryText[colorMode]}
+      ></Spinner>
+    </Flex>
+  )
+}
 
 export { LoadingScreen }
