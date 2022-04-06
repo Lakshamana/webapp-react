08ddfe6fef49f333f2e90318d2f478f35e5a0e00
biancasilva <bianca@fanhero.com>
2022-04-06 18:14:11 -0300

    feat(mainLayout): initialize one signal

--- src/components/templates/mainLayout/index.tsx
+++ src/components/templates/mainLayout/index.tsx
@@ -1,8 +1,13 @@
+import { useEffect } from 'react'
 import { Header, InternalFooter } from 'components'
 import { ChildContainer, LayoutContainer } from './style'
 import { Props, defaultProps } from './types'
+import { initializeOneSignal } from 'utils/pushNotifications'
 
 const MainLayout = ({ children, ...props }: Props) => {
+  useEffect(() => {
+    initializeOneSignal()
+  }, [])
   return (
     <LayoutContainer flexDirection="column">
       <Header />
