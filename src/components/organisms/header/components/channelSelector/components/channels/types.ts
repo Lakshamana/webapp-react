ddfb1b7e41eacda3573611787d99766288cc3544
biancasilva <bianca@fanhero.com>
2022-04-11 15:24:13 -0300

    fix(channelselector): change selected channel type

--- src/components/organisms/header/components/channelSelector/components/channels/types.ts
+++ src/components/organisms/header/components/channelSelector/components/channels/types.ts
@@ -1,9 +1,10 @@
 import { Channel } from 'generated/graphql'
 import { ColorMode } from 'types/common'
+import { ChannelStorageData } from 'types/channel'
 
 export interface PropsChannels {
   channels: Channel[]
-  selected: Channel | null
+  selected: ChannelStorageData | null
   onSelect: any
   colorMode: ColorMode
   isLoading?: boolean
