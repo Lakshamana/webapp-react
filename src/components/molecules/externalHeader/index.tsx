5e082111978c4e216e87326a158f2674a83dd28f
biancasilva <bianca@fanhero.com>
2022-04-11 15:21:48 -0300

    fix(externalHeader): fix logo width

--- src/components/molecules/externalHeader/index.tsx
+++ src/components/molecules/externalHeader/index.tsx
@@ -1,9 +1,11 @@
-import { Logo, LanguageSelector, Container } from 'components'
+import { useMediaQuery } from '@chakra-ui/media-query'
 import { Box } from '@chakra-ui/layout'
+import { useCustomizationStore } from 'services/stores'
+import { ThumborInstanceTypes, useThumbor } from 'services/hooks'
+import { Logo, LanguageSelector, Container } from 'components'
 import { ExternalHeaderProps } from './types'
 import { BoxHeader, HeaderItems } from './style'
-import { ThumborInstanceTypes, useThumbor } from 'services/hooks'
-import { useCustomizationStore } from 'services/stores'
+import { breakpoints } from 'styles'
 
 const ExternalHeader = ({
   rightContent,
@@ -11,6 +13,7 @@ const ExternalHeader = ({
 }: ExternalHeaderProps) => {
   const { organizationConfig } = useCustomizationStore()
   const { generateImage } = useThumbor()
+  const [isDesktop] = useMediaQuery(`(min-width: ${breakpoints.sm})`)
 
   const orgLogo = () => {
     if (!organizationConfig?.IMAGES?.LOGO) return ''
@@ -28,7 +31,7 @@ const ExternalHeader = ({
       <HeaderItems>
         <Logo
           ignoreFallback
-          width={180}
+          maxWidth={isDesktop ? '180px' : '120px'}
           marginRight={[3, 4]}
           py={20}
           src={orgLogo()}
