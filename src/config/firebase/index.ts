59f2af73cd23c0c284eccdd4d4ece3a9de6bc2d6
biancasilva <bianca@fanhero.com>
2022-04-11 12:28:28 -0300

    feat(firebase): add env to set remote config fetch interval

--- src/config/firebase/index.ts
+++ src/config/firebase/index.ts
@@ -7,11 +7,17 @@ import 'firebase/auth'
 import 'firebase/database'
 import 'firebase/remote-config'
 
-const { NODE_ENV } = process.env
+const { NODE_ENV, REACT_APP_REMOTE_CONFIG_MINIMUM_FETCH_INTERVAL_MILLIS } =
+  process.env
 
 export const firebaseApp = initializeApp(FIREBASE_CONFIG)
+
+const developmentFetchInterval =
+  REACT_APP_REMOTE_CONFIG_MINIMUM_FETCH_INTERVAL_MILLIS || '1000'
+
+  console.log(developmentFetchInterval)
 
 export const firebaseRemoteConfig = getRemoteConfig()
 firebaseRemoteConfig.settings.fetchTimeoutMillis = 15000
 firebaseRemoteConfig.settings.minimumFetchIntervalMillis =
-  NODE_ENV === 'development' ? 1000 : 10800000
+  NODE_ENV === 'development' ? parseInt(developmentFetchInterval) : 10800000
