613874adc3787e17947142afa6ad9316fd3b02ec
Renato Siqueira <renatoelysiqueira@gmail.com>
2022-04-13 10:13:34 -0300

    included colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -86,6 +86,10 @@ const defaultColors: any = {
     light: '#E2E9E9',
     dark: '#20242A',
   },
+  comments: {
+    light: '#444444',
+    dark: '#666666',
+  },
   alerts: {
     success: {
       default: '#33DA80',
