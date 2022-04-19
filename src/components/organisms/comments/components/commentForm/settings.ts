ed2b774d7bc0a49ce963976c0235c612a74fd5a7
Renato Siqueira <renatoelysiqueira@gmail.com>
2022-04-19 10:31:10 -0300

    moved to another folder/refactored

--- src/components/organisms/comments/components/commentForm/settings.ts
+++
@@ -1,9 +0,0 @@
-import * as Yup from 'yup'
-
-export const initialValues = {
-  description: ''
-}
-
-export const validationSchema = Yup.object().shape({
-  description: Yup.string().required()
-})
