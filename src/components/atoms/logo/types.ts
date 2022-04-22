04653d4444a40d490b3f35671353bab2e5d3d3da
biancasilva <bianca@fanhero.com>
2022-04-22 11:49:49 -0300

    feat(logo): ad link to org/channel logo

--- src/components/atoms/logo/types.ts
+++ src/components/atoms/logo/types.ts
@@ -3,6 +3,7 @@ import { ImageProps } from '@chakra-ui/react'
 export interface Props extends ImageProps {
   height?: number
   width?: string | string[] | number | number[]
+  clickable?: boolean
 }
 
 export const defaultProps = {
