d5ba7cd037983fdb3cc11abeb5e77891d01746a1
Renato Siqueira <renatoelysiqueira@gmail.com>
2022-04-22 15:10:10 -0300

    Feat: included full query for now

---
+++ src/services/graphql/queries/live/liveEvent.ts
@@ -0,0 +1,58 @@
+import { gql } from '@apollo/client'
+
+export const QUERY_LIVE_EVENT = gql`
+  query GetLiveEvent($slug: String) {
+    liveEvent(slug: $slug) {
+      access
+      channel
+      commentsEnabled
+      createdAt
+      description
+      encodingProfile
+      geoFence
+      hlsPlaybackUrl
+      id
+      isDeleted
+      kind
+      organization
+      orientation
+      presenceEnabled
+      reactionsEnabled
+      scheduledStartAt
+      slug
+      source
+      status
+      streamName
+      tags
+      thumbnail {
+        account
+        aspectRatio
+        baseUrl
+        channel
+        createdAt
+        filename
+        height
+        id
+        imgPath
+        orientation
+        status
+        type
+        upload
+        width
+      }
+      title
+      type
+      config {
+        drm
+        dvr
+        introVideo
+        loop
+        primarySource
+        redundancy
+        secondarySource
+        streamInput
+        streamProfile
+      }
+    }
+  }
+`
