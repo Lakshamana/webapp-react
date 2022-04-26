4d25f5655c2fae839235150eabe6d62959a7dd08
biancasilva <bianca@fanhero.com>
2022-04-26 16:26:23 -0300

    fix(livechat): types

--- src/components/organisms/livechat/types.ts
+++ src/components/organisms/livechat/types.ts
@@ -1,16 +1,16 @@
 export interface Props {
-  dataChat?: Array<Chat>;
-  title?: string;
-  onPressEnter?: any;
-  onChangeChat?: any;
-  onCloseChat?: any;
-  entityId: string
+  dataChat?: Array<Chat>
+  title?: string
+  onPressEnter?: any
+  onChangeChat?: any
+  onCloseChat?: any
+  entityId: Maybe<string>
 }
 
 interface Chat {
-  iconUrl?: string;
-  date?: string;
-  username?: string;
-  message?: string;
-  isOwnUser?: boolean;
+  iconUrl?: string
+  date?: string
+  username?: string
+  message?: string
+  isOwnUser?: boolean
 }
