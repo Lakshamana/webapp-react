10b2fd80b3da485b6ef582cf1dff3fc61989bbf7
biancasilva <bianca@fanhero.com>
2022-04-26 16:26:10 -0300

    feat(livechat): get messages from firestore

--- src/components/organisms/livechat/index.tsx
+++ src/components/organisms/livechat/index.tsx
@@ -2,9 +2,87 @@ import { useState } from 'react'
 import { Flex } from '@chakra-ui/react'
 import { Props } from './types'
 import { LivechatFooter, LivechatHeader, LivechatBody } from './components'
+import { firebaseDB } from 'config/firebase'
+import { useAuthStore } from 'services/stores'
+import {
+  collection,
+  query,
+  DocumentData,
+  limit,
+  onSnapshot,
+  addDoc,
+  orderBy,
+  startAfter,
+} from 'firebase/firestore'
+import { useEffect } from 'react'
+import { timestampNow, parseResultSnapshot } from 'utils/firebase'
+import { MessageDocumentData } from 'types/firebase'
 
 const Livechat = ({ entityId, onPressEnter }: Props) => {
-  const [value, setValue] = useState('')
+  const [messages, setMessagesData] = useState<DocumentData[]>([])
+  const [reactions, setReactionsData] = useState<DocumentData[]>([])
+  const { account } = useAuthStore()
+
+  const messagesCollection = collection(
+    firebaseDB,
+    `livestream/${entityId}/comments`
+  )
+  const reactionsCollection = collection(
+    firebaseDB,
+    `livestream/${entityId}/reactions`
+  )
+
+  const messagesQuery = query(
+    messagesCollection,
+    orderBy('dateAdded'),
+    limit(100)
+  )
+
+  const reactionsQuery = query(
+    reactionsCollection,
+    orderBy('dateAdded'),
+    startAfter(timestampNow())
+  )
+
+  const sendNewMessage = (message: string) => {
+    if (!message) {
+      return
+    }
+    const filteredMessage = message.trim()
+    if (!filteredMessage) {
+      return
+    }
+
+    const messageToSend: MessageDocumentData = {
+      userId: account?.id!,
+      username: account?.username || '',
+      avatarPath: account?.profile?.avatar_url || '',
+      updatedAt: timestampNow(),
+      text: filteredMessage,
+      dateAdded: timestampNow(),
+    }
+
+    addDoc(messagesCollection, messageToSend)
+  }
+
+  useEffect(() => {
+    const messagesUnsubscriber = onSnapshot(messagesQuery, (snapshot) => {
+      const messages = parseResultSnapshot(snapshot)
+      setMessagesData((oldArray) => [...oldArray, ...messages])
+    })
+
+    const reactionsUnsubscriber = onSnapshot(reactionsQuery, (snapshot) => {
+      const reactions = parseResultSnapshot(snapshot)
+      setReactionsData((oldArray) => [...oldArray, ...reactions])
+    })
+
+    // Unsubscribe on unmount to avoid a memory leak
+    return () => {
+      messagesUnsubscriber()
+      reactionsUnsubscriber()
+    }
+    //eslint-disable-next-line
+  }, [])
 
   return (
     <Flex
@@ -14,12 +92,8 @@ const Livechat = ({ entityId, onPressEnter }: Props) => {
       justifyContent={'space-between'}
     >
       <LivechatHeader />
-      <LivechatBody />
-      <LivechatFooter
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        onPressIcon={() => onPressEnter(value)}
-      />
+      <LivechatBody messages={messages} />
+      <LivechatFooter sendMessage={sendNewMessage} />
     </Flex>
   )
 }
