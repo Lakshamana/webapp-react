0a5ad08277b1d153b2f4cd6bb6212f37812582a3
biancasilva <bianca@fanhero.com>
2022-04-26 16:27:58 -0300

    feat(livechatbody): fix styles, add scroll to bottom and convert data function

--- src/components/organisms/livechat/components/livechatBody/index.tsx
+++ src/components/organisms/livechat/components/livechatBody/index.tsx
@@ -1,20 +1,43 @@
 import { MainChatBody } from './style'
 import { Props } from './types'
-
+import { formatDistance } from 'date-fns'
+import { useRef, useEffect } from 'react'
 import { BoxChat } from '../boxChat'
+import { useAuthStore } from 'services/stores'
 
 const LivechatBody = ({ messages }: Props) => {
+  const { account } = useAuthStore()
+
+  const messagesEndRef = useRef<null | HTMLDivElement>(null)
+
+  useEffect(() => {
+    messagesEndRef?.current?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
+      inline: 'nearest',
+    })
+  }, [messages])
+
+  const convertData = (data) => {
+    const date = new Date(data.seconds * 1000)
+    return formatDistance(new Date(), date, {
+      includeSeconds: true,
+    })
+  }
+
   return (
     <MainChatBody>
       {messages?.map((e) => (
         <BoxChat
-          isOwnUser={e.isOwnUser}
+          key={e.id}
+          isOwnUser={e.userId === account?.id}
           username={e.username}
-          date={e.createdAt}
-          message={e.message}
-          avatarUrl={e.userAvatar}
+          date={convertData(e.dateAdded)}
+          message={e.text}
+          avatarUrl={e.avatarPath}
         />
       ))}
+      <div ref={messagesEndRef} />
     </MainChatBody>
   )
 }
