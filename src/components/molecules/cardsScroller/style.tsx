94937c710c867e260955d01bfed51f4e0e103be4
João Pedro Moraes <joaopedromoraes@outlook.com>
2022-04-26 20:36:56 -0300

    fix: remove min-height auto not woking

--- src/components/molecules/cardsScroller/style.tsx
+++ src/components/molecules/cardsScroller/style.tsx
@@ -13,10 +13,6 @@ export const SwiperStyled = styled(Swiper)`
     padding: 0 1.3rem !important;
   }
 
-  .swiper-wrapper {
-    min-height: auto;
-  }
-
   .swiper-slide {
     &:hover {
       transition: all 250ms ease-out;
