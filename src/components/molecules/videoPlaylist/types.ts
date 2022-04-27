ded30abf526b2937d175d8945ac0d4a2b92f9b7d
Renato Siqueira <renatoelysiqueira@gmail.com>
2022-04-27 11:11:49 -0300

    Fix: renamed to specific name

--- src/components/molecules/videoPlaylist/types.ts
+++ src/components/molecules/videoPlaylist/types.ts
@@ -2,7 +2,7 @@ import { Post } from 'generated/graphql'
 
 export interface VideoPlaylistProps {
   title?: string
-  autoplay?: boolean
+  showAutoplay?: boolean
   videos?: Post[]
   activeVideo?: string
 }
