e911b84e1f6d91f96258f864e6131a34b7300414
João Pedro Moraes <joaopedromoraes@outlook.com>
2022-04-28 09:51:15 -0300

    feat: remove unused dependency

--- src/modules/checkout/cardInfo/index.tsx
+++ src/modules/checkout/cardInfo/index.tsx
@@ -15,7 +15,7 @@ import { colors } from 'styles'
 import { useState } from 'react'
 
 export const CardInfo = () => {
-  const [ modalType, setmodalType ] = useState(ModalType.success)
+  const [ modalType ] = useState(ModalType.success)
   const { colorMode } = useThemeStore()
   const { t } = useTranslation()
   const { isOpen, onOpen, onClose } = useDisclosure()
