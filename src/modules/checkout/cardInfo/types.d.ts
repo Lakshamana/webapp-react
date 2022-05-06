028da9d98df9afbb29334c2dd78c40142143e99d
João Pedro Moraes <joaopedromoraes@outlook.com>
2022-05-06 10:17:27 -0300

    feat(cardInfo): feat add types

--- src/modules/checkout/cardInfo/types.d.ts
+++ src/modules/checkout/cardInfo/types.d.ts
@@ -5,3 +5,45 @@ export type cardForm = {
   email: string,
   cpf: string,
 }
+
+export type pmDataType = {
+  token: string;
+  created_at: Date;
+  updated_at: Date;
+  email?: any;
+  data?: any;
+  storage_state: string;
+  test: boolean;
+  metadata?: any;
+  callback_url?: any;
+  last_four_digits: string;
+  first_six_digits: string;
+  card_type: string;
+  first_name: string;
+  last_name: string;
+  month: number;
+  year: number;
+  address1?: any;
+  address2?: any;
+  city?: any;
+  state?: any;
+  zip?: any;
+  country?: any;
+  phone_number?: any;
+  company?: any;
+  full_name: string;
+  eligible_for_card_updater: boolean;
+  shipping_address1?: any;
+  shipping_address2?: any;
+  shipping_city?: any;
+  shipping_state?: any;
+  shipping_zip?: any;
+  shipping_country?: any;
+  shipping_phone_number?: any;
+  issuer_identification_number: string;
+  payment_method_type: string;
+  errors: any[];
+  fingerprint: string;
+  verification_value: string;
+  number: string;
+}
