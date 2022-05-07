f1bab800c2c4e5c696caebc596cc6d0964e47f6b
João Pedro Moraes <joaopedromoraes@outlook.com>
2022-05-07 19:37:03 -0300

    feat: integrate spredly in cardinfo

--- src/modules/checkout/cardInfo/index.tsx
+++ src/modules/checkout/cardInfo/index.tsx
@@ -1,4 +1,5 @@
 import {
+  Button,
   Checkbox,
   Divider,
   Flex,
@@ -6,20 +7,21 @@ import {
   useDisclosure,
 } from '@chakra-ui/react'
 import imageBg from 'assets/background/bg-checkout-login.png'
-import { Button, Input, SelectInputStyle } from 'components'
+import { Input, SelectInputStyle } from 'components'
 import { ModalNotification, ModalType } from '../components/notification'
 import { useTranslation } from 'react-i18next'
 import { useThemeStore } from 'services/stores'
 import { colors } from 'styles'
 import { useEffect, useState } from 'react'
 import { InputSpreedly } from './style'
+import { FormikHelpers, useFormik } from 'formik'
+import * as Yup from 'yup'
+import { cardForm } from './types'
 
-const {
-  REACT_APP_SPREENDLY_KEY,
-}  = process.env
+const { REACT_APP_SPREENDLY_KEY } = process.env
 
 export const CardInfo = () => {
-  const [ modalType ] = useState(ModalType.success)
+  const [modalType] = useState(ModalType.success)
   const { colorMode } = useThemeStore()
   const { t } = useTranslation()
   const { isOpen, onOpen, onClose } = useDisclosure()
@@ -29,68 +31,147 @@ export const CardInfo = () => {
     paymentErrors: [],
     paymentProcessing: false,
     paymentCaptured: false,
-    route: 'credit-card'
+    route: 'credit-card',
   })
 
+  const months = [
+    '01',
+    '02',
+    '03',
+    '04',
+    '05',
+    '06',
+    '07',
+    '08',
+    '09',
+    '10',
+    '11',
+    '12',
+  ]
+  const years = [...new Array(100)].map(
+    (_, index) => new Date().getFullYear() + index
+  )
   const [disabledButton, setdisabledButton] = useState(true)
 
   useEffect(() => {
     if (Spreedly) {
       setupSpreedly()
     }
-   return () => Spreedly.removeHandlers()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    return () => Spreedly.removeHandlers()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
-  
+
   const setupSpreedly = () => {
     Spreedly.init(REACT_APP_SPREENDLY_KEY, {
       numberEl: 'spreedly-number',
-      cvvEl: 'spreedly-cvv'
-    });
-    
+      cvvEl: 'spreedly-cvv',
+    })
+
     Spreedly.on('ready', () => {
       setdisabledButton(false)
-      Spreedly.setParam('allow_blank_name', true);
-      Spreedly.setParam('allow_expired_date', true);
+      Spreedly.setParam('allow_blank_name', true)
+      Spreedly.setParam('allow_expired_date', true)
       // credit card number
-      Spreedly.setPlaceholder('number', t("page.checkout.card_info.card_number"));
-      Spreedly.setFieldType('number', 'text');
-      Spreedly.setStyle('number', `font-size: 16px; background: transparent; color: ${colors.generalText[colorMode]}`);
-      Spreedly.setNumberFormat('prettyFormat');
+      Spreedly.setPlaceholder(
+        'number',
+        t('page.checkout.card_info.card_number')
+      )
+      Spreedly.setFieldType('number', 'text')
+      Spreedly.setStyle(
+        'number',
+        `font-size: 16px; background: transparent; color: ${colors.generalText[colorMode]}`
+      )
+      Spreedly.setNumberFormat('prettyFormat')
       // cvv
-      Spreedly.setPlaceholder('cvv', t("page.checkout.card_info.cvv"));
-      Spreedly.setFieldType('cvv', 'text');
-      Spreedly.setStyle('cvv', `font-size: 16px; background: transparent; color: ${colors.generalText[colorMode]}`);
+      Spreedly.setPlaceholder('cvv', t('page.checkout.card_info.cvv'))
+      Spreedly.setFieldType('cvv', 'text')
+      Spreedly.setStyle(
+        'cvv',
+        `font-size: 16px; background: transparent; color: ${colors.generalText[colorMode]}`
+      )
       // testing
       // Spreedly.setValue('number', '4111111111111111');
       // Spreedly.setValue('cvv', '123');
-    });
+    })
 
     Spreedly.on('errors', (errors: any) => {
       for (let i = 0; i < errors.length; i++) {
-        var error = errors[i];
-        console.log(error);
+        var error = errors[i]
+        console.log(error)
       }
 
-      setstate({ ...state, paymentProcessing: false });
+      setstate({ ...state, paymentProcessing: false })
 
       // refresh the form
-      Spreedly.reload();
-      // resetForm()
+      Spreedly.reload()
+      setdisabledButton(false)
+      resetForm()
 
-      const errorMessages = errors.map((err: any) => err.message);
-      setstate({ ...state, paymentErrors: errorMessages });
-    });
+      const errorMessages = errors.map((err: any) => err.message)
+      setstate({ ...state, paymentErrors: errorMessages })
+    })
 
-    Spreedly.on('paymentMethod', (token: string, pmData: any) =>{
+    Spreedly.on('paymentMethod', (token: string, pmData: any) => {
       // continuar implementação da chamada
-      console.log('tokenizeCreditCard: ',token)
+      setdisabledButton(false)
+      onOpen()
+      console.log('tokenizeCreditCard: ', token)
       console.log(pmData)
     })
   }
 
+  const submitPaymentForm = (
+    values: cardForm,
+    actions: FormikHelpers<cardForm>
+  ) => {
+    setdisabledButton(true)
+    const requiredFields: any = values
+    Spreedly.tokenizeCreditCard(requiredFields)
+    setstate({ ...state, paymentProcessing: true })
+  }
+
+  const {
+    values,
+    errors,
+    touched,
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    resetForm,
+  } = useFormik({
+    initialValues: {
+      full_name: '',
+      month: '',
+      year: '',
+      email: '',
+      cpf: '',
+      terms: false,
+    },
+    validationSchema: Yup.object({
+      full_name: Yup.string().required(
+        t('page.checkout.card_info.mistakes.full_name_required')
+      ),
+      month: Yup.string().required(
+        t('page.checkout.card_info.mistakes.month_required')
+      ),
+      year: Yup.string().required(
+        t('page.checkout.card_info.mistakes.year_required')
+      ),
+      email: Yup.string().email(t('common.error.valid_email')),
+      terms: Yup.bool().oneOf(
+        [true],
+        t('common.error.accept_terms_and_conditions')
+      ),
+    }),
+    onSubmit: submitPaymentForm,
+  })
+
   return (
     <Flex flexDirection="column" alignItems="center">
+      {/* <Flex color="white" gridGap="3em" alignItems="center">
+        <pre>{JSON.stringify(values, null, 2) }</pre>
+        <pre>{JSON.stringify(errors, null, 2) }</pre>
+      </Flex> */}
       <Text
         fontSize="32px"
         fontWeight="700"
@@ -171,62 +252,91 @@ export const CardInfo = () => {
           gridGap="1em"
         >
           <Input
-            name="payload.password"
-            type={'text'}
+            name="full_name"
+            type="text"
+            value={values.full_name}
             placeholder={t('page.checkout.card_info.card_name')}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            errorMessage={errors.full_name}
+            error={!!errors.full_name && touched.full_name}
           />
-          <InputSpreedly id="spreedly-number" />
-          <Flex w="100%" gridGap="1em">
-            <Input
-              name="payload.password"
-              type={'text'}
-              placeholder={t('page.checkout.card_info.date')}
-            />
-            <InputSpreedly id="spreedly-cvv" />
+          <InputSpreedly id="spreedly-number" w="100%" mb="8px"/>
+          <Flex gridGap="1em" w="100%">
+            <Flex gridGap="0.5em" alignItems="center" w="100%">
+              <SelectInputStyle
+                name="month"
+                placeholder={t('page.checkout.card_info.date_month')}
+                value={values.month}
+                options={months.map((value) => ({ value, label: value }))}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                errorBorderColor="#d9534f"
+                isInvalid={!!errors.month && touched.month}
+              />
+              <Text fontSize="24px" color="#E1E1E1">
+                /
+              </Text>
+              <SelectInputStyle
+                name="year"
+                placeholder={t('page.checkout.card_info.date_year')}
+                value={values.year}
+                options={years.map((value) => ({ value, label: `${value}` }))}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                errorBorderColor="#d9534f"
+                isInvalid={!!errors.year && touched.year}
+              />
+            </Flex>
+            <InputSpreedly id="spreedly-cvv" w="110px" />
           </Flex>
-          <SelectInputStyle
-            placeholder={t('page.checkout.card_info.country')}
-            options={[
-              { value: 'Aliqua', label: 'Aliqua cupidatat id' },
-              { value: 'cupidatat', label: 'Aliqua cupidatat id' },
-            ]}
-            onChange={(evt: any) => {
-              const { value } = evt?.target
-              console.log(value)
-            }}
-          />
-          <Divider color={colors.secondaryText[colorMode]} />
+          {/* <Divider color={colors.secondaryText[colorMode]} />
 
           <Input
             name="payload.password"
-            type={'text'}
+            type="text"
             placeholder={t('page.checkout.card_info.full_name')}
           />
 
           <Flex w="100%" gridGap="1em">
             <Input
               name="payload.password"
-              type={'text'}
+              type="text"
               placeholder={t('page.checkout.card_info.email')}
             />
             <Input
               name="payload.password"
-              type={'text'}
+              type="text"
               placeholder={t('page.checkout.card_info.CPF')}
             />
-          </Flex>
+          </Flex> */}
 
           <Flex alignItems="flex-start" gridGap="12px" mt="1em">
-            <Checkbox defaultIsChecked fontSize="12px"></Checkbox>
+            <Checkbox
+              fontSize="12px"
+              name="terms"
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
             <Text fontSize="12px" color={colors.generalText[colorMode]}>
               {t('page.checkout.card_info.authorize')}
             </Text>
           </Flex>
-            <Button
-              height="56px"
-              width="236px"
-            label={t('page.checkout.card_info.place_you_order')}
-          ></Button>
+          <Button
+            textTransform="uppercase"
+            height="56px"
+            width="236px"
+            disabled={disabledButton}
+            isLoading={disabledButton}
+            onClick={() => handleSubmit()}
+            backgroundColor="#D0021B"
+            color="white"
+            _hover={{ bg: '#9B84FE' }}
+            _disabled={{ bg: '#A4A4A4' }}
+            _active={{ bg: '#9B84FE' }}
+          >
+            {t('page.checkout.card_info.place_you_order')}
+          </Button>
         </Flex>
       </Flex>
       <ModalNotification {...{ isOpen, onOpen, onClose, modalType }} />
