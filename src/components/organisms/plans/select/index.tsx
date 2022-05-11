26eea584e7e2a5fbc5620fb1188e97204f04ef56
João Pedro Moraes <joaopedromoraes@outlook.com>
2022-05-11 09:07:12 -0300

    feat(plans): create select plans component

---
+++ src/components/organisms/plans/select/index.tsx
@@ -0,0 +1,96 @@
+import { Box, Button, Flex, Text } from '@chakra-ui/react'
+
+export const SelectPlan = () => {
+  const plans = [
+    {
+      imageUrl:
+        'https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2021/06/35211_4DA8E347FB9842EB.jpg?w=876&h=484&crop=1',
+      imageAlt: 'Rear view of modern home with pool',
+      title: 'Platinum - Subscription Title',
+      subtitle:
+        'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor ate incididunt ut labore et.',
+      value: 199,
+    },
+    {
+      imageUrl:
+        'https://www.show.news/__export/1629475032162/sites/debate/img/2021/08/20/lana.jpg_839202635.jpg',
+      imageAlt: 'Rear view of modern home with pool',
+      title: 'Gold - Subscription Title',
+      subtitle:
+        'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor ate incididunt ut labore et.',
+      value: 165,
+    },
+    {
+      imageUrl:
+        'https://www.show.news/__export/1629475032162/sites/debate/img/2021/08/20/lana.jpg_839202635.jpg',
+      imageAlt: 'Rear view of modern home with pool',
+      title: 'Silver - Subscription Title',
+      subtitle:
+        'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor ate incididunt ut labore et.',
+      value: 85,
+    },
+  ]
+  return (
+    <Flex flexDirection="column" mt="44px">
+      <Text fontSize="28px" fontWeight="500">
+        Title - Lorem ipsum
+      </Text>
+      <Flex gridGap="24px" mt="16px">
+        {plans.map((plan) => (
+          <Box
+            maxW="sm"
+            borderWidth="1px"
+            borderRadius="8px"
+            overflow="hidden"
+            w="340px"
+            background="#222222"
+          >
+            <Box
+              h="184px"
+              w="100%"
+              backgroundImage={`url(${plan.imageUrl})`}
+              backgroundSize="100%"
+              backgroundPosition="center"
+              backgroundRepeat="no-repeat"
+            />
+            <Box p="20px">
+              <Text color="white" fontWeight="600" fontSize="18px">
+                {plan.title}
+              </Text>
+              <Text
+                color="#A4A4A4"
+                fontWeight="400"
+                fontSize="12px"
+                maxW="256px"
+                w="100%"
+                mt="6px"
+              >
+                {plan.subtitle}
+              </Text>
+              <Flex
+                mt="15px"
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <Button
+                  h="36px"
+                  w="156px"
+                  bg="#0660F9"
+                  color="white"
+                  fontSize="12px"
+                  textTransform="uppercase"
+                  fontWeight="400"
+                >
+                  Select
+                </Button>
+                <Text color="white" fontWeight="400" fontSize="18px">
+                  ${plan.value}
+                </Text>
+              </Flex>
+            </Box>
+          </Box>
+        ))}
+      </Flex>
+    </Flex>
+  )
+}
