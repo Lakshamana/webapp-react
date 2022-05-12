02ef352a6dbfa47185a86d7bfa8cccca3f2b8444
Renato Siqueira <renatoelysiqueira@gmail.com>
2022-05-12 16:40:13 -0300

    Feat: modal, logical, edit, delete and report add

--- src/components/organisms/comments/index.tsx
+++ src/components/organisms/comments/index.tsx
@@ -1,24 +1,36 @@
 import { useState, useEffect } from 'react'
-import { Flex } from '@chakra-ui/react'
+import { useTranslation } from 'react-i18next'
+import { Flex, Box, Text, Textarea } from '@chakra-ui/react'
 import { useLazyQuery, useMutation } from '@apollo/client'
 import InfiniteScroll from 'react-infinite-scroll-component'
-import { QUERY_COMMENTS, MUTATION_ADD_COMMENT } from 'services/graphql'
+import {
+  QUERY_COMMENTS,
+  MUTATION_ADD_COMMENT,
+  MUTATION_UPDATE_COMMENT,
+  MUTATION_DELETE_COMMENT,
+  MUTATION_ADD_REPORT
+} from 'services/graphql'
 import { Post, SortDirection, Comment as CommentType } from 'generated/graphql'
 import { DEFAULT_PAGESIZE_COMMENTS } from 'config/constants'
-import { CommentHeader, CommentInput, CommentCard, CommentLoading } from 'components'
+import { CommentHeader, CommentInput, CommentCard, CommentLoading, Modal } from 'components'
+import { useCommentsStore, useThemeStore } from 'services/stores'
+import { colors } from 'styles'
+import { initialValues, validationSchema } from './settings'
+import { useFormik } from 'formik'
 
 const Comments = ({ ...props }: Post) => {
-  const [comments, setComments] = useState<CommentType[]>([])
-  const [totalComments, setTotalComments] = useState<number>(0)
-  const [filterPage, setFilterPage] = useState(0)
-  const [filterBy, SetFilterBy] = useState<SortDirection>(SortDirection.Desc)
-  const [hasMore, setHasMore] = useState(true)
-
-  const [getComments, { data: allComments }] = useLazyQuery(QUERY_COMMENTS, {
-    fetchPolicy: 'cache-and-network'
+  const { t } = useTranslation()
+  const { colorMode } = useThemeStore()
+  const { commentsStore, setUpdateCommentsStore, modalOption, setModalOption, resetModal } = useCommentsStore()
+  const [filterBy, setFilterBy] = useState<SortDirection>(SortDirection.Desc)
+  const [filterPage, setFilterPage] = useState<number>(0)
+  const [getComments, { data, loading: dataLoading }] = useLazyQuery(QUERY_COMMENTS, {
+    fetchPolicy: 'no-cache'
   })
-
-  const [addComment, { data: newComment, loading: addCommentLoading }] = useMutation(MUTATION_ADD_COMMENT)
+  const [addComment, { data: newComment, loading: newCommentLoading }] = useMutation(MUTATION_ADD_COMMENT)
+  const [editComment, { data: editedComment, loading: editedCommentLoading }] = useMutation(MUTATION_UPDATE_COMMENT)
+  const [deleteComment, { data: deletedComment, loading: deleteCommentLoading }] = useMutation(MUTATION_DELETE_COMMENT)
+  const [reportComment, { data: reportedComment, loading: reportCommentLoading }] = useMutation(MUTATION_ADD_REPORT)
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => getAllComments(), [])
@@ -26,29 +38,90 @@ const Comments = ({ ...props }: Post) => {
   useEffect(() => getAllComments(), [filterBy])
 
   useEffect(() => {
-    if (!newComment) return
-    if (filterBy === 'ASC') {
-      setComments(comments.concat(newComment?.addComment))
+    if (!data) return
+    setFilterPage(data.comments?.page)
+    if (filterPage === 0) {
+      setUpdateCommentsStore({
+        hasMore: data.comments?.hasNextPage,
+        totalComments: data.comments?.total,
+        allComments: data.comments?.rows
+      })
       return
     }
-    const updateComment = [
-      newComment?.addComment,
-      ...comments
+
+    const updateComments = [
+      ...commentsStore.allComments,
+      ...data.comments?.rows
     ]
-    setComments(updateComment)
+    setUpdateCommentsStore({
+      hasMore: data.comments?.hasNextPage,
+      totalComments: data.comments?.total,
+      allComments: updateComments
+    })
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [newComment])
+  }, [data])
 
   useEffect(() => {
-    if (!allComments) return
-    setHasMore(allComments.comments?.hasNextPage)
-    setTotalComments(allComments.comments?.total)
-    setFilterPage(allComments.comments?.page)
-    filterPage === 0
-      ? setComments(allComments.comments?.rows)
-      : setComments(comments.concat(allComments.comments?.rows))
+    if (!newComment) return
+    const totalComments = commentsStore.totalComments + 1
+    if (filterBy === SortDirection.Desc) {
+      const updateComments = [
+        newComment.addComment,
+        ...commentsStore.allComments
+      ]
+      setUpdateCommentsStore({
+        totalComments,
+        allComments: [...updateComments]
+      })
+      return
+    }
+    if (!commentsStore.hasMore) {
+      const updateComments = [
+        ...commentsStore.allComments,
+        newComment.addComment
+      ]
+      setUpdateCommentsStore({
+        totalComments,
+        allComments: [...updateComments]
+      })
+      return
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [allComments])
+  }, [newCommentLoading])
+
+  useEffect(() => {
+    if (!editedComment) return
+    const totalComments = commentsStore.totalComments
+    const updateComments = commentsStore.allComments.map(each => {
+      if (each.id === editedComment.updateComment.id) {
+        return {
+          ...each,
+          description: editedComment.updateComment.description
+        }
+      }
+      return each
+    })
+    setUpdateCommentsStore({
+      totalComments,
+      allComments: [...updateComments]
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [editedComment])
+
+  useEffect(() => {
+    if (!deletedComment) return
+    const totalComments = commentsStore.totalComments - 1
+    const updateComments = commentsStore.allComments.filter(each => each.id !== deletedComment.deleteComment.id)
+    setUpdateCommentsStore({
+      totalComments,
+      allComments: [...updateComments]
+    })
+    resetModal()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [deletedComment])
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => resetModal(), [reportedComment])
 
   const getAllComments = (page: number = 1) => {
     getComments({
@@ -64,59 +137,162 @@ const Comments = ({ ...props }: Post) => {
   }
 
   const loadMore = () => {
-    if (hasMore) getAllComments(filterPage + 1)
+    if (commentsStore.hasMore) getAllComments(filterPage + 1)
   }
 
-  const getSortByFilter = () => filterBy === 'ASC'
+  const getSortByFilter = () => filterBy === SortDirection.Asc
     ? "createdAt.asc"
     : "createdAt.desc"
 
   const handleFilterChange = (evt: any) => {
     const { value } = evt?.target;
-    setComments([])
+    setUpdateCommentsStore({ allComments: [], hasMore: false, totalComments: 0 })
     setFilterPage(0)
-    SetFilterBy(value)
+    setFilterBy(value)
   }
 
-  return (
-    <Flex flexDirection="column" width={'100%'}>
-      <CommentHeader
-        totalComments={totalComments}
-        filterBy={filterBy}
-        handleFilterChange={handleFilterChange}
-      />
-      <CommentInput
-        postId={props.id}
-        action={addComment}
-        actionLoading={addCommentLoading}
-        totalComments={totalComments}
-        setTotalComments={setTotalComments}
-      />
-      {
-        addCommentLoading &&
-        totalComments === 0 &&
-        <CommentLoading show={3} />
+  const handleReportComment = ({ reportReason }) => {
+    reportComment({
+      variables: {
+        payload: {
+          idReported: modalOption.id,
+          type: 'POST',
+          reason: reportReason
+        }
       }
-      <InfiniteScroll
-        dataLength={comments?.length}
-        next={loadMore}
-        hasMore={hasMore}
-        loader={<CommentLoading show={3} />}
+    })
+  }
+
+  const handleSelectPopupOption = (selected) => {
+    if (selected.option === 'DELETE') {
+      setModalOption({
+        status: true,
+        typeEvent: selected.option,
+        text: t('page.post.comment.confirm_delete'),
+        action: () => deleteComment({ variables: { id: selected.id } }),
+        loadingAction: deleteCommentLoading
+      })
+      return
+    }
+
+    if (selected.option === 'REPORT') {
+      setModalOption({
+        status: true,
+        typeEvent: selected.option,
+        text: '',
+        id: selected.id,
+        action: formik.handleSubmit,
+        loadingAction: reportCommentLoading
+      })
+    }
+  }
+
+  const closeModal = () => {
+    formik.resetForm()
+    resetModal()
+  }
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    validateOnChange: true,
+    validateOnBlur: false,
+    onSubmit: handleReportComment
+  })
+
+  return (
+    <>
+      <Modal
+        isCentered
+        isOpen={modalOption.status}
+        onClose={closeModal}
+        onConfirm={modalOption.action}
+        isActionDisabled={modalOption.loadingAction}
+        loading={modalOption.loadingAction}
+        closeOnOverlayClick={false}
+        actionLabel={modalOption.text}
       >
+        <>
+          {
+            modalOption.typeEvent === 'REPORT' &&
+            <Box textAlign="center">
+              <Text
+                color={colors.generalText[colorMode]}
+                fontSize={'1.5rem'}
+                textAlign={'center'}
+                fontWeight={500}
+              >
+                {t('page.post.comment.report')}
+              </Text>
+              <Text
+                color={colors.generalText[colorMode]}
+                textAlign={'center'}
+                mb={2}
+              >
+                {t('page.post.comment.report_reason')}
+              </Text>
+              <Textarea
+                type="text"
+                name="reportReason"
+                value={formik.values.reportReason}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                color={colors.inputText[colorMode]}
+              />
+              {
+                !!formik.errors.reportReason && formik.touched.reportReason &&
+                <Text
+                  color={colors.generalText[colorMode]}
+                  textAlign={'center'}
+                  mb={-4}
+                >
+                  {formik.errors.reportReason}
+                </Text>
+              }
+            </Box>
+          }
+        </>
+      </Modal>
+      <Flex flexDirection="column" width={'100%'}>
+        <CommentHeader
+          totalComments={commentsStore.totalComments}
+          filterBy={filterBy}
+          loading={dataLoading}
+          handleFilterChange={handleFilterChange}
+        />
+        <CommentInput
+          postId={props.id}
+          action={addComment}
+          actionLoading={newCommentLoading}
+        />
         {
-          comments?.map((comment: CommentType) =>
-            <CommentCard
-              key={`comment-${comment.id}`}
-              postId={props.id}
-              addComment={addComment}
-              addCommentLoading={addCommentLoading}
-              totalComments={totalComments}
-              setTotalComments={setTotalComments}
-              {...comment}
-            />
-          )}
-      </InfiniteScroll>
-    </Flex>
+          newCommentLoading &&
+          commentsStore.totalComments === 0 &&
+          <CommentLoading show={3} />
+        }
+        <InfiniteScroll
+          dataLength={commentsStore.allComments?.length}
+          next={loadMore}
+          hasMore={commentsStore?.hasMore || false}
+          loader={<CommentLoading show={3} />}
+        >
+          {
+            commentsStore.allComments?.map((comment: CommentType) =>
+              <CommentCard
+                key={`comment-${comment.id}`}
+                postId={props.id}
+                addComment={addComment}
+                editComment={editComment}
+                selectPopupOption={handleSelectPopupOption}
+                newCommentLoading={newCommentLoading}
+                editedCommentLoading={editedCommentLoading}
+                deleteCommentLoading={deleteCommentLoading}
+                {...comment}
+              />
+            )}
+        </InfiniteScroll>
+      </Flex>
+    </>
   )
 }
 
