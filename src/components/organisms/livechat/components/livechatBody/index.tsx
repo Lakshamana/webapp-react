d7eca1149d8150c4cb87f1e5a2ae82c9b1d5a350
biancasilva <bianca@fanhero.com>
2022-05-12 22:52:13 -0300

    feat(livechatbody): add prop to receive isCommentsEnable and add chat disabled status

--- src/components/organisms/livechat/components/livechatBody/index.tsx
+++ src/components/organisms/livechat/components/livechatBody/index.tsx
@@ -1,12 +1,20 @@
 import { MainChatBody } from './style'
+import { Icon } from '@iconify/react'
+import { Text } from 'components'
 import { Props } from './types'
 import { formatDistance } from 'date-fns'
 import { useRef, useEffect } from 'react'
+import { useThemeStore } from 'services/stores'
+import { useTranslation } from 'react-i18next'
 import { BoxChat } from '../boxChat'
 import { useAuthStore } from 'services/stores'
+import { Center } from '@chakra-ui/layout'
+import { colors } from 'styles'
 
-const LivechatBody = ({ messages }: Props) => {
+const LivechatBody = ({ messages, enabled }: Props) => {
   const { account } = useAuthStore()
+  const { colorMode } = useThemeStore()
+  const { t } = useTranslation()
 
   const messagesEndRef = useRef<null | HTMLDivElement>(null)
 
@@ -24,6 +32,16 @@ const LivechatBody = ({ messages }: Props) => {
       includeSeconds: true,
     })
   }
+
+  if (!enabled)
+    return (
+      <Center color={colors.secondaryText[colorMode]} flexDirection={'column'}>
+        <Icon width={60} icon="mdi:message-bulleted-off" />
+        <Text fontSize={'1rem'} mt={2} fontWeight="bolder">
+          {t('common.disabled_chat')}
+        </Text>
+      </Center>
+    )
 
   return (
     <MainChatBody>
