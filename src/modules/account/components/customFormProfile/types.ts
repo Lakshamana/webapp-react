d5fe466b8e0ea34b7e88bca77802100509a6b8b8
João Pedro Moraes <joaopedromoraes@outlook.com>
2022-05-18 10:18:32 -0300

    feat(account): update types

--- src/modules/account/components/customFormProfile/types.ts
+++ src/modules/account/components/customFormProfile/types.ts
@@ -1,6 +1,8 @@
+import { Profile } from "generated/graphql"
+
 export interface Props {
 	fields?: any
-	initialValues?: any,
 	handleFormSubmit: (formData: Object) => void
-	isLoading: boolean
+	isLoading: boolean,
+	user: Profile
 }
