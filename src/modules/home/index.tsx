3a217b0c7c02a7ff1f55cb750ceab78e6af53ead
biancasilva <bianca@fanhero.com>
2022-05-23 14:43:04 -0300

    fix(home): fix billboard actions verification

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -218,7 +218,7 @@ const HomePage = () => {
 
         memo.push({
           ...curr,
-          actions: curr.actions.map((action) => ({
+          actions: curr.actions?.map((action) => ({
             ...action,
             bgColor: convertToValidColor(action.bgColor),
             borderColor: convertToValidColor(action.borderColor),
