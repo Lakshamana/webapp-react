9d985f6e476853ec1e1e6ae61d3cb30386a5f155
biancasilva <bianca@fanhero.com>
2022-06-22 14:53:29 -0300

    fix(push): use org name from organization config file

--- src/utils/pushNotifications.ts
+++ src/utils/pushNotifications.ts
@@ -1,12 +1,9 @@
 import OneSignal from 'react-onesignal'
 import i18n from 'config/i18n'
-import { getData } from 'services/storage'
-import { ORGANIZATION_INFO } from 'config/constants'
+import { organizationData } from 'config/organization'
 
 const { REACT_APP_ONE_SIGNAL_APP_ID, REACT_APP_ONE_SIGNAL_SAFARI_WEB_ID } =
   process.env
-
-const org = getData(ORGANIZATION_INFO)
 
 export const initializeOneSignal = () => {
   if (REACT_APP_ONE_SIGNAL_APP_ID && REACT_APP_ONE_SIGNAL_SAFARI_WEB_ID) {
@@ -20,7 +17,9 @@ export const initializeOneSignal = () => {
             {
               type: 'push',
               text: {
-                actionMessage: i18n.t('common.push.title', { org: org?.name}),
+                actionMessage: i18n.t('common.push.title', {
+                  org: organizationData?.name,
+                }),
                 acceptButton: i18n.t('common.push.allow'),
                 cancelButton: i18n.t('common.push.cancel'),
               },
