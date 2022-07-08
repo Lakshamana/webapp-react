b10a01a50d4dc4b5ab2c016b866fce65c96bedd0
Renato Siqueira <renatoelysiqueira@gmail.com>
2022-07-08 17:04:18 -0300

    Feat: payment integration

--- src/components/organisms/payment/cardInfo/index.tsx
+++ src/components/organisms/payment/cardInfo/index.tsx
@@ -1,25 +1,27 @@
-import { useMutation } from '@apollo/client'
+import { useLazyQuery, useMutation } from '@apollo/client'
 import { Button, Checkbox, Divider, Flex, Text, useDisclosure } from '@chakra-ui/react'
 import axios from 'axios'
 import { Input, SelectInputStyle } from 'components'
-import { FormikHelpers, useFormik } from 'formik'
-import { OrderStatus } from 'generated/graphql'
+import { useFormik } from 'formik'
 import { ModalType } from 'modules/checkout/components/notification'
 import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { MUTATION_CONFIRM_ORDER } from 'services/graphql'
+import { MUTATION_CONFIRM_ORDER, QUERY_GET_ORDER_RESULT } from 'services/graphql'
 import { useThemeStore } from 'services/stores'
 import { colors } from 'styles'
 import * as Yup from 'yup'
 import { ModalNotification } from '../components'
 import { InputSpreedly } from './style'
-import { cardForm, pmDataType, Props, SpreedlyError } from './types'
+import { cardForm, OrderType, pmDataType, Props, SpreedlyError } from './types.d'
 
 const { REACT_APP_SPREEDLY_KEY } = process.env
+const INSPIRE_PAYMENT_API = 'https://api-payment-staging.inspireplatform.io'
+const HEADERS = { headers: { tenant: 'Marvel-wu61z', }, }
+const MONTHS = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
 
 export const CardInfoSpreedly = ({
   productPrice,
-  product,
+  product
 }: Props) => {
   const [modalType, setmodalType] = useState(ModalType.success)
   const { colorMode } = useThemeStore()
@@ -46,41 +48,47 @@ export const CardInfoSpreedly = ({
   const [allCountries, setallCountries] = useState([])
   const [allStatesById, setallStatesById] = useState([])
 
+  const [getOrderResult, { data: orderResult, loading: orderLoading, stopPolling },
+  ] = useLazyQuery(QUERY_GET_ORDER_RESULT, {
+    fetchPolicy: 'network-only',
+    pollInterval: 5000,
+  })
+
+  useEffect(() => {
+    if (orderResult) {
+      const { status } = orderResult?.order
+      if (status === OrderType.active) {
+        if (stopPolling) stopPolling()
+        setmodalType(ModalType.success)
+        onOpen()
+        return
+      }
+
+      if (status === OrderType.failed) {
+        if (stopPolling) stopPolling()
+        setdisabledButton(false)
+        setmodalType(ModalType.failure)
+        onOpen()
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [orderResult, orderLoading])
+
   const [confirmOrder] = useMutation(
     MUTATION_CONFIRM_ORDER,
     {
       onCompleted: async (result) => {
-        setdisabledButton(false)
-        const { confirmOrder: { status } } = result
-        if(OrderStatus.Active === status) {
-          setmodalType(ModalType.success)
-          onOpen()
-          return
-        }
-        setmodalType(ModalType.failure)
-        onOpen()
+        console.log('QUERY', result)
+        const { confirmOrder: { id } } = result
+        getOrderResult({ variables: { id } })
       },
-      onError: (error) => {
+      onError: () => {
         setmodalType(ModalType.failure)
         setdisabledButton(false)
       },
     }
   )
 
-  const months = [
-    '01',
-    '02',
-    '03',
-    '04',
-    '05',
-    '06',
-    '07',
-    '08',
-    '09',
-    '10',
-    '11',
-    '12',
-  ]
   const years = [...new Array(100)].map(
     (_, index) => new Date().getFullYear() + index
   )
@@ -136,7 +144,6 @@ export const CardInfoSpreedly = ({
 
       setstate({ ...state, paymentProcessing: false })
 
-      // refresh the form
       Spreedly.reload()
       setdisabledButton(false)
       resetForm()
@@ -173,9 +180,6 @@ export const CardInfoSpreedly = ({
           }
         }
       })
-      onOpen()
-      console.log('tokenizeCreditCard: ', token)
-      console.log(pmData)
     })
 
     Spreedly.on('validation', (inputProperties: SpreedlyError) => {
@@ -185,7 +189,7 @@ export const CardInfoSpreedly = ({
 
   const submitPaymentForm = (
     values: cardForm,
-    actions: FormikHelpers<cardForm>
+    // actions: FormikHelpers<cardForm>
   ) => {
     Spreedly.validate()
     setdisabledButton(true)
@@ -199,7 +203,7 @@ export const CardInfoSpreedly = ({
       }
     }
     Spreedly.tokenizeCreditCard(requiredFields)
-    setstate({ ...state, paymentProcessing: true })
+    setstate({ ...state, paymentProcessing: false })
   }
 
   const {
@@ -230,11 +234,11 @@ export const CardInfoSpreedly = ({
     },
     validationSchema: Yup.object({
       phone_number: Yup.string()
-      .required(
-        t('common.error.field_required', {
-          field_name: t('common.custom_field.phone'),
-        })
-      ),
+        .required(
+          t('common.error.field_required', {
+            field_name: t('common.custom_field.phone'),
+          })
+        ),
       full_name: Yup.string().required(
         t('page.checkout.card_info.mistakes.full_name_required')
       ),
@@ -314,120 +318,105 @@ export const CardInfoSpreedly = ({
   })
 
   const getCountries = async () => {
-    const result = await axios.get(
-      'https://api-payment-staging.inspireplatform.io/countries',
-      {
-        headers: {
-          tenant: 'Marvel-wu61z',
-        },
-      }
-    )
+    const result = await axios.get(`${INSPIRE_PAYMENT_API}/countries`, HEADERS)
     const options = result.data.body.data.map(({ id, name }) => ({ value: id, label: name }))
     setallCountries(options)
   }
 
   const getStatesById = async (id: string) => {
     const result = await axios.get(
-      `https://api-payment-staging.inspireplatform.io/states?countryId=${id}`,
-      {
-        headers: {
-          tenant: 'Marvel-wu61z',
-        },
-      }
+      `${INSPIRE_PAYMENT_API}/states?countryId=${id}`,
+      HEADERS
     )
     const options = result.data.body.data.map(({ id, name }) => ({ value: id, label: name }))
     setallStatesById(options)
   }
 
-  useEffect(() => {
-    getCountries()
-  }, [])
-  
+  useEffect(() => { getCountries() }, [])
 
   return (
-    <Flex
-      flex="1"
-      borderRadius="8px"
-      alignItems="center"
-      justifyContent="center"
-      flexDirection="column"
-      gridGap="1em"
-    >
-      {/* <pre>
-        <code>{JSON.stringify(values, null, 4)}</code>
-      </pre> */}
-      <Input
-        name="full_name"
-        type="text"
-        value={values.full_name}
-        placeholder={t('page.checkout.card_info.card_name')}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        errorMessage={errors.full_name}
-        error={!!errors.full_name && touched.full_name}
-      />
-      <InputSpreedly
-        id="spreedly-number"
-        w="100%"
-        mb="8px"
-        error={!spreedlyError.validNumber}
-      />
-      <Flex gridGap="1em" w="100%">
-        <Flex gridGap="0.5em" alignItems="center" w="100%">
-          <SelectInputStyle
-            name="month"
-            placeholder={t('page.checkout.card_info.date_month')}
-            value={values.month}
-            options={months.map((value) => ({ value, label: value }))}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            errorBorderColor="#d9534f"
-            isInvalid={!!errors.month && touched.month}
-          />
-          <Text fontSize="24px" color="#E1E1E1">
-            /
-          </Text>
-          <SelectInputStyle
-            name="year"
-            placeholder={t('page.checkout.card_info.date_year')}
-            value={values.year}
-            options={years.map((value) => ({ value, label: `${value}` }))}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            errorBorderColor="#d9534f"
-            isInvalid={!!errors.year && touched.year}
+    <>
+      <ModalNotification {...{ isOpen, onClose, modalType }} />
+      <Flex
+        flex="1"
+        borderRadius="8px"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        gridGap="1em"
+      >
+        <Input
+          name="full_name"
+          type="text"
+          value={values.full_name}
+          placeholder={t('page.checkout.card_info.card_name')}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          errorMessage={errors.full_name}
+          error={!!errors.full_name && touched.full_name}
+        />
+        <InputSpreedly
+          id="spreedly-number"
+          w="100%"
+          mb="8px"
+          error={!spreedlyError.validNumber}
+        />
+        <Flex gridGap="1em" w="100%">
+          <Flex gridGap="0.5em" alignItems="center" w="100%">
+            <SelectInputStyle
+              name="month"
+              placeholder={t('page.checkout.card_info.date_month')}
+              value={values.month}
+              options={MONTHS.map((value) => ({ value, label: value }))}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              errorBorderColor="#d9534f"
+              isInvalid={!!errors.month && touched.month}
+            />
+            <Text fontSize="24px" color="#E1E1E1">
+              /
+            </Text>
+            <SelectInputStyle
+              name="year"
+              placeholder={t('page.checkout.card_info.date_year')}
+              value={values.year}
+              options={years.map((value) => ({ value, label: `${value}` }))}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              errorBorderColor="#d9534f"
+              isInvalid={!!errors.year && touched.year}
+            />
+          </Flex>
+          <InputSpreedly
+            id="spreedly-cvv"
+            w="110px"
+            error={!spreedlyError.validCvv}
           />
         </Flex>
-        <InputSpreedly
-          id="spreedly-cvv"
-          w="110px"
-          error={!spreedlyError.validCvv}
-        />
-      </Flex>
-      <Divider />
-      <Flex gridGap="1em" w="100%">
+        <Divider />
+        <Flex gridGap="1em" w="100%">
+          <Input
+            name="email"
+            type="text"
+            value={values.email}
+            placeholder={t('page.checkout.card_info.email')}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            errorMessage={errors.email}
+            error={!!errors.email && touched.email}
+          />
+          <Input
+            name="cpf"
+            type="text"
+            value={values.cpf}
+            placeholder={t('page.checkout.card_info.CPF')}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            errorMessage={errors.cpf}
+            error={!!errors.cpf && touched.cpf}
+          />
+        </Flex>
         <Input
-          name="email"
-          type="text"
-          value={values.email}
-          placeholder={t('page.checkout.card_info.email')}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          errorMessage={errors.email}
-          error={!!errors.email && touched.email}
-        />
-        <Input
-          name="cpf"
-          type="text"
-          value={values.cpf}
-          placeholder={t('page.checkout.card_info.CPF')}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          errorMessage={errors.cpf}
-          error={!!errors.cpf && touched.cpf}
-        />
-      </Flex>
-      <Input
           name="phone_number"
           type="text"
           value={values.phone_number}
@@ -437,118 +426,119 @@ export const CardInfoSpreedly = ({
           errorMessage={errors.phone_number}
           error={!!errors.phone_number && touched.phone_number}
         />
-      <SelectInputStyle
-        name="country"
-        placeholder={t('page.checkout.card_info.country')}
-        value={values.country}
-        options={allCountries}
-        onChange={(e) => {
-          handleChange(e);
-          getStatesById(e.target.value)
-        }}
-        onBlur={handleBlur}
-        errorBorderColor="#d9534f"
-        isInvalid={!!errors.country && touched.country}
-      />
-      <SelectInputStyle
-        name="state"
-        placeholder={t('page.checkout.card_info.state')}
-        value={values.state}
-        options={allStatesById}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        errorBorderColor="#d9534f"
-        isInvalid={!!errors.state && touched.state}
-      />
-      <Input
-        name="address1"
-        type="text"
-        value={values.address1}
-        placeholder={t('page.checkout.card_info.address01')}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        errorMessage={errors.address1}
-        error={!!errors.address1 && touched.address1}
-      />
-      <Input
-        name="address2"
-        type="text"
-        value={values.address2}
-        placeholder={t('page.checkout.card_info.address02')}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        errorMessage={errors.address2}
-        error={!!errors.address2 && touched.address2}
-      />
-      <Input
-        name="number"
-        type="text"
-        value={values.number}
-        placeholder={t('page.checkout.card_info.number')}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        errorMessage={errors.number}
-        error={!!errors.number && touched.number}
-      />
-      <Input
-        name="zip"
-        type="text"
-        value={values.zip}
-        placeholder={t('page.checkout.card_info.zip_code')}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        errorMessage={errors.zip}
-        error={!!errors.zip && touched.zip}
-      />
-      <Input
-        name="district"
-        type="text"
-        value={values.district}
-        placeholder={t('page.checkout.card_info.district')}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        errorMessage={errors.district}
-        error={!!errors.district && touched.district}
-      />
-      <Input
-        name="city"
-        type="text"
-        value={values.city}
-        placeholder={t('page.checkout.card_info.city')}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        errorMessage={errors.city}
-        error={!!errors.city && touched.city}
-      />
-      <Flex alignItems="flex-start" gridGap="12px" mt="1em">
-        <Checkbox
-          fontSize="12px"
-          name="terms"
-          isChecked={values.terms}
+        <SelectInputStyle
+          name="country"
+          placeholder={t('page.checkout.card_info.country')}
+          value={values.country}
+          options={allCountries}
+          onChange={(e) => {
+            handleChange(e);
+            getStatesById(e.target.value)
+          }}
+          onBlur={handleBlur}
+          errorBorderColor="#d9534f"
+          isInvalid={!!errors.country && touched.country}
+        />
+        <SelectInputStyle
+          name="state"
+          placeholder={t('page.checkout.card_info.state')}
+          value={values.state}
+          options={allStatesById}
           onChange={handleChange}
           onBlur={handleBlur}
-          isInvalid={!!errors.terms && touched.terms}
+          errorBorderColor="#d9534f"
+          isInvalid={!!errors.state && touched.state}
         />
-        <Text fontSize="12px" color={colors.generalText[colorMode]}>
-          {t('page.checkout.card_info.authorize')}
-        </Text>
+        <Input
+          name="address1"
+          type="text"
+          value={values.address1}
+          placeholder={t('page.checkout.card_info.address01')}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          errorMessage={errors.address1}
+          error={!!errors.address1 && touched.address1}
+        />
+        <Input
+          name="address2"
+          type="text"
+          value={values.address2}
+          placeholder={t('page.checkout.card_info.address02')}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          errorMessage={errors.address2}
+          error={!!errors.address2 && touched.address2}
+        />
+        <Input
+          name="number"
+          type="text"
+          value={values.number}
+          placeholder={t('page.checkout.card_info.number')}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          errorMessage={errors.number}
+          error={!!errors.number && touched.number}
+        />
+        <Input
+          name="zip"
+          type="text"
+          value={values.zip}
+          placeholder={t('page.checkout.card_info.zip_code')}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          errorMessage={errors.zip}
+          error={!!errors.zip && touched.zip}
+        />
+        <Input
+          name="district"
+          type="text"
+          value={values.district}
+          placeholder={t('page.checkout.card_info.district')}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          errorMessage={errors.district}
+          error={!!errors.district && touched.district}
+        />
+        <Input
+          name="city"
+          type="text"
+          value={values.city}
+          placeholder={t('page.checkout.card_info.city')}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          errorMessage={errors.city}
+          error={!!errors.city && touched.city}
+        />
+        <Flex alignItems="flex-start" gridGap="12px" mt="1em">
+          <Checkbox
+            fontSize="12px"
+            name="terms"
+            id="terms"
+            isChecked={values.terms}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            isInvalid={!!errors.terms && touched.terms}
+          />
+          <Text htmlFor='terms' fontSize="12px" color={colors.generalText[colorMode]}>
+            {t('page.checkout.card_info.authorize')}
+          </Text>
+        </Flex>
+        <Button
+          textTransform="uppercase"
+          height="56px"
+          width="236px"
+          disabled={disabledButton}
+          isLoading={disabledButton}
+          onClick={() => handleSubmit()}
+          backgroundColor="#D0021B"
+          color="white"
+          _hover={{ bg: '#9B84FE' }}
+          _disabled={{ bg: '#A4A4A4' }}
+          _active={{ bg: '#9B84FE' }}
+        >
+          {t('page.checkout.card_info.place_you_order')}
+        </Button>
       </Flex>
-      <Button
-        textTransform="uppercase"
-        height="56px"
-        width="236px"
-        disabled={disabledButton}
-        isLoading={disabledButton}
-        onClick={() => handleSubmit()}
-        backgroundColor="#D0021B"
-        color="white"
-        _hover={{ bg: '#9B84FE' }}
-        _disabled={{ bg: '#A4A4A4' }}
-        _active={{ bg: '#9B84FE' }}
-      >
-        {t('page.checkout.card_info.place_you_order')}
-      </Button>
-      <ModalNotification {...{ isOpen, onClose, modalType }} />
-    </Flex>
+    </>
   )
 }
