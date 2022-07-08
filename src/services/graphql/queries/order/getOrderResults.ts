49b3e80a8e4d16c3d439048f1dc107eee7491a58
Renato Siqueira <renatoelysiqueira@gmail.com>
2022-07-08 16:50:30 -0300

    Feat: included new query

---
+++ src/services/graphql/queries/order/getOrderResults.ts
@@ -0,0 +1,10 @@
+import { gql } from '@apollo/client'
+
+export const QUERY_GET_ORDER_RESULT = gql`
+  query getOrderResult($id: String!) {
+    order(id: $id) {
+      id
+      status
+    }
+  }
+`
