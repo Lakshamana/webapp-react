8e3086ed19d0823809df1aeff5f1f6bcc2e1d384
Renato Siqueira <renatoelysiqueira@gmail.com>
2022-07-09 16:48:48 -0300

    Feat: add mutation

---
+++ src/services/graphql/mutation/order/addPendingOrder.ts
@@ -0,0 +1,10 @@
+import { gql } from '@apollo/client'
+
+export const MUTATION_ADD_PENDING_ORDER = gql`
+  mutation addPendingOrder ($product: String!) {
+    addPendingOrder(payload: { product: $product }) {
+      id
+      status
+    }
+  }
+`
