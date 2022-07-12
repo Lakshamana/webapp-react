4bc9c4ff27f79a6028e376703326024bd01bc799
João Pedro Moraes <joaopedromoraes@outlook.com>
2022-07-12 19:00:09 -0300

    feat: update types profileinfo

--- src/modules/account/components/profileInfo/types.ts
+++ src/modules/account/components/profileInfo/types.ts
@@ -2,6 +2,7 @@ import { Profile } from 'generated/graphql';
 
 export interface ProfileData {
   updateProfile: (profile: Object) => void
+  updateAvatar: (image: string | null) => void,
   isLoading: boolean
   user: Profile
   locale: string
