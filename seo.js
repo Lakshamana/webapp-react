b197b79daaae9c8ea1a10307787e8670fe679d86
Renato Siqueira <renatoelysiqueira@gmail.com>
2022-07-14 15:38:47 -0300

    Refact: included new fields

--- seo.js
+++ seo.js
@@ -1,8 +1,27 @@
 module.exports = [
   {
     path: "/",
+    favicon: '%PUBLIC_URL%',
     title: "MARVEL - Fan Hero",
-    description: "O Marvel Cinematic Universe é uma franquia de mídia americana e um universo compartilhado centrado em uma série de filmes de super-heróis, produzida independentemente pela Marvel Studios e baseada em personagens que aparecem nas revistas em quadrinhos americanas publicadas pela Marvel",
-    image: 'https://legadodamarvel.com.br/wp-content/uploads/2022/07/Grande-ator-da-Marvel-e-preso-alcoolizado-e-fas-teorizam-sua-demissao-legadodamarvel-750x394.webp'
+    description: "O Marvel Cinematic Universe é uma franquia de mídia americana e um universo compartilhado centrado em uma série de filmes de super-heróis.",
+    url: '<url-here>',
+    image: 'https://legadodamarvel.com.br/wp-content/uploads/2022/07/Grande-ator-da-Marvel-e-preso-alcoolizado-e-fas-teorizam-sua-demissao-legadodamarvel-750x394.webp',
+    domain: 'fanhero.tv'
   },
+  {
+    path: "/coca-cola",
+    favicon: 'https://express-favicon.herokuapp.com/coca-cola',
+    title: 'Coca Cola Platform',
+    description: 'Coca-Cola é um refrigerante carbonado vendido em lojas, restaurantes, mercados e máquinas de venda automática em todo o mundo.',
+    url: '<url-here>',
+    image: 'https://www.coca-cola.com.br/assets/images/home/no-sugar/mobile/414w/no-sugar-banner.png?v=69c479da55b6816037a616641ef3f4bd',
+    domain: 'cocacola.fanhero.tv'
+  },
+  {
+    path: "/valetudo",
+    favicon: 'https://express-favicon.herokuapp.com/favicon1',
+    title: 'VALE TUDO!!',
+    image: 'https://www.coca-cola.com.br/assets/images/home/no-sugar/mobile/414w/no-sugar-banner.png?v=69c479da55b6816037a616641ef3f4bd',
+    domain: 'cocacola.fanhero.tv'
+  }
 ];
--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const app = new express()
 const PORT = process.env.PORT || 3003
 
 const defaultValues = {
+  favicon: '%PUBLIC_URL%',
   title: 'Fanhero Video Platform',
-  description: 'An end-to-end video platform powered by the Fanhero Video Technology Cloud'
+  description: 'An end-to-end video platform powered by the Fanhero Video Technology Cloud',
+  url: '<url-here>',
+  image: 'https://fanhero.com/wp-content/uploads/img-home-2.jpg.webp',
+  domain: 'fanhero.tv'
 }
 
 app.use("/static", express.static(path.join(__dirname, "build/static")))
@@ -15,12 +19,16 @@ app.get("*", (req, res) => {
   let pathname = req.pathname || req.originalUrl
   let page = seo.find((item) => item.path === pathname)
   console.log('SEO', page, req)
-  if (!page) { page = defaultValues }
+  let defineValues = { ...defaultValues, ...page }
   let html = fs.readFileSync(path.join(__dirname, "build", "index.html"))
   let htmlWithSeo = html
     .toString()
-    .replace("__SEO_TITLE__", page.title)
-    .replace("__SEO_DESCRIPTION__", page.description)
+    .replace("__SEO_FAVICON__", defineValues.favicon)
+    .replace("__SEO_TITLE__", defineValues.title)
+    .replace("__SEO_DESCRIPTION__", defineValues.description)
+    .replace("__SEO_URL__", defineValues.url)
+    .replace("__SEO_IMAGE__", defineValues.image)
+    .replace("__SEO_DOMAIN__", defineValues.domain)
   return res.send(htmlWithSeo)
 })
 
