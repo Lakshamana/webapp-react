2a53ecbd6c2de679cc495128d0ef06990b601726
Renato Siqueira <renatoelysiqueira@gmail.com>
2022-07-14 16:53:18 -0300

    Fix

--- server.js
+++ server.js
@@ -18,10 +18,12 @@ app.use("/static", express.static(path.join(__dirname, "build/static")))
 app.get("*", (req, res) => {
   let pathname = req.pathname || req.originalUrl
   let page = seo.find((item) => item.path === pathname)
+
   console.log('SEO', page, 'req', 'HOST >>:', req.host)
+
+  let html = fs.readFileSync(path.join(__dirname, "build", "index.html"))
   if (page) {
     let defineValues = { ...defaultValues, ...page }
-    let html = fs.readFileSync(path.join(__dirname, "build", "index.html"))
     let htmlWithSeo = html
       .toString()
       .replace("__SEO_FAVICON_ICON__", `${defineValues.favicon}/favicon.ico`)
@@ -31,10 +33,20 @@ app.get("*", (req, res) => {
       .replaceAll("__SEO_URL__", defineValues.url)
       .replaceAll("__SEO_IMAGE__", defineValues.image)
       .replaceAll("__SEO_DOMAIN__", defineValues.domain)
-    console.log(defineValues)
     return res.send(htmlWithSeo)
   }
-  return res.sendFile(path.join(__dirname, "build", "index.html"));
+
+  let htmlWithSeo = html
+    .toString()
+    .replace("__SEO_FAVICON_ICON__", `${defaultValues.favicon}/favicon.ico`)
+    .replace("__SEO_FAVICON_APPLE__", `${defaultValues.favicon}/logo192.png`)
+    .replaceAll("__SEO_TITLE__", defaultValues.title)
+    .replaceAll("__SEO_DESCRIPTION__", defaultValues.description)
+    .replaceAll("__SEO_URL__", defaultValues.url)
+    .replaceAll("__SEO_IMAGE__", defaultValues.image)
+    .replaceAll("__SEO_DOMAIN__", defaultValues.domain)
+  return res.send(htmlWithSeo)
+  // return res.sendFile(path.join(__dirname, "build", "index.html"));
 })
 
 app.listen(PORT, () => console.log("listen on port: " + PORT))
