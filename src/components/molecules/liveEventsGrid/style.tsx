b20e61db9904623a9abcef9fdd791364874ed464
biancasilva <bianca@fanhero.com>
2022-07-14 18:48:10 -0300

    feat: add new liveEventsGrid molecule

---
+++ src/components/molecules/liveEventsGrid/style.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Wrapper: any = styled.div`
+  &:hover {
+    transition: all 250ms ease-out;
+    -webkit-transition: all 250ms ease-out;
+    margin-top: -30px;
+    transform: scale(1.2);
+    z-index: 9999;
+  }
+`
