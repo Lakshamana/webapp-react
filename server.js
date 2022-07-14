13b3712b2ec885f7f1f6ccfbb5ce5a473b220eba
Renato Siqueira <renatoelysiqueira@gmail.com>
2022-07-14 14:21:42 -0300

    Refact: defined default values

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-const seo = require("./seo");
-const app = new express();
+const express = require("express")
+const path = require("path")
+const fs = require("fs")
+const seo = require("./seo")
+const app = new express()
 const PORT = process.env.PORT || 3003
 
-app.use("/static", express.static(path.join(__dirname, "build/static")));
-app.get('/test', (req, res) => res.send('ok test again 2'))
-app.get("*", (req, res) => {
-  let pathname = req.pathname || req.originalUrl;
-  let page = seo.find((item) => item.path === pathname);
-  if (page) {
-    let html = fs.readFileSync(path.join(__dirname, "build", "index.html"));
-    let htmlWithSeo = html
-      .toString()
-      .replace("__SEO_TITLE__", page.title)
-      .replace("__SEO_DESCRIPTION__", page.description);
-    return res.send(htmlWithSeo);
-  }
-  return res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+const defaultValues = {
+  title: 'Fanhero Video Platform',
+  description: 'An end-to-end video platform powered by the Fanhero Video Technology Cloud'
+}
 
-app.listen(PORT, () => console.log("listened on " + PORT))
+app.use("/static", express.static(path.join(__dirname, "build/static")))
+app.get("*", (req, res) => {
+  let pathname = req.pathname || req.originalUrl
+  let page = seo.find((item) => item.path === pathname)
+  console.log('SEO', page)
+  if (!page) { page = defaultValues }
+  let html = fs.readFileSync(path.join(__dirname, "build", "index.html"))
+  let htmlWithSeo = html
+    .toString()
+    .replace("__SEO_TITLE__", page.title)
+    .replace("__SEO_DESCRIPTION__", page.description)
+  return res.send(htmlWithSeo)
+})
+
+app.listen(PORT, () => console.log("listen on port: " + PORT))
