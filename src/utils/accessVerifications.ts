e0e5fa7594a9d53cf14d81c7049b443e7f4a0512
biancasilva <bianca@fanhero.com>
2022-07-22 10:50:55 -0300

    feat: add isGeolocked verification to utils

--- src/utils/accessVerifications.ts
+++ src/utils/accessVerifications.ts
@@ -6,7 +6,7 @@ import {
   Post,
   SearchCategory,
   SearchLiveEvent,
-  SearchPost,
+  SearchPost
 } from 'generated/graphql'
 import { AccessEnum } from 'types/access'
 
@@ -26,6 +26,18 @@ export const isEntityPrivate = (entity: EntityType | Channel) => {
   return entity.kind === Kinds.Private
 }
 
-export const isEntityBlocked = (entity: EntityType) => {
+export const isEntityGeolocked = (entity: EntityType) => {
+  return entity.access === AccessEnum.GEOLOCKED
+}
+
+export const isEntityExclusive = (entity: EntityType) => {
   return isEntityOnPaywall(entity) || isEntityPrivate(entity)
+}
+
+export const isEntityBlocked = (entity: EntityType) => {
+  return (
+    isEntityOnPaywall(entity) ||
+    isEntityPrivate(entity) ||
+    isEntityGeolocked(entity)
+  )
 }
