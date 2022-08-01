094e958eadeb85bac39bcf4d4a64ebc868823159
Renato Siqueira <renatoelysiqueira@gmail.com>
2022-08-01 17:11:56 -0300

    Refact: Moved compts and included modal behavior

--- src/components/atoms/livestreamPostCard/index.tsx
+++ src/components/atoms/livestreamPostCard/index.tsx
@@ -1,94 +1,78 @@
 import { useMediaQuery } from '@chakra-ui/media-query'
-import { Box, Flex, Text } from '@chakra-ui/react'
-import { Icon } from '@iconify/react'
+import { Modal, ModalContent, ModalOverlay, useDisclosure } from '@chakra-ui/react'
 import { useState } from 'react'
 import { useHistory } from 'react-router'
 import { useThemeStore } from 'services/stores'
-
-import { Badge } from '@chakra-ui/react'
-import { Status } from 'generated/graphql'
 import { breakpoints, colors } from 'styles'
 import { LivestreamPostCardProps } from 'types/livestreams'
-import { stripHTML, stripHTMLExceptLineBreaks } from 'utils/helperFunctions'
-import { BlockedContent, CardWrapper, PostContent } from './style'
+import { PostCard } from './postCard'
 
 const LivestreamPostCard = ({ ...props }: LivestreamPostCardProps) => {
   const history = useHistory()
   const { colorMode } = useThemeStore()
   const [hover, setHover] = useState(false)
+  const [mobileBehavior, setMobileBehavior] = useState(false)
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const [isDesktop] = useMediaQuery(`(min-width: ${breakpoints.sm})`)
 
-  const isLive = props.status === Status.Live
+  const actionHover = (status: boolean) => () => {
+    if (isDesktop) {
+      setHover(status)
+      setMobileBehavior(false)
+    }
+  }
 
-  const selectPost = () => {
-    history.push(`${props.url}`)
+  const defineAction = () => {
+    if (isDesktop || mobileBehavior) {
+      history.push(`${props.url}`)
+      return
+    }
+    setHover(true)
+    setMobileBehavior(true)
+    onOpen()
+  }
+
+
+  const DefineWrapper = ({ children }) => {
+    const closeModal = () => {
+      setHover(false)
+      setMobileBehavior(false)
+      onClose()
+    }
+    return (
+      <Modal isOpen={isOpen} onClose={closeModal} isCentered>
+        <ModalOverlay />
+        <ModalContent
+          color={colors.generalText[colorMode]}
+          background={colors.cardBg[colorMode]}
+        >
+          {children}
+        </ModalContent>
+      </Modal>
+    )
   }
 
   return (
-    <CardWrapper
-      onMouseLeave={() => setHover(false)}
-      onMouseEnter={() => setHover(true)}
-    >
-      <PostContent onClick={selectPost} {...props}>
-        {(props.isExclusive || props.isGeolocked) && (
-          <BlockedContent>
-            <Icon
-              width={20}
-              color={colors.white}
-              icon={`mdi:${props.isExclusive ? 'lock' : 'earth'}`}
-            />
-          </BlockedContent>
-        )}
-        {isLive && (
-          <Badge
-            position="absolute"
-            top="0"
-            right="0"
-            m={2}
-            fontWeight="700"
-            color={colors.generalText[colorMode]}
-            background={colors.brand.live_badges.live}
-          >
-            Live
-          </Badge>
-        )}
-      </PostContent>
-      {hover && isDesktop && (
-        <Box
-          position="absolute"
-          padding="0.6rem"
-          borderBottomLeftRadius="4px"
-          borderBottomRightRadius="4px"
-          w={'100%'}
-          background={colors.footerBg[colorMode]}
-        >
-          <Flex direction="column">
-            <Text
-              fontSize="0.85rem"
-              fontWeight="bolder"
-              color={colors.generalText[colorMode]}
-            >
-              {stripHTML(props.title || '')}
-            </Text>
-            {props.description && (
-              <Text
-                mt={1}
-                fontSize="0.7rem"
-                noOfLines={2}
-                lineHeight={'0.9rem'}
-                color={colors.secondaryText[colorMode]}
-              >
-                <span
-                dangerouslySetInnerHTML={{
-                  __html: stripHTMLExceptLineBreaks(props.description),
-                }}
-              />
-              </Text>
-            )}
-          </Flex>
-        </Box>
-      )}
-    </CardWrapper>
+    <>
+      <PostCard
+        hover={hover}
+        defineAction={defineAction}
+        actionHover={actionHover}
+        mobileBehavior={mobileBehavior}
+        {...props}
+      />
+      {
+        !isDesktop &&
+        <DefineWrapper>
+          <PostCard
+            hover={hover}
+            defineAction={defineAction}
+            actionHover={actionHover}
+            {...props}
+          />
+        </DefineWrapper>
+      }
+    </>
   )
 }
 
