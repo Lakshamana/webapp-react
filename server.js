39c7ed44b4c6e0eb1ef58e00afd833293c20efb6
Renato Siqueira <renatoelysiqueira@gmail.com>
2022-08-03 18:34:57 -0300

    Refact to include tenant data

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const axios = require('axios')
+const crypto = require('crypto-js')
 const path = require("path")
 const fs = require("fs")
 const app = new express()
 const PORT = process.env.PORT || 3004
 const API_ENDPOINT = 'fourdotzero-dev.fanheroapi.com' //process.env.REACT_APP_API_ENDPOINT
+const REMOVE_ENV_SECRET = '04e131888657a08d7c48ea2e2c22c5a9' //process.env.REACT_APP_REMOTE_ENV_SECRET
 
 const defaultValues = {
   favicon: '',
@@ -21,32 +23,47 @@ const getTenantData = async (req, res) => {
   let tenant = subDomain.includes('localhost') ? 'marvel-dev' : subDomain
 
   const endpoint = `https://${API_ENDPOINT}`
-
-  // const metadata = await axios.get()
-  // await axios.get(
-  //   endpoint + '/env-config',
-  //   { headers: { organization: `https://${tenant}.fanhero.tv` } }
-  // ).then((result) => {
-  //   const decryptedEnv = crypto.AES.decrypt(
-  //     result.data.body.data.result,
-  //     REMOVE_ENV_SECRET
-  //   )
-  //   const data = JSON.parse(decryptedEnv.toString(crypto.enc.Utf8))
-  //   console.log(data)
-  // })
-
-  // let page = seo.find((item) => item.path === pathname)
-  // let page = seo.find((item) => item.tenant === tenant)
-
-  // console.log('----------------')
-  // console.log('SEO', page)
-  console.log('HOST:', req.hostname)
-  console.log('TENANT:', tenant)
-  console.log('PATH: ', pathname)
-
   let html = fs.readFileSync(path.join(__dirname, "build", "index.html"))
   let defineValues = { ...defaultValues }
-  // if (page) { defineValues = { ...defaultValues, ...page } }
+
+  let TENANT_URL = `https://${tenant}.fanhero.tv`
+  await axios.get(
+    endpoint + '/env-config',
+    { headers: { organization: TENANT_URL } }
+  ).then((result) => {
+    const decryptedEnv = crypto.AES.decrypt(
+      result.data.body.data.result,
+      REMOVE_ENV_SECRET
+    )
+    const data = JSON.parse(decryptedEnv.toString(crypto.enc.Utf8))
+    defineValues = {
+      ...defaultValues,
+      title: data.inspireTenantId,
+      description: data.firebaseProject,
+      url: TENANT_URL,
+      image: 'https://fanhero.com/wp-content/uploads/img-home-2.jpg.webp',
+      domain: tenant
+    }
+  })
+
+  const getData = async (path, endpointName) => {
+    const startPosition = pathname.indexOf(path) + path.length
+    const postSlug = pathname.slice(startPosition, pathname.length)
+    await axios
+      .get(`${endpoint}/${endpointName}/metadata?slug=${postSlug}`)
+      .then(({ data }) => {
+        const { baseUrl, imgPath } = data.body.data.image
+        defineValues = {
+          ...defaultValues,
+          // favicon: 'https://express-favicon.herokuapp.com/coca-cola',
+          title: data.body.data.title,
+          description: data.body.data.description,
+          url: pathname,
+          image: `${baseUrl}/${imgPath}`
+        }
+      })
+      .catch(err => console.log(err))
+  }
 
   let isPost = pathname.includes('/post/')
   if (isPost) {
@@ -69,24 +86,10 @@ const getTenantData = async (req, res) => {
   }
 
   let isCategory = pathname.includes('/category/')
-  if (isCategory) {
-    const startPosition = pathname.indexOf('/category/') + 10
-    const postSlug = pathname.slice(startPosition, pathname.length)
-    await axios
-      .get(`${endpoint}/categories/metadata?slug=${postSlug}`)
-      .then(({ data }) => {
-        const image = data.body.data.image.baseUrl + '/' + data.body.data.image.imgPath
-        defineValues = {
-          ...defaultValues,
-          // favicon: 'https://express-favicon.herokuapp.com/coca-cola',
-          title: data.body.data.title,
-          description: data.body.data.description,
-          url: pathname,
-          image
-        }
-      })
-      .catch(err => console.log(err))
-  }
+  if (isCategory) getData('/category/', 'categories')
+
+  let isLive = pathname.includes('/live/')
+  if (isLive) getData('/live/', 'live-events')
 
   let htmlWithSeo = html
     .toString()
