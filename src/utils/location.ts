085049a8d9585fa168dc25ca0cbd459293c71e08
João Pedro Moraes <joaopedromoraes@outlook.com>
2022-08-09 17:45:19 -0300

    feat: add error handling

--- src/utils/location.ts
+++ src/utils/location.ts
@@ -4,7 +4,15 @@ import { LocationMaxmindResponse } from 'types/location.d'
 const { REACT_APP_API_ENDPOINT } = process.env
 
 export const get = async (): Promise<LocationMaxmindResponse> => {
-  const ip = await axios.get('https://api.ipify.org/?format=json')
-  const city = await axios.post(`https://${REACT_APP_API_ENDPOINT}/maxmind/lookup`, { ip: ip.data.ip })
-  return city.data.body.data
+  try {
+    const ip = await axios.get('https://api.ipify.org/?format=json')
+    const city = await axios.post(`https://${REACT_APP_API_ENDPOINT}/maxmind/lookup`, { ip: ip.data.ip })
+    return city.data.body.data
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw error;
+    } else {
+      throw new Error()
+    }
+  }
 }
