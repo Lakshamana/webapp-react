78a731cbacf925be76b740f062d9293b5673fe15
biancasilva <bianca@fanhero.com>
2022-08-16 00:13:18 -0300

    fix: extends SpaceProps

--- src/components/organisms/feedPostCard/components/pollPostCard/style.tsx
+++ src/components/organisms/feedPostCard/components/pollPostCard/style.tsx
@@ -1,50 +1,53 @@
-import styled from "styled-components"
-import { SpaceProps, space } from "styled-system"
-import { FeedPostCardProps, itensQuestions } from "../../types"
-import { colors } from "styles"
+import styled from 'styled-components'
+import { space, SpaceProps } from 'styled-system'
+import { colors } from 'styles'
+import { FeedPostCardProps, itensQuestions } from '../../types'
+
+interface Props extends SpaceProps, FeedPostCardProps {}
 
 export const PollContent = styled.div<SpaceProps>`
-	${space}
+  ${space}
 
-	height: auto;
+  height: auto;
 `
 
 export const PollStatus = styled.div<SpaceProps>`
-	${space}
+  ${space}
 
-	display: flex;
-	align-items: center;
+  display: flex;
+  align-items: center;
 `
 
-export const PollItem = styled.div<FeedPostCardProps>`
-	${space}
+export const PollItem = styled.div<Props>`
+  ${space}
 
-	position: relative;
-	display: flex;
-	background: ${colors.grey["900"]};
-	border-radius: 2px;
-	overflow: hidden;
-	font-size: 14px;
-	&:hover {
-		border: ${(props: FeedPostCardProps) =>
-			props.voted ? `2px solid ${colors.blue["300"]}` : ""};
-	}
+  position: relative;
+  display: flex;
+  background: ${colors.grey['900']};
+  border-radius: 2px;
+  overflow: hidden;
+  font-size: 14px;
+  &:hover {
+    border: ${(props: FeedPostCardProps) =>
+      props.voted && `2px solid ${colors.blue['300']}`};
+  }
 `
 
 export const PollItemText = styled.div<SpaceProps>`
-	${space}
+  ${space}
 
-	width: 85%;
-	display: flex;
-	align-items: center;
-	z-index: 1;
+  width: 85%;
+  display: flex;
+  align-items: center;
+  z-index: 1;
 `
 
 export const ProgressBar = styled.div`
-	width: ${(props: itensQuestions) => (props.percentage ? props.percentage : "")};
-	left: 0;
-	top: 0;
-	bottom: 0;
-	position: absolute;
-	background: ${colors.grey["800"]};
+  width: ${(props: itensQuestions) =>
+    props.percentage ? props.percentage : ''};
+  left: 0;
+  top: 0;
+  bottom: 0;
+  position: absolute;
+  background: ${colors.grey['800']};
 `
