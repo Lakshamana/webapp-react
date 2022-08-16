d518dad5064ce6f19f6163755eecf98e5abefdb7
biancasilva <bianca@fanhero.com>
2022-08-16 00:14:22 -0300

    feat: add new const to use on Feed filter

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -10,6 +10,7 @@ export const VIDEO_VOLUME = '@fh:video-volume'
 export const ANONYMOUS_AUTH = '@fh:anonymous-auth'
 export const SHOW_NEXT_VIDEO_IN = 10
 export const MAX_CARDS_SCROLLER_RESULTS = 10
+export const MAX_FEED_POSTS_RESULTS = 5
 export const DEFAULT_POLLING_INTERVAL = 20 * 1000
 export const DEFAULT_LIMIT_QUERY = 20
 export const DEFAULT_PAGESIZE_FEEDS = 10
