1a0e15cfec47947c8ac9c2b9dba9996d4ac9f96f
biancasilva <bianca@fanhero.com>
2022-08-16 00:06:24 -0300

    feat: add Public queries

--- src/services/graphql/queries/categories/categories.ts
+++ src/services/graphql/queries/categories/categories.ts
@@ -68,9 +68,122 @@ export const QUERY_CATEGORIES = gql`
   }
 `
 
+export const QUERY_PUBLIC_CATEGORIES = gql`
+  query GetPublicCategories($filter: CategoryFilter) {
+    publicCategories(filter: $filter) {
+      hasNextPage
+      hasPreviousPage
+      isFirstPage
+      isLastPage
+      page
+      pageNumberIsGood
+      pageSize
+      rows {
+        access
+        pinnedStatus {
+          pinned
+        }
+        parentId
+        slug
+        createdAt
+        sort
+        kind
+        customization {
+          desktop {
+            imgPath
+          }
+          mobile {
+            imgPath
+          }
+          thumbnail {
+            imgPath
+          }
+        }
+        children(filter: { sortBy: "sort.asc" }) {
+          parentId
+          slug
+          description
+          featuredAt
+          sort
+          pinnedStatus {
+            pinned
+          }
+          customization {
+            desktop {
+              imgPath
+            }
+            mobile {
+              imgPath
+            }
+            thumbnail {
+              imgPath
+            }
+            __typename
+          }
+          geoFence
+          id
+          kind
+          name
+          description
+        }
+        description
+        featuredAt
+        geoFence
+        id
+        name
+      }
+    }
+  }
+`
+
 export const QUERY_CATEGORIES_CARDS = gql`
   query GetCategoriesCards($filter: CategoryFilter) {
     categories(filter: $filter) {
+      hasNextPage
+      hasPreviousPage
+      isFirstPage
+      isLastPage
+      page
+      pageNumberIsGood
+      pageSize
+      rows {
+        id
+        name
+        access
+        description
+        pinnedStatus {
+          pinned
+        }
+        slug
+        kind
+        customization {
+          thumbnail {
+            imgPath
+          }
+        }
+        children(filter: { sortBy: "sort.asc" }) {
+          id
+          name
+          access
+          description
+          pinnedStatus {
+            pinned
+          }
+          slug
+          kind
+          customization {
+            thumbnail {
+              imgPath
+            }
+          }
+        }
+      }
+    }
+  }
+`
+export const QUERY_PUBLIC_CATEGORIES_CARDS = gql`
+  query GetPublicCategoriesCards($filter: CategoryFilter) {
+    publicCategories(filter: $filter) {
       hasNextPage
       hasPreviousPage
       isFirstPage
--- src/services/graphql/queries/live/liveEvents.ts
+++ src/services/graphql/queries/live/liveEvents.ts
@@ -29,3 +29,33 @@ export const QUERY_LIVE_EVENTS = gql`
     }
   }
 `
+// Query to access live events with anonymous token
+export const QUERY_PUBLIC_LIVE_EVENTS = gql`
+  query GetPublicLiveEvents($filter: LiveEventFilter) {
+    publicLiveEvents(filter: $filter) {
+      hasNextPage
+      hasPreviousPage
+      isFirstPage
+      isLastPage
+      page
+      pageCount
+      pageNumberIsGood
+      pageSize
+      rows {
+        id
+        access
+        createdAt
+        description
+        kind
+        scheduledStartAt
+        slug
+        status
+        thumbnail {
+          imgPath
+        }
+        title
+        __typename
+      }
+    }
+  }
+`
