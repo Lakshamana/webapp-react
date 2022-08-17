383f8a60789cc9c3220c5e0fa45eb153a7786728
João Pedro Moraes <joaopedromoraes@outlook.com>
2022-08-17 11:38:45 -0300

    Revert "Feature: add location rule in signup custom fields"

--- src/components/organisms/signupForm/components/customFields/index.tsx
+++ src/components/organisms/signupForm/components/customFields/index.tsx
@@ -1,43 +1,17 @@
 import { Flex } from '@chakra-ui/react'
 import { Button, CustomForm, Text } from 'components'
-import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useThemeStore } from 'services/stores/theme'
 import { colors, fonts, sizes } from 'styles'
-import { get } from 'utils/location'
 import { RegistrationProps } from './types'
 
 const CustomFieldsForm = ({
   handleFormSubmit,
   isLoading,
-  fields: customfields,
+  fields,
 }: RegistrationProps) => {
   const { t } = useTranslation()
   const { colorMode } = useThemeStore()
-  const itsNotBrazil = (value) => value.name !== 'rg' && value.name !== 'cpf'
-  const [fields, setfields] = useState(customfields)
-  const [loading, setloading] = useState(true)
-
-  const getLocation = async () => {
-    setloading(true)
-    try {
-      const {
-        country: { iso_code },
-      } = await get()
-      if (iso_code !== 'BR') {
-        setfields(fields.filter(itsNotBrazil))
-      }
-    } catch (e) {
-      console.error(e)
-    } finally {
-      setloading(false)
-    }
-  }
-
-  useEffect(() => {
-    getLocation()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
 
   return (
     <Flex alignItems={'center'} flexDirection={'column'} gridGap={2}>
@@ -58,29 +32,24 @@ const CustomFieldsForm = ({
       >
         {t('signup.custom_fields.subtitle')}
       </Text>
-      {
-        !loading && (
-          <CustomForm
-            fields={fields}
-            button={({ dirty, isValid, handleSubmit }) => (
-              <Button
-                width={[sizes.loginButtonWidth]}
-                marginTop={10}
-                isDisabled={!(dirty && isValid)}
-                label={t('common.next')}
-                onClick={handleSubmit}
-                isLoading={isLoading}
-              />
-            )}
-            handleFormSubmit={(data) => {
-              handleFormSubmit(data)
-            }}
+      <CustomForm
+        fields={fields}
+        button={({ dirty, isValid, handleSubmit }) => (
+          <Button
+            width={[sizes.loginButtonWidth]}
+            marginTop={10}
+            isDisabled={!(dirty && isValid)}
+            label={t('common.next')}
+            onClick={handleSubmit}
+            isLoading={isLoading}
           />
-        )
-      }
+        )}
+        handleFormSubmit={(data) => {
+          handleFormSubmit(data)
+        }}
+      />
     </Flex>
   )
 }
 
 export { CustomFieldsForm }
-
