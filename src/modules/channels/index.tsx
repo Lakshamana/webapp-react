460daba2b623a1f9995a376291a4dad0bbc2b5ca
biancasilva <bianca@fanhero.com>
2022-08-22 11:10:40 -0300

    feat: add PUBLIC_CHANNELS query and fix logic to use public Org

--- src/modules/channels/index.tsx
+++ src/modules/channels/index.tsx
@@ -1,24 +1,25 @@
-import { useEffect, useState } from 'react'
 import { useQuery } from '@apollo/client'
-import { Spinner, Flex } from '@chakra-ui/react'
+import { Flex, Spinner } from '@chakra-ui/react'
+import { Container, Text } from 'components'
+import { Channel, Kinds } from 'generated/graphql'
+import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useHistory } from 'react-router'
-import { QUERY_CHANNELS } from 'services/graphql'
+import { QUERY_CHANNELS, QUERY_PUBLIC_CHANNELS } from 'services/graphql'
+import { useAuthStore, useChannelsStore, useCommonStore, useOrganizationStore } from 'services/stores'
 import { useThemeStore } from 'services/stores/theme'
-import { useChannelsStore, useCommonStore } from 'services/stores'
-import { Channel } from 'generated/graphql'
-import { Container, Text } from 'components'
-import { ChannelsGrid } from './components'
 import { colors } from 'styles'
+import { ChannelsGrid } from './components'
 import { IDefinedChannels } from './types'
 
 const ChannelsPage = () => {
   const { t } = useTranslation()
   const history = useHistory()
   const { colorMode } = useThemeStore()
-  const [loading, setLoading] = useState(true)
   const { setChannelsList, channelsList, setActiveChannel } = useChannelsStore()
   const { setPageTitle } = useCommonStore()
+  const { isAnonymousAccess } = useAuthStore()
+  const { organization } = useOrganizationStore()
 
   const definedChannel = async ({
     id,
@@ -30,19 +31,28 @@ const ChannelsPage = () => {
     history.push(`/c/${slug}`)
   }
 
-  const { data } = useQuery(QUERY_CHANNELS, {
-    variables: {
-      filter: {},
-    },
-    onCompleted: async (result) => {
-      setChannelsList(result.channels)
-      if (result.channels.length === 1) {
-        await definedChannel({ ...result.channels[0] })
-        return
-      }
-      setLoading(false)
-    },
-  })
+  const { loading } = useQuery(
+    isAnonymousAccess && organization?.kind === Kinds.Public
+      ? QUERY_PUBLIC_CHANNELS
+      : QUERY_CHANNELS,
+    {
+      variables: {
+        filter: {},
+      },
+      onCompleted: async (result) => {
+        const channelsList = isAnonymousAccess
+          ? result.publicChannels
+          : result.channels
+
+        setChannelsList(channelsList)
+
+        if (channelsList.length === 1) {
+          await definedChannel({ ...channelsList[0] })
+          return
+        }
+      },
+    }
+  )
 
   const selectChannel = async (channelId: string | null) => {
     const selected = channelsList?.find(
@@ -63,6 +73,11 @@ const ChannelsPage = () => {
     setPageTitle(t('page.channels.page_title'))
     //eslint-disable-next-line
   }, [])
+
+  useEffect(() => {
+    if (organization?.kind === Kinds.Exclusive && isAnonymousAccess) history.replace('/login')
+    //eslint-disable-next-line
+  }, [organization])
 
   if (loading)
     return (
@@ -94,7 +109,7 @@ const ChannelsPage = () => {
       </Text>
       <ChannelsGrid
         channelSelected={selectChannel}
-        channelsList={data?.channels}
+        channelsList={channelsList || []}
       />
     </Container>
   )
