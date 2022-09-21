04a2bc69037b254c7a1c298d3ff07fa85211aa37
biancasilva <bianca@fanhero.com>
2022-09-20 22:58:28 -0300

    feat: track social-login analytics report

--- src/services/firebase/index.ts
+++ src/services/firebase/index.ts
@@ -18,6 +18,7 @@ import {
 import { CreateAccountSocialSignInDto } from 'generated/graphql'
 import { getData } from 'services/storage'
 import { SocialType } from 'types/common'
+import { sendAuthReport } from 'utils/analytics'
 
 const CUSTOM_TOKEN_AUTH = getAuth(firebaseApp)
 
@@ -85,7 +86,6 @@ export const SocialSignIn = (
   return new Promise(function (resolve, reject) {
     signInWithPopup(CUSTOM_TOKEN_AUTH, PROVIDER)
       .then((result: UserCredential) => {
-        // console.log("==> fluxo sucesso login")
         const credential = generateCredential(kind, result)
         if (credential) {
           const { refreshToken, accessToken } = result.user['stsTokenManager']
@@ -96,6 +96,7 @@ export const SocialSignIn = (
             refreshToken,
           })
         }
+        sendAuthReport({ email: `${result.user.email}`, kind: 'social_signin' })
       })
       .catch((err: AuthError) => {
         // console.log("==> fluxo erro login")
@@ -103,49 +104,27 @@ export const SocialSignIn = (
         const pendingCred = generatePendingCredential(kind, err)
         if (err.code === 'auth/account-exists-with-different-credential') {
           // console.log("==> fluxo erro 'auth/account-exists-with-different-credential'")
-          fetchSignInMethodsForEmail(CUSTOM_TOKEN_AUTH, email).then((methods) => {
-            // console.log(methods)
-            if (methods[0] === 'password') {
-              // console.log("==> fluxo erro method 'password'")
-              return reject(err)
-            }
-            const provider = getProvider(methods[0])
-            if (methods[0] !== kind) {
-              // console.log("==> fluxo erro if kind diferente do method")
-              const { currentUser } = CUSTOM_TOKEN_AUTH
-              if (currentUser) {
-                // console.log("==> fluxo de link de provinders")
-                linkWithPopup(currentUser, provider)
-                  .then((resultLink) => {
-                    // console.log("==> sucesso no fluxo de link de multiplas contas")
-                    // console.log(resultLink)
-                    const credential = generateCredential(kind, resultLink)
-                    if (credential) {
-                      const { refreshToken, accessToken } =
-                        resultLink.user['stsTokenManager']
-                      const { providerId } = credential
-                      resolve({
-                        accessToken,
-                        authProvider: providerId,
-                        refreshToken,
-                      })
-                    }
-                  })
-                  .catch((error) => {
-                    // console.log("==> erro no fluxo de link de multiplas contas")
-                    return reject(error)
-                  })
+          fetchSignInMethodsForEmail(CUSTOM_TOKEN_AUTH, email).then(
+            (methods) => {
+              // console.log(methods)
+              if (methods[0] === 'password') {
+                // console.log("==> fluxo erro method 'password'")
+                return reject(err)
               }
-            }
-            signInWithPopup(CUSTOM_TOKEN_AUTH, provider)
-              .then((result: UserCredential) => {
-                if (pendingCred) {
-                  linkWithCredential(result.user, pendingCred)
-                    .then((usercred) => {
-                      const credential = generateCredential(kind, usercred)
+              const provider = getProvider(methods[0])
+              if (methods[0] !== kind) {
+                // console.log("==> fluxo erro if kind diferente do method")
+                const { currentUser } = CUSTOM_TOKEN_AUTH
+                if (currentUser) {
+                  // console.log("==> fluxo de link de provinders")
+                  linkWithPopup(currentUser, provider)
+                    .then((resultLink) => {
+                      // console.log("==> sucesso no fluxo de link de multiplas contas")
+                      // console.log(resultLink)
+                      const credential = generateCredential(kind, resultLink)
                       if (credential) {
                         const { refreshToken, accessToken } =
-                          usercred.user['stsTokenManager']
+                          resultLink.user['stsTokenManager']
                         const { providerId } = credential
                         resolve({
                           accessToken,
@@ -154,11 +133,35 @@ export const SocialSignIn = (
                         })
                       }
                     })
-                    .catch((err) => reject(err))
+                    .catch((error) => {
+                      // console.log("==> erro no fluxo de link de multiplas contas")
+                      return reject(error)
+                    })
                 }
-              })
-              .catch((err) => reject(err))
-          })
+              }
+              signInWithPopup(CUSTOM_TOKEN_AUTH, provider)
+                .then((result: UserCredential) => {
+                  if (pendingCred) {
+                    linkWithCredential(result.user, pendingCred)
+                      .then((usercred) => {
+                        const credential = generateCredential(kind, usercred)
+                        if (credential) {
+                          const { refreshToken, accessToken } =
+                            usercred.user['stsTokenManager']
+                          const { providerId } = credential
+                          resolve({
+                            accessToken,
+                            authProvider: providerId,
+                            refreshToken,
+                          })
+                        }
+                      })
+                      .catch((err) => reject(err))
+                  }
+                })
+                .catch((err) => reject(err))
+            }
+          )
         }
         reject(err)
       })
