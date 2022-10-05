48afda75a0d4374a836dd5afbd788692f89e9cfa
João Pedro Moraes <joaopedromoraes@outlook.com>
2022-10-04 22:49:43 -0300

    feat: add responsive mode

--- src/modules/subscriptionManagement/index.tsx
+++ src/modules/subscriptionManagement/index.tsx
@@ -1,12 +1,15 @@
+import { Flex } from "@chakra-ui/react";
 import { Container } from "components";
-import { Navbar } from "modules/account/components";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useThemeStore } from "services/stores";
 import { sizes } from "styles";
+import { Navbar, SubscriptionCard } from "./components";
 
 export const SubscriptionManagement = () => {
   const { t } = useTranslation()
   const { colorMode } = useThemeStore()
+  const [active, setactive] = useState(true)
   return (
     <Container
       width={1}
@@ -16,6 +19,16 @@ export const SubscriptionManagement = () => {
       justifyContent="space-between"
     >
       <Navbar {...{ colorMode }} text={t('page.account.back')} />
+      <Flex
+        gridGap='29px'
+        w='100%'
+        justifyContent='center'
+        flexWrap='wrap'
+      >
+        <SubscriptionCard checked={active} onChange={(e) => setactive(e.target.checked)}/>
+        <SubscriptionCard checked={active} onChange={(e) => setactive(e.target.checked)}/>
+        <SubscriptionCard checked={active} onChange={(e) => setactive(e.target.checked)}/>
+      </Flex>
     </Container>
   )
 }
