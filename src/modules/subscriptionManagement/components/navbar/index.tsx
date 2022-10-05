fb1b47513e07ff41291bdd7abed1f8d66ca10d31
João Pedro Moraes <joaopedromoraes@outlook.com>
2022-10-04 21:02:04 -0300

    feat: create navbar component in module

---
+++ src/modules/subscriptionManagement/components/navbar/index.tsx
@@ -0,0 +1,41 @@
+import { Divider, Flex } from '@chakra-ui/layout'
+import { Icon } from '@iconify/react'
+import { Container, Text } from 'components'
+
+import { NavbarProps } from './types'
+
+import { useHistory } from 'react-router-dom'
+import { colors } from 'styles'
+
+const Navbar = ({ colorMode, text }: NavbarProps) => {
+  let history = useHistory()
+  return (
+    <Container width={1} mt={1} alignItems="center" mb={2}>
+      <Flex alignItems="center" onClick={history.goBack} cursor="pointer">
+        <Icon
+          icon="mdi:arrow-left"
+          width={20}
+          color={colors.generalText[colorMode]}
+        />
+        <Divider
+          orientation="vertical"
+          height={4}
+          ml={2}
+          mr={3}
+          color={colors.grey['700']}
+        />
+        <Text
+          style={{ textTransform: 'uppercase' }}
+          fontWeight={'600'}
+          fontSize={'1.05rem'}
+          color={colors.generalText[colorMode]}
+        >
+          {text}
+        </Text>
+      </Flex>
+    </Container>
+  )
+}
+
+export { Navbar }
+
---
+++ src/modules/subscriptionManagement/components/navbar/types.ts
@@ -0,0 +1,6 @@
+import { ColorMode } from 'types/common'
+
+export interface NavbarProps {
+  colorMode: ColorMode
+  text: string
+}
