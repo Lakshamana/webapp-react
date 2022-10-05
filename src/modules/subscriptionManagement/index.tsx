545cb1b94413a7683dc00fb2eac03d38fd785db9
João Pedro Moraes <joaopedromoraes@outlook.com>
2022-10-05 14:49:11 -0300

    feat: update style module

--- src/modules/subscriptionManagement/index.tsx
+++ src/modules/subscriptionManagement/index.tsx
@@ -1,9 +1,9 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 import { Container } from "components";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useThemeStore } from "services/stores";
-import { sizes } from "styles";
+import { colors, sizes } from "styles";
 import { Navbar, SubscriptionCard } from "./components";
 
 export const SubscriptionManagement = () => {
@@ -19,10 +19,17 @@ export const SubscriptionManagement = () => {
       justifyContent="space-between"
     >
       <Navbar {...{ colorMode }} text={t('page.account.back')} />
+      <Text
+        fontWeight='500'
+        fontSize='20px'
+        lineHeight='24px'
+        color={colors.generalText[colorMode]}
+        m='24px'
+      >Plans</Text>
       <Flex
         gridGap='29px'
         w='100%'
-        justifyContent='center'
+        justifyContent='flex-start'
         flexWrap='wrap'
       >
         <SubscriptionCard checked={active} onChange={(e) => setactive(e.target.checked)}/>
