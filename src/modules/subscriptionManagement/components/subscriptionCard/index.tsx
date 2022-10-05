5f1acde650ceb6bab90124eb80e18028eb233333
João Pedro Moraes <joaopedromoraes@outlook.com>
2022-10-04 22:22:17 -0300

    feat: add switch and types

--- src/modules/subscriptionManagement/components/subscriptionCard/index.tsx
+++ src/modules/subscriptionManagement/components/subscriptionCard/index.tsx
@@ -1,22 +1,22 @@
-import { Button, Flex, Text } from "@chakra-ui/react"
+import { Button, Flex, FormControl, FormLabel, Switch, Text } from "@chakra-ui/react"
 import { useState } from "react"
 import { useThemeStore } from "services/stores"
 import { colors } from "styles"
+import { Props } from "./types"
 
-export const SubscriptionCard = () => {
-  const {
-    label,
-    subtitle,
-    price,
-    detail,
-  } = {
-    label: 'ABS Monthly Plan',
-    subtitle: 'Flameng - Campeonato Carioca',
-    price: '$12,99/mo',
-    detail: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+export const SubscriptionCard = (
+  {
+    checked,
+    disabled,
+    onChange,
+    label = 'ABS Monthly Plan',
+    subtitle = 'Flameng - Campeonato Carioca',
+    price = '$12,99/mo',
+    description = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-  }
+  }: Props
+) => {
   const { colorMode } = useThemeStore()
   const [openDetail, setopenDetail] = useState(false)
   return (
@@ -52,7 +52,7 @@ export const SubscriptionCard = () => {
         </Flex>
         <Text fontWeight='400' fontSize='20px' lineHeight='24px'>{price}</Text>
       </Flex>
-      <Flex mt='33px'>
+      <Flex mt='33px' justifyContent='space-between' w="100%">
         <Button
           variant='unstyled'
           fontSize='14px'
@@ -64,13 +64,24 @@ export const SubscriptionCard = () => {
           disabled={openDetail}
           onClick={() => setopenDetail(!openDetail)}
         >PLAN DETAIL</Button>
+        <FormControl display='flex' alignItems='center' w='auto'>
+          <FormLabel htmlFor='active' mb='0'>
+            {checked ? 'Active' : 'Inactive'}
+          </FormLabel>
+          <Switch
+            id='active'
+            isChecked={checked}
+            isDisabled={disabled}
+            onChange={onChange}
+          />
+        </FormControl>
       </Flex>
       {
         openDetail && (
           <Flex flexDir='column' gridGap="28px" mt="40px">
             <Flex flexDir='column' gridGap="28px">
               <Text fontWeight='400' fontSize='20px' lineHeight='24px'>Plan detail</Text>
-              <Text>{detail}</Text>
+              <Text>{description}</Text>
               <Text fontWeight='400' fontSize='14px' lineHeight='17px'>{subtitle}</Text>
             </Flex>
             <Flex w='100%' justifyContent='center'>
@@ -78,7 +89,6 @@ export const SubscriptionCard = () => {
                 variant='unstyled'
                 fontSize='14px'
                 fontWeight='600'
-                // color='#0660f9'
                 _focus={{
                   outline: 'none'
                 }}
