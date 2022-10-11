5015de6eeb9836ae20a82c7c93e451f3950205f2
biancasilva <bianca@fanhero.com>
2022-10-11 13:59:53 -0300

    fix: oneSignal push config

---
+++ src/config/pushNotification/index.ts
@@ -0,0 +1,30 @@
+import { configEnvs } from 'config/envs'
+import i18n from 'config/i18n'
+import { organizationData } from 'config/organization'
+import OneSignal from 'react-onesignal'
+
+export async function runOneSignal() {
+  if (configEnvs.onesignalAppId && configEnvs.onesignalSafariWebId) {
+    await OneSignal.init({
+      appId: configEnvs.onesignalAppId,
+      safari_web_id: configEnvs.onesignalSafariWebId,
+      promptOptions: {
+        slidedown: {
+          prompts: [
+            {
+              type: 'push',
+              text: {
+                actionMessage: i18n.t('common.push.title', {
+                  org: organizationData?.name,
+                }),
+                acceptButton: i18n.t('common.push.allow'),
+                cancelButton: i18n.t('common.push.cancel'),
+              },
+            },
+          ],
+        },
+      },
+    })
+    OneSignal.showSlidedownPrompt()
+  }
+}
