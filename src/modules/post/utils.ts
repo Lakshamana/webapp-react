f46e2799ad316cf460c70c074538cf6affdbcdc8
biancasilva <bianca@fanhero.com>
2022-10-20 10:54:41 -0300

    feat: return Text and Photo post types

--- src/modules/post/utils.ts
+++ src/modules/post/utils.ts
@@ -4,7 +4,7 @@ import { PostType, Status } from 'generated/graphql'
 export const relatedPostsFilter = (page: number, categories?: string[]) => {
   return {
     filter: {
-      typeIn: [PostType.Video, PostType.OnDemand],
+      typeIn: [PostType.Video, PostType.OnDemand, PostType.Photo, PostType.Text],
       status: Status.Published,
       pageSize: MAX_PLAYLIST_RESULTS,
       page,
@@ -16,7 +16,7 @@ export const relatedPostsFilter = (page: number, categories?: string[]) => {
 export const playlistPostsFilter = (page: number) => {
   return {
     postsFilters: {
-      typeIn: [PostType.Video, PostType.OnDemand],
+      typeIn: [PostType.Video, PostType.OnDemand, PostType.Photo, PostType.Text],
       status: Status.Published,
       pageSize: MAX_PLAYLIST_RESULTS,
       page,
