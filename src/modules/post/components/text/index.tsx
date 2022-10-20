d93a846341ba741c77c5fde6fcc3a376cb075a3e
biancasilva <bianca@fanhero.com>
2022-10-20 10:45:42 -0300

    feat: add a new Text Post component

---
+++ src/modules/post/components/text/index.tsx
@@ -0,0 +1,44 @@
+import { Image } from '@chakra-ui/react'
+import { Post } from 'generated/graphql'
+import { ThumborInstanceTypes, ThumborParams, useThumbor } from 'services/hooks'
+
+const TextPost = ({ ...postData }: Post) => {
+  const { generateImage } = useThumbor()
+
+  // TODO: Transform getImageUrl on a hook
+  const getImageUrl = () => {
+    const imageOptions: ThumborParams = {
+      size: {
+        width: 0,
+        height: 500,
+      },
+    }
+    const imgPath =
+      postData.media?.__typename === 'MediaPhoto' ? postData.media?.imgPath : ''
+
+    const image = generateImage(
+      ThumborInstanceTypes.IMAGE,
+      imgPath || '',
+      imageOptions
+    )
+
+    return image
+  }
+
+  const postImage = getImageUrl()
+
+  if (!postImage) return <></>
+
+  return (
+    <Image
+      objectPosition="center"
+      boxSize={'auto'}
+      alt={postData.title}
+      src={postImage}
+      htmlWidth="auto"
+      loading="lazy"
+    />
+  )
+}
+
+export { TextPost }
