d96119c2b8872f060d22a3fc3e6e6e88e5ef16cd
biancasilva <bianca@fanhero.com>
2022-10-20 11:11:56 -0300

    feat: make tagsScroller able to show text and photo post cards

--- src/components/molecules/tagsScroller/index.tsx
+++ src/components/molecules/tagsScroller/index.tsx
@@ -1,6 +1,12 @@
-import { CardsScroller, SkeletonScroller, VideoPostCard } from 'components'
+import {
+  CardsScroller,
+  PhotoPostCard,
+  SkeletonScroller,
+  TextPostCard,
+  VideoPostCard
+} from 'components'
 import { CategoryPostCard } from 'components/atoms'
-import { Category, Post } from 'generated/graphql'
+import { Category, Post, PostType } from 'generated/graphql'
 import { useEffect, useState } from 'react'
 import { ThumborInstanceTypes, ThumborParams, useThumbor } from 'services/hooks'
 import { useChannelsStore } from 'services/stores'
@@ -17,8 +23,9 @@ const TagsScroller = (props: Props) => {
   const [filteredItems, setFilteredItems] = useState<TagScrollerItem[]>()
 
   useEffect(() => {
-    const allItems: any = props.rows
-      .sort((a, b) => (a.createdAt || a.publishedAt) - (b.createdAt || b.publishedAt))
+    const allItems: any = props.rows.sort(
+      (a, b) => (a.createdAt || a.publishedAt) - (b.createdAt || b.publishedAt)
+    )
     setScrollerItems(allItems)
     //eslint-disable-next-line
   }, [props])
@@ -41,12 +48,13 @@ const TagsScroller = (props: Props) => {
           description: item.description,
           duration:
             item.__typename === 'Post' &&
-              item.media?.__typename === 'MediaVideo'
+            item.media?.__typename === 'MediaVideo'
               ? item.media?.duration
               : '',
           isExclusive: isEntityBlocked(item),
           thumbnail,
           isPinned: item.pinnedStatus?.pinned,
+          type: item.__typename === 'Post' ? item.type : '',
           url,
           __typename: item.__typename,
         }
@@ -56,24 +64,34 @@ const TagsScroller = (props: Props) => {
     //eslint-disable-next-line
   }, [scrollerItems])
 
+    //TODO: Build a hook to get Images
   const getImageUrl = (item: Post | Category) => {
-    const imageOptions: ThumborParams = { size: { height: 400 } }
+    const imageOptions: ThumborParams = { size: { height: 250 } }
     if (isEntityBlocked(item)) imageOptions.blur = 20
     let path = ''
-    if (item.__typename === 'Post') path = item.thumbnail?.imgPath || ''
+    if (item.__typename === 'Post')
+      path =
+        (item.media?.__typename === 'MediaVideo' && item.thumbnail?.imgPath) ||
+        (item.media?.__typename === 'MediaPhoto' && item.media.imgPath) ||
+        ''
     if (item.__typename === 'Category')
       path = item.customization?.thumbnail?.imgPath || ''
-    const image = generateImage(ThumborInstanceTypes.IMAGE, path, imageOptions)
-    return image
+
+    const secondImgUrl =
+      item.__typename === 'Post' && item.media?.__typename === 'MediaVideo'
+        ? `${item.media.baseUrl}/${item.media.thumbnailPath}`
+        : ''
+
+    if (path) {
+      return generateImage(ThumborInstanceTypes.IMAGE, path, imageOptions)
+    }
+
+    return secondImgUrl
   }
 
   return (
     <ContentScroller>
-      {
-        props.isLoading &&
-        !filteredItems?.length &&
-        <SkeletonScroller />
-      }
+      {props.isLoading && !filteredItems?.length && <SkeletonScroller />}
       {!!filteredItems?.length && (
         <CardsScroller
           title={props.sectionTitle}
@@ -83,9 +101,10 @@ const TagsScroller = (props: Props) => {
         >
           {filteredItems?.map((item: TagScrollerItem) => (
             <SwiperSlide key={`slide-${item.id}`}>
-              {item.__typename === 'Post' && (
-                <VideoPostCard key={`post-${item.id}`} {...item} />
-              )}
+              {(item.type === PostType.Video ||
+                item.type === PostType.OnDemand) && <VideoPostCard key={`post-${item.id}`} {...item} />}
+              {item.type === PostType.Text && <TextPostCard key={`post-${item.id}`} {...item} />}
+              {item.type === PostType.Photo && <PhotoPostCard key={`post-${item.id}`} {...item} />}
               {item.__typename === 'Category' && (
                 <CategoryPostCard key={`category-${item.id}`} {...item} />
               )}
