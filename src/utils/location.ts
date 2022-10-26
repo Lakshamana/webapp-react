8bdd10566e69b5e7c860180545342d97fcd04d88
biancasilva <bianca@fanhero.com>
2022-10-26 15:36:20 -0300

    fix: maxmind

--- src/utils/location.ts
+++ src/utils/location.ts
@@ -4,16 +4,20 @@ import { LocationMaxmindResponse } from 'types/location.d'
 
 const { REACT_APP_API_ENDPOINT } = process.env
 
-export const getMaxmindLocation = async (): Promise<LocationMaxmindResponse> => {
-  try {
-    const ip = await axios.get(GET_IP_API)
-    const city = await axios.post(`https://${REACT_APP_API_ENDPOINT}/maxmind/lookup`, { ip: ip.data.ip })
-    return city.data.body.data
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      throw error;
-    } else {
-      throw new Error()
+export const getMaxmindLocation =
+  async (): Promise<LocationMaxmindResponse> => {
+    try {
+      const ip = await axios.get(GET_IP_API)
+      const city = await axios.post(
+        `https://${REACT_APP_API_ENDPOINT}/maxmind/lookup`,
+        { ip: ip.data.ip }
+      )
+      return city.data
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        throw error
+      } else {
+        throw new Error()
+      }
     }
   }
-}
