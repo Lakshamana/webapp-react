ea138f598c27f5476a242dd2029e0a00dbf7f9ed
biancasilva <bianca@fanhero.com>
2022-11-02 17:07:14 -0300

    fix: channel card geolocked icon

--- src/components/atoms/contentCards/channel/index.tsx
+++ src/components/atoms/contentCards/channel/index.tsx
@@ -1,11 +1,10 @@
 import { Icon } from '@iconify/react'
-import { ChannelProps } from 'types/channel'
-import { ChannelContent, BlockedContent } from './styles'
 import { colors } from 'styles'
+import { ChannelProps } from 'types/channel'
+import { BlockedContent, ChannelContent } from './styles'
 
 const ChannelCard = ({ ...props }: ChannelProps) => {
   const selectChannel = () => props.id && props.onClick(props.id)
-
   return (
     <ChannelContent {...props} onClick={selectChannel}>
       {(props.isExclusive || props.isGeolocked) && (
@@ -14,7 +13,7 @@ const ChannelCard = ({ ...props }: ChannelProps) => {
             width={20}
             color={colors.white}
             icon={`mdi:${props.isExclusive ? 'lock' : 'earth'}`}
-          ></Icon>
+          />
         </BlockedContent>
       )}
     </ChannelContent>
--- src/modules/channels/components/channelsGrid/index.tsx
+++ src/modules/channels/components/channelsGrid/index.tsx
@@ -3,10 +3,18 @@ import { ChannelCard } from 'components'
 import { Channel } from 'generated/graphql'
 import { Props } from './types'
 
-import { ThumborInstanceTypes, ThumborParams, useThumbor } from 'hooks/useThumbor'
+import {
+  ThumborInstanceTypes,
+  ThumborParams,
+  useThumbor
+} from 'hooks/useThumbor'
 
 import { useCustomizationStore } from 'services/stores'
-import { isEntityBlocked, isEntityGeolocked } from 'utils/accessVerifications'
+import {
+  isEntityBlocked,
+  isEntityExclusive,
+  isEntityGeolocked
+} from 'utils/accessVerifications'
 
 const ChannelsGrid = ({ channelsList, channelSelected }: Props) => {
   const { generateImage } = useThumbor()
@@ -41,7 +49,7 @@ const ChannelsGrid = ({ channelsList, channelSelected }: Props) => {
             description={channel.description}
             key={channel.id}
             onClick={(value) => channelSelected(value)}
-            isExclusive={isEntityBlocked(channel)}
+            isExclusive={isEntityExclusive(channel)}
             isGeolocked={isEntityGeolocked(channel)}
             image={channelThumbnail}
           />
